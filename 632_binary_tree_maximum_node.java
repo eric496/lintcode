62e47d99b8d0a59e690782f6360f4941a546eb8f
eric496 <eric.mlengineer@gmail.com>
2017-05-15 22:51:09 -0400

    add 632

---
+++ 632_binary_tree_maximum_node.java
@@ -0,0 +1,34 @@
+/*
+Find the maximum node in a binary tree, return the node.
+Have you met this question in a real interview?
+Example
+
+Given a binary tree:
+
+     1
+   /   \
+ -5     2
+ / \   /  \
+0   3 -4  -5 
+
+return the node with value 3.
+*/
+
+public class Solution {
+    public TreeNode maxNode(TreeNode root) {
+        if(root == null) return null;
+        TreeNode left = maxNode(root.left);
+        TreeNode right = maxNode(root.right);
+        return max(root, max(left, right));
+    }
+    
+    TreeNode max(TreeNode left, TreeNode right) {
+        if(left == null) return right;
+        if(right == null) return left;
+        if(left.val < right.val) {
+            return right;
+        } else {
+            return left;
+        }
+    }
+}
