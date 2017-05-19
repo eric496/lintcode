01b4b264c1caae2eb1c65e612315062f64e683b4
eric496 <eric.mlengineer@gmail.com>
2017-05-18 22:57:52 -0400

    add 1

---
+++ 111_climbing_stairs.java
@@ -0,0 +1,23 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example
+Given an example n=3 , 1+1+1=2+1=1+2=3
+return 3
+*/
+
+public class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1) return 1;
+        int prevPrev = 1;
+        int prev = 1;
+        int cur = 0;
+        for(int i = 2; i <= n; i++) {
+            cur = prevPrev + prev;
+            prevPrev = prev;
+            prev = cur;
+        }
+        return cur;
+    }
+}
