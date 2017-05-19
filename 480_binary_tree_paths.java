85f2a77c5f10bdec62e31850d147dff974e5395d
eric496 <eric.mlengineer@gmail.com>
2017-05-18 22:53:28 -0400

    add 480

---
+++ 480_binary_tree_paths.java
@@ -0,0 +1,52 @@
+/*
+Given a binary tree, return all root-to-leaf paths.
+
+Example
+Given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+All root-to-leaf paths are:
+[
+  "1->2->5",
+  "1->3"
+]
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<String>();
+        if(root == null) return result;
+        findPaths(root, String.valueOf(root.val), result);
+        return result;
+    }
+
+    private void findPaths(TreeNode node, String path, List<String> result) {
+        if(node == null) return;
+        if(node.left == null && node.right == null) {
+            result.add(path);
+            return;
+        }
+        if(node.left != null) {
+            findPaths(node.left, path + "->" + String.valueOf(node.left.val), result);
+        }
+        if(node.right != null) {
+            findPaths(node.right, path + "->" + String.valueOf(node.right.val), result);
+        }
+    }
+}
