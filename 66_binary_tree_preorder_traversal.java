92e975ad6b0dc1fa6f035f662295c1044410353e
eric496 <eric.mlengineer@gmail.com>
2017-05-18 22:56:14 -0400

    add 66

---
+++ 66_binary_tree_preorder_traversal.java
@@ -0,0 +1,45 @@
+/*
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+Example
+Given:
+
+    1
+   / \
+  2   3
+ / \
+4   5
+
+return [1,2,4,5,3].
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {    
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null) return result;
+        stack.push(root);
+        while(!stack.empty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
+    }
+}
