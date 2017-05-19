ca4cfb55b85c638b91ec7020eb4676607242f62c
eric496 <eric.mlengineer@gmail.com>
2017-05-18 22:54:59 -0400

    add 68

---
+++ 68_binary_tree_postorder_traversal.java
@@ -0,0 +1,41 @@
+/*
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example
+Given binary tree {1,#,2,3},
+
+   1
+    \
+     2
+    /
+   3
+
+return [3,2,1].
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        traverse(root, result);
+        return result;
+    }
+    
+    private void traverse(TreeNode root, ArrayList<Integer> result) {
+        if(root == null) return;
+        traverse(root.left, result);
+        traverse(root.right, result);
+        result.add(root.val);
+    }
+}
+
