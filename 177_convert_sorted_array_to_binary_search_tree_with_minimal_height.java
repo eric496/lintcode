7df6eed97b2d51bf0ab7c77f6bad139636af6d3e
eric496 <eric.mlengineer@gmail.com>
2017-08-24 16:30:17 -0400

    add 177

---
+++ 177_convert_sorted_array_to_binary_search_tree_with_minimal_height.java
@@ -0,0 +1,42 @@
+/*
+Given a sorted (increasing order) array, Convert it to create a binary tree with minimal height.
+
+Notice
+There may exist multiple valid solutions, return any of them.
+
+Example
+Given [1,2,3,4,5,6,7], return
+
+     4
+   /   \
+  2     6
+ / \    / \
+1   3  5   7
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] A) {  
+        if(A == null || A.length == 0) return null;
+        return buildTree(A, 0, A.length - 1);
+    }  
+    
+    public TreeNode buildTree(int[] A, int start, int end) {
+        if(start > end) return null;
+        TreeNode node = new TreeNode(A[(start + end) / 2]);
+        node.left = buildTree(A, start, (start + end) / 2 - 1);
+        node.right = buildTree(A, (start + end) / 2 + 1, end);
+        return node;
+    }
+}
