262181a507654a18d473f9319aa00b438aca59a0
eric496 <eric.mlengineer@gmail.com>
2017-08-31 16:12:19 -0400

    add 662

---
+++ 662_guess_number_game.java
@@ -0,0 +1,28 @@
+/*
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+Example
+n = 10, I pick 4 (but you don't know)
+Return 4. Correct !
+*/
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1, high = n;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if(res == 0) {
+                return mid;
+            } else if(res == -1) {
+                high = mid - 1;
+            } else if(res == 1) {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
