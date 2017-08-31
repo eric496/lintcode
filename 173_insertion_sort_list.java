cff6ff586f1a576d618be21f13af2c3750c9e870
eric496 <eric.mlengineer@gmail.com>
2017-08-31 16:29:19 -0400

    add 173

---
+++ 173_insertion_sort_list.java
@@ -0,0 +1,37 @@
+/*
+Sort a linked list using insertion sort.
+
+Example
+Given 1->3->2->0->null, return 0->1->2->3->null.
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+    
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+
+        while (head != null) {
+            ListNode node = dummy;
+            while (node.next != null && node.next.val < head.val) {
+                node = node.next;
+            }
+            ListNode temp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+}
