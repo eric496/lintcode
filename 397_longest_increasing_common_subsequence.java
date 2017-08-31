4e1cde86fbad57ff6a8c8f3b5f39b6807d630951
eric496 <eric.mlengineer@gmail.com>
2017-08-31 16:45:47 -0400

    add 397

---
+++ 397_longest_increasing_common_subsequence.java
@@ -0,0 +1,41 @@
+/*
+Give an integer array，find the longest increasing continuous subsequence in this array.
+An increasing continuous subsequence:
+    Can be from right to left or from left to right.
+    Indices of the integers in the subsequence should be continuous.
+
+Notice
+O(n) time and O(1) extra space.
+
+Example
+For [5, 4, 2, 1, 3], the LICS is [5, 4, 2, 1], return 4.
+For [5, 1, 2, 3, 4], the LICS is [1, 2, 3, 4], return 4.
+*/
+
+public class Solution {
+    public int longestIncreasingContinuousSubsequence(int[] A) {
+        if(A.length == 0) return 0;
+        if(A.length == 1) return 1;
+        int result = 0;
+        int left = 1;
+        int right = 1;
+        for(int i = 0; i < A.length - 1; i++) {
+            if(A[i] < A[i+1]) {
+                left++;
+            } else {
+                left = 1;
+            }
+            result = Math.max(result, left);
+        }
+        
+        for(int i = A.length - 1; i >= 1; i--) {
+            if(A[i] < A[i-1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            result = Math.max(result, right);
+        }
+        return result;
+    }
+}
