4bc24074a9c3186fbfefbadfa34d66a844ba0140
eric496 <eric.mlengineer@gmail.com>
2017-08-31 22:12:30 -0400

    add 112

---
+++ 112_remove_duplicates_from_sorted_list.java
@@ -0,0 +1,34 @@
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode node = head;
+        while (node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
