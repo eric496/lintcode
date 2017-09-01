ea53df736102346cc05163bf080164624ff0c708
eric496 <eric.mlengineer@gmail.com>
2017-08-31 21:20:39 -0400

    add 110

---
+++ 110_minimum_path_sum.java
@@ -0,0 +1,24 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Notice
+You can only move either down or right at any point in time.
+*/
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] sum = new int[m][n];
+        sum[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++) sum[i][0] = sum[i-1][0] + grid[i][0];
+        for(int i = 1; i < n; i++) sum[0][i] = sum[0][i-1] + grid[0][i];
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                sum[i][j] = Math.min(sum[i-1][j], sum[i][j-1]) + grid[i][j];
+            }
+        }
+        return sum[m-1][n-1];
+    }
+}
