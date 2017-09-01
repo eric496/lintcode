e50bbda89174b3510d42f73502fa0d4893e22da1
eric496 <eric.mlengineer@gmail.com>
2017-08-31 21:48:32 -0400

    add 96

---
+++ 96_partition_list.java
@@ -0,0 +1,43 @@
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example
+Given 1->4->3->2->5->2->null and x = 3,
+return 1->2->2->4->3->5->null.
+*/
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+ 
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head == null) return null;
+        ListNode lhead = new ListNode(Integer.MIN_VALUE);
+        ListNode lrunner = lhead;
+        ListNode rhead = new ListNode(Integer.MIN_VALUE);
+        ListNode rrunner = rhead;
+        while(head != null) {
+            if(head.val < x) {
+                lrunner.next = head;
+                lrunner = lrunner.next;
+            } else {
+                rrunner.next = head;
+                rrunner = rrunner.next;
+            }
+            head = head.next;
+        }
+        rrunner.next = null;
+        lrunner.next = rhead.next;
+        return lhead.next;
+    }
+}
