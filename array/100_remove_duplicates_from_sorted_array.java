f857e02af8fe5e0aa6efbe100fe27d219aa5302d
eric496 <eric.mlengineer@gmail.com>
2017-09-01 13:44:46 -0400

    add thought process 100

--- array/100_remove_duplicates_from_sorted_array.java
+++ array/100_remove_duplicates_from_sorted_array.java
@@ -7,13 +7,25 @@ Given input array A = [1,1,2],
 Your function should return length = 2, and A is now [1,2].
 */
 
+/*
+Thought process
+1. Corner cases when the array is null or empty
+2. The question requires not only counting the length but changing the array IN PLACE as well
+*/
+
+/*
+O(n) time and O(1) space
+loop through the integer array, compare each element with its previous element
+if they are not the same, replace the count position with the current element
+if they are the same, loop goes on but count position does not move
+*/
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        int len = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != nums[len]) nums[++len] = nums[i]; 
+        if(nums.length == 0 || nums == null) return 0;
+        int count = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]) nums[count++] = nums[i];
         }
-        return len + 1;
+        return count;
     }
-}
+} 
