ca0d216678bb4d9637c03c43310a78617cf965ee
eric496 <eric.mlengineer@gmail.com>
2017-09-01 01:23:47 -0400

    add 115

---
+++ 115_unique_paths_II.java
@@ -0,0 +1,61 @@
+/*
+Follow up for "Unique Paths":
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Notice
+m and n will be at most 100.
+
+Example
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+*/
+
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid==null||obstacleGrid.length==0) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0]=1;
+     
+        //left column
+        for(int i=1; i<m; i++){
+            if(obstacleGrid[i][0]==1){
+                dp[i][0] = 0;
+            }else{
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+     
+        //top row
+        for(int i=1; i<n; i++){
+            if(obstacleGrid[0][i]==1){
+                dp[0][i] = 0;
+            }else{
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+     
+        //fill up cells inside
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+     
+            }
+        }
+     
+        return dp[m-1][n-1];
+    }
+}
