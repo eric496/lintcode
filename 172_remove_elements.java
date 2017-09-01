31650424a073140f0ae0c10453cd63b324f6324a
eric496 <eric.mlengineer@gmail.com>
2017-08-31 22:13:31 -0400

    add 172

---
+++ 172_remove_elements.java
@@ -0,0 +1,21 @@
+/*
+Given an array and a value, remove all occurrences of that value in place and return the new length.
+The order of elements can be changed, and the elements after the new length don't matter.
+
+Example
+Given an array [0,4,4,0,0,2,4,4], value=4
+return 4 and front four elements of the array is [0,0,0,2]
+*/
+
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        if(A == null || A.length == 0) return 0;
+        int i = 0;
+        int j = 0;
+        while(i < A.length) {
+            if(A[i] != elem) A[j++] = A[i];
+            i++;
+        }
+        return j;
+    }
+}
