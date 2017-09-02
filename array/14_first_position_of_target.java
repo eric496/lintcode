dd93814126197c8facd7d177de8696db6a3f7b31
eric496 <eric.mlengineer@gmail.com>
2017-09-01 22:58:53 -0400

    add thought process 14

--- array/14_first_position_of_target.java
+++ array/14_first_position_of_target.java
@@ -6,21 +6,29 @@ Example
 If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
 */
 
+/*
+Thought process
+1. A classical binary search problem when you see O(log n)
+2. Use start + 1 < end in while condition
+3. Notice it is required to find the FIRST index, so instead of "return mid" we should instead let "end = mid" and check the value at the end
+*/
+
 class Solution {
     public int binarySearch(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
-        int low = 0;
-        int high = nums.length - 1;
-        while(low + 1 < high) {
-            int middle = (high + low) / 2;
-            if(target <= nums[middle]) {
-                high = middle;
-            } else {
-                low = middle;
+        if(nums.length == 0 || nums == null) return -1;
+        int start = 0, end = nums.length - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid]) {
+                end = mid;
+            } else if(target > nums[mid]) {
+                start = mid;
+            } else if(target == nums[mid]) {
+                end = mid;
             }
         }
-        if(nums[low] == target) return low;
-        if(nums[high] == target) return high;
+        if(target == nums[start]) return start;
+        if(target == nums[end]) return end;
         return -1;
     }
 }
