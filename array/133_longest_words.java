bdf213aeea9a9e34420926252748340db8849330
eric496 <eric.mlengineer@gmail.com>
2017-09-01 20:34:11 -0400

    add thought process 133

--- array/133_longest_words.java
+++ array/133_longest_words.java
@@ -21,17 +21,42 @@ Given
 the longest words are ["like", "love", "hate"].
 */
 
+/*
+Thought process
+1. An obvious approach is two passes: first for loop to count the max length of all words; 
+second for loop to pick out the words which are of the max length.
+2. Don't forget the corner case when the dictionary is null or empty.
+*/
+
+//two passes
 class Solution {
     ArrayList<String> longestWords(String[] dictionary) {
-        if(dictionary.length == 0) return null;
+        if(dictionary == null || dictionary.length == 0) return null;
         int max = 0;
-        ArrayList<String> result = new ArrayList<String>();
-        for(String s: dictionary) {
+        for(String s : dictionary) {
             if(s.length() > max) max = s.length();
         }
-        for(String s: dictionary) {
-            if(s.length() == max) result.add(s);
+        ArrayList<String> res = new ArrayList<String>();
+        for(String s : dictionary) {
+            if(s.length() == max) res.add(s);
         }
-        return result;
+        return res;
+    }
+}
+
+//one pass
+class Solution {
+    ArrayList<String> longestWords(String[] dictionary) {
+        if(dictionary == null || dictionary.length == 0) return null;
+        ArrayList<String> res = new ArrayList<String>();
+        for(String s : dictionary) {
+            if(res.isEmpty() || s.length() > res.get(0).length()) {
+                res.clear();
+                res.add(s);
+            } else if(s.length() == res.get(0).length()) {
+                res.add(s);
+            }
+        }
+        return res;
     }
 }
