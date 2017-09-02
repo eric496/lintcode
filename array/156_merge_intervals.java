cd567a5445570849fbfc869eb38c9dab959be967
eric496 <eric.mlengineer@gmail.com>
2017-09-02 11:03:33 -0400

    add thought process 156

--- array/156_merge_intervals.java
+++ array/156_merge_intervals.java
@@ -11,29 +11,41 @@ Given intervals => merged intervals:
 ]
 */
 
+/*
+Thought process
+1. Sort the interval list by interval start values.
+2. Two pointers: if current start value is smaller than the end value, 
+   compare the end value with the current end value, store the greater one.
+   If current start value is greater than the end value, 
+   add interval to the result list and update the start and end value.
+3. DO NOT forget to add the last interval to the result list.
+*/
+
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        if (intervals == null || intervals.isEmpty()) return intervals;
-        List<Interval> result = new ArrayList<Interval>();
-        // sort with Comparator
-        Collections.sort(intervals, new IntervalComparator());
-        Interval prev = intervals.get(0);
-        for (Interval interval : intervals) {
-            if (prev.end < interval.start) {
-                result.add(prev);
-                prev = interval;
+        if(intervals == null || intervals.size() <= 1) return intervals;
+        
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval i1, Interval i2) {
+                return i1.start - i2.start;
+            }
+        });
+        
+        List<Interval> res = new LinkedList<Interval>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+        for (Interval i : intervals) {
+            if (i.start <= end) {
+                end = Math.max(end, i.end);
             } else {
-                prev.start = Math.min(prev.start, interval.start);
-                prev.end = Math.max(prev.end, interval.end);
+                res.add(new Interval(start, end));
+                start = i.start;
+                end = i.end;
             }
         }
-        result.add(prev);
-        return result;
-    }
-
-    private class IntervalComparator implements Comparator<Interval> {
-        public int compare(Interval a, Interval b) {
-            return a.start - b.start;
-        }
+        //add the last interval!
+        res.add(new Interval(start, end));
+        return res;
     }
 }
