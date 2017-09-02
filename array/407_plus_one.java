7e3bd8c68534cd5765b9ff86c0b23453c1dbd17b
eric496 <eric.mlengineer@gmail.com>
2017-09-02 18:29:21 -0400

    add thought process 407

--- array/407_plus_one.java
+++ array/407_plus_one.java
@@ -7,22 +7,27 @@ Given [1,2,3] which represents 123, return [1,2,4].
 Given [9,9,9] which represents 999, return [1,0,0,0].
 */
 
+/*
+Thought process
+1. To solve this type of problems in 3 steps:
+   1) sum = current number + carry;
+   2) sum % 10 as the number at ones place / tens place / hundreds place ...
+   3) carry sum / 10 to the next higher place.  
+*/
+
 public class Solution {
     public int[] plusOne(int[] digits) {
-        int sum = 0;
-        int carry = 1;
-        ArrayList<Integer> result = new ArrayList<Integer>();
+        int sum = 0, carry = 1;
+        ArrayList<Integer> res = new ArrayList<Integer>();
         for(int i = digits.length - 1; i >= 0; i--) {
             sum = digits[i] + carry;
-            result.add(sum % 10);
+            res.add(sum % 10);
             carry = sum / 10;
         }
-        if(carry != 0) result.add(carry);
-        int[] res = new int[result.size()];
-        for(int i = result.size() - 1; i >= 0; i--) {
-            res[result.size() - i - 1] = result.get(i);
-        }
-        return res;
+        if(carry != 0) res.add(carry);
+        Collections.reverse(res);
+        int[] result = new int[res.size()];
+        for(int i = 0; i < res.size(); i++) result[i] = res.get(i);
+        return result;
     }
 }
-
