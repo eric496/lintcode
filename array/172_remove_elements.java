61e1442cd1228d8657797ffe1df0d7fb4f970e5d
eric496 <eric.mlengineer@gmail.com>
2017-09-02 11:13:29 -0400

    add thought process 172

--- array/172_remove_elements.java
+++ array/172_remove_elements.java
@@ -7,6 +7,13 @@ Given an array [0,4,4,0,0,2,4,4], value=4
 return 4 and front four elements of the array is [0,0,0,2]
 */
 
+/*
+Thought process
+1. IN PLACE: probably use two pointers.
+2. Intuitively, we would like judge if value = k. But it is not straightforward programmatically this way. 
+   Instead we will judge if value != k, and keep moving these values in the front of the array.
+*/
+
 public class Solution {
     public int removeElement(int[] A, int elem) {
         if(A == null || A.length == 0) return 0;
