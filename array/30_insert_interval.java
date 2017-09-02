ced81361647a026d7308936cae9331b3aa7ab485
eric496 <eric.mlengineer@gmail.com>
2017-09-02 16:56:35 -0400

    add thought process 30

--- array/30_insert_interval.java
+++ array/30_insert_interval.java
@@ -17,23 +17,34 @@ Insert [3, 4] into [[1,2], [5,9]], we get [[1,2], [3,4], [5,9]].
  *     }
  */
 
+/*
+Thought process
+1. There are 3 cases: 1) new interval is strictly smaller than the current interval (newInterval.end < i.start)
+                      2) new interval is strictly greater than the current interval (newInterval.start > i.end)
+                      3) new interval is overlapping with the current interval
+2. Set a position pointer to keep track of the insertion position so we can insert interval outside the for loop.
+   It should increment only when case 2) is true.
+   For case 3), update the new interval start with the smaller value of new interval start and the current interval start, 
+   similarly, update the new interval end with the greater value of new interval end and the current interval end.
+*/
+
 class Solution {
     public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
         if(intervals == null || newInterval == null) return intervals;
-        ArrayList<Interval> result = new ArrayList<Interval>();
+        ArrayList<Interval> res = new ArrayList<Interval>();
         int pos = 0;
-        for(Interval interval : intervals) {
-            if(newInterval.end < interval.start) {
-                result.add(interval);
-            } else if(newInterval.start > interval.end) {
-                pos++;
-                result.add(interval);
+        for(Interval i : intervals) {
+            if(newInterval.end < i.start) {
+                res.add(i);
+            } else if(newInterval.start > i.end) {
+                res.add(i);
+		pos++;
             } else {
-                newInterval.start = Math.min(newInterval.start, interval.start);
-                newInterval.end = Math.max(newInterval.end, interval.end);
+                newInterval.start = Math.min(newInterval.start, i.start);
+                newInterval.end = Math.max(newInterval.end, i.end);
             }
         }
-        result.add(pos, newInterval);
-        return result;
+        res.add(pos, newInterval);
+        return res;
     }
 }
