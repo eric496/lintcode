86cb797af58219cf4049e21c7939f7111d6a7afe
eric496 <eric.mlengineer@gmail.com>
2017-09-02 12:36:58 -0400

    add thought process 197

--- math/197_permutation_index.java
+++ math/197_permutation_index.java
@@ -5,16 +5,31 @@ Example
 Given [1,2,4], return 1.
 */
 
+/*
+Thought process
+1. Count the number of the succeeding numbers which are smaller than current number (each one from left to right), set it to smaller_succeed.
+2. Suppose the index of the current number is i, we exchange the current number with one smaller succeeding number.
+   After the exchange, if the length from the current number (exclusive) to the end of the array is n, then there are n! eligible permutations.
+   Multiply smaller_succeed * n!, we get the possible permutations for the current number.
+   Add the statistic up for every number, we get the total number of permutations ahead of the current permutation. (initialize index = 1 will assure to return the current index.)
+*/
+
 public class Solution {
+    public long factorial(long n) {
+        if(n == 0) return 1;
+        if(n <= 2) return n;
+        return n * factorial(n-1);
+    }
+    
     public long permutationIndex(int[] A) {
         if (A == null || A.length == 0) return 0;
-        long index = 1, fact = 1;
-        for (int i = A.length - 1; i >= 0; i--) {
-            // get rank in every iteration
-            int rank = 0;
-            for (int j = i + 1; j < A.length; j++) if (A[i] > A[j]) rank++;
-            index += rank * fact;
-            fact *= (A.length - i);
+        long index = 1;
+        for (int i = 0; i < A.length; i++) {
+            int smaller_succeed = 0;
+            for (int j = i + 1; j < A.length; j++) {
+                if(A[i] > A[j]) smaller_succeed++;
+            }
+            index += smaller_succeed * factorial(A.length - i - 1);
         }
         return index;
     }
