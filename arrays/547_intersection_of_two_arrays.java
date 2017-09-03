f481a7db35beaeb90a56b331509e188b5994f666
eric496 <eric.mlengineer@gmail.com>
2017-09-03 19:13:50 -0400

    add thought process 547

--- arrays/547_intersection_of_two_arrays.java
+++ arrays/547_intersection_of_two_arrays.java
@@ -9,17 +9,25 @@ Example
 Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
 */
 
+/*
+Thought process:
+    1. Use two hash sets. Note that use hash set to store unique values as well
+       because this allows to automatically remove duplicates added to the set.
+*/
+
+//O(n) time and O(n) space
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> hash = new HashSet<Integer>();
         for(Integer n : nums1) hash.add(n);
-        HashSet<Integer> hashResult = new HashSet<Integer>();
+        HashSet<Integer> res = new HashSet<Integer>();
         for(Integer n : nums2) {
-            if(hash.contains(n) && !hashResult.contains(n)) hashResult.add(n);
+            //Actually !res.contains(n) is not necessary because it is set. Leave it here for better understanding.
+            if(hash.contains(n) && !res.contains(n)) res.add(n);
         }
-        int[] result = new int[hashResult.size()];
+        int[] result = new int[res.size()];
         int ix = 0;
-        for(Integer n : hashResult) result[ix++] = n;
+        for(Integer n : res) result[ix++] = n;
         return result;
     }
 }
