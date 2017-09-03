e613f6c42b0a9c13f46e7e32fc2f62795e53b381
eric496 <eric.mlengineer@gmail.com>
2017-09-03 16:20:07 -0400

    add thought process 457

--- arrays/457_classical_binary_search.java
+++ arrays/457_classical_binary_search.java
@@ -8,21 +8,29 @@ For target = 5, return 4 or 5.
 For target = 6, return -1.
 */
 
+/*
+Thought process: 
+    Remember to check the values at "start" and "end", 
+    because the while loop ends at where "start" meets "end", 
+    while the values at "start" and "end" haven't been checked yet.
+*/
+
 public class Solution {
     public int findPosition(int[] nums, int target) {
         if(nums == null || nums.length == 0) return -1;
-        int start = 0;
-        int end = nums.length - 1;
-        while(start + 1 < end) {
+        int start = 0, end = nums.length - 1;
+        while(start < end) {
             int mid = start + (end - start) / 2;
-            if(nums[mid] >= target) {
-                end = mid;
-            } else {
-                start = mid;
+            if(target == nums[mid]) {
+                return mid;
+            } else if(target > nums[mid]) {
+                start = mid + 1;
+            } else if(target < nums[mid]) {
+                end = mid - 1;
             }
         }
-        if(nums[start] == target) return start;
-        if(nums[end] == target) return end;
+        if(target == nums[start]) return start;
+        if(target == nums[end]) return end;
         return -1;
     }
 }
