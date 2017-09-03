bdd939c9a5320a35b08372c8ca71f10c6778376a
eric496 <eric.mlengineer@gmail.com>
2017-09-03 15:40:42 -0400

    add Kadane's algorithm 41

--- arrays/41_maximum_subarray.java
+++ arrays/41_maximum_subarray.java
@@ -22,6 +22,19 @@ public class Solution {
     }
 }
 
+//Kadane's algorithm
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int sum = nums[0], max = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            if(sum > max) max = sum;
+        }
+        return max;
+    }
+}
+
 //DP
 public int maxSubArray(int[] A) {
         int[] dp = new int[n];
