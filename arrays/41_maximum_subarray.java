881d3e681bb2b39dd759816ee5ce999cdb9a7c5f
eric496 <eric.mlengineer@gmail.com>
2017-09-03 16:02:55 -0400

    add thought process 41

--- arrays/41_maximum_subarray.java
+++ arrays/41_maximum_subarray.java
@@ -8,41 +8,26 @@ Example
 Given the array [−2,2,−3,4,−1,2,1,−5,3], the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 */
 
-public class Solution {
-    public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        for(Integer num : nums) {
-            sum += num;
-            max = Math.max(max, sum);
-            sum = Math.max(sum, 0);
-        }
-        return max;
-    }
-}
+/*
+Thought process
+1. Kadane's algorithm: 
+   the maximum sum of a contiguous subarray ending at a specific element is either 
+   1) the maximum sum of a contiguous subarray ending at its previous element, OR
+   2) the specific element itself, 
+   depending on which is larger. 
+   So, the algorithm is bascially loop through the array, compare each element with max sum of subarray ending at its previous element, 
+   keep the larger one and update the global maximum.
+*/
 
 //Kadane's algorithm
 public class Solution {
     public int maxSubArray(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
-        int sum = nums[0], max = nums[0];
+        int localMax = nums[0], globalMax = nums[0];
         for(int i = 1; i < nums.length; i++) {
-            sum = Math.max(sum + nums[i], nums[i]);
-            if(sum > max) max = sum;
+            localMax = Math.max(localMax + nums[i], nums[i]);
+            if(localMax > globalMax) globalMax = localMax;
         }
-        return max;
+        return globalMax;
     }
-}
-
-//DP
-public int maxSubArray(int[] A) {
-        int[] dp = new int[n];
-        dp[0] = A[0];
-        int max = dp[0];
-        for(int i = 1; i < A.length; i++){
-            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            max = Math.max(max, dp[i]);
-        }        
-        return max;
 }
