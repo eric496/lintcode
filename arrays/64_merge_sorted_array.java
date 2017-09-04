84a1f6086f24120e7177490b671d5829c556c02b
eric496 <eric.mlengineer@gmail.com>
2017-09-03 22:39:57 -0400

    add thought process 64

--- arrays/64_merge_sorted_array.java
+++ arrays/64_merge_sorted_array.java
@@ -9,23 +9,22 @@ A = [1, 2, 3, empty, empty], B = [4, 5]
 After merge, A will be filled as [1, 2, 3, 4, 5]
 */
 
+/*
+Thought process:
+    1. In-place solution: It is hard to compare elements and store them in A in forward order. 
+       So think about the comparison from the end. Do a pair comparison of each two elements from A and B from the end, 
+       store the larger one at the end of A. 
+    2. NOTICE that if there are more elements in B than A, we need to put the extra elements from B in their corresponding 
+       locations in A. On the other hand, we don't have to worry the extra elements in A if A has more elements than B, 
+       because they are already in the correct locations.
+*/
+
 class Solution {
     public void mergeSortedArray(int[] A, int m, int[] B, int n) {
         int ix = m + n - 1;
         m--;
         n--;
-        while(m >= 0 && n >= 0) {
-            if(A[m] > B[n]) {
-                A[ix] = A[m];
-                ix--;
-                m--;
-            } else {
-                A[ix] = B[n];
-                ix--;
-                n--;
-            }
-        }
-        while(m >= 0) A[ix--] = A[m--];
+        while(m >= 0 && n >= 0) A[ix--] = (A[m] > B[n]) ? A[m--] : B[n--];
         while(n >= 0) A[ix--] = B[n--];
     }
 }
