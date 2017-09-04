92f6f4941ea4be7cdb315791cc4a011d0f811376
eric496 <eric.mlengineer@gmail.com>
2017-09-04 17:04:34 -0400

    add thought process 212

--- strings/212_space_replacement.java
+++ strings/212_space_replacement.java
@@ -3,30 +3,36 @@ Write a method to replace all spaces in a string with %20. The string is given i
 You code should also return the new length of the string after replacement.
 
 Notice
-If you are using Java or Python，please use characters array instead of string.
+    If you are using Java or Python，please use characters array instead of string.
 
 Example
-Given "Mr John Smith", length = 13.
-The string after replacement should be "Mr%20John%20Smith", you need to change the string in-place and return the new length 17.
+    Given "Mr John Smith", length = 13.
+    The string after replacement should be "Mr%20John%20Smith", you need to change the string in-place and return the new length 17.
+
+Challenge
+    Do it in-place.
+*/
+
+/*
+Thought process:
+    1. It is hard to solve it in forward order loop. 
+       But there are enough empty space at the end of the array, so we can loop backward.
+    2. Notice we have to do an extra loop to count the spaces and the total length of the new array.
 */
 
 public class Solution {
     public int replaceBlank(char[] string, int length) {
         if(length == 0 || string == null) return 0;
-        int count = length;
-        for(int i = 0; i < length; i++) {
-            if(string[i] == ' ') count += 2;
-        }
+        int count = length; 
+        for(char c : string) if(c == ' ') count += 2;
         int ix = count - 1;
         for(int i = length - 1; i >= 0; i--) {
             if(string[i] == ' ') {
-                string[ix] = '0';
-                string[ix - 1] = '2';
-                string[ix - 2] = '%';
-                ix -= 3;
+                string[ix--] = '0';
+                string[ix--] = '2';
+                string[ix--] = '%';
             } else {
-                string[ix] = string[i];
-                ix--;
+                string[ix--] = string[i];
             }
         }
         return count;
