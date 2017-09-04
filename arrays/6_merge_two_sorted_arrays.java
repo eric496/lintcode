86af883127962ca379bfc86b43dc7d96293f8902
eric496 <eric.mlengineer@gmail.com>
2017-09-03 22:57:39 -0400

    add thought process 6

--- arrays/6_merge_two_sorted_arrays.java
+++ arrays/6_merge_two_sorted_arrays.java
@@ -7,22 +7,25 @@ B=[2,4,5,6]
 return [1,2,2,3,4,4,5,6]
 */
 
+/*
+Thought process
+    1. This problem is just the merge part of mergesort algorithm, so it is straightforward.
+       Create a new array whose size is size(A) + size(B). Do a pair comparison of each two 
+       elements from A and B, put the smaller one in the new array. Create 3 index runner to 
+       keep track of the current positions. 
+    2. Challenge: How can you optimize your algorithm if one array is very large and the other is very small? 
+*/
+
 class Solution {
     public int[] mergeSortedArray(int[] A, int[] B) {
-        int len = A.length + B.length;
-        int[] res = new int[len];
-        int i = 0, j = 0;
-        int index = 0;
-        while (i < A.length && j < B.length) {
-            if (A[i] < B[j]) {
-                res[index++]  = A[i++];
-            }
-            else {
-                res[index++] = B[j++];
-            }
+        int lenA = A.length, lenB = B.length;
+        int[] res = new int[lenA + lenB];
+        int pos = 0, posA = 0, posB = 0;
+        while(posA < lenA && posB < lenB) {
+            res[pos++] = (A[posA] < B[posB]) ? A[posA++] : B[posB++];
         }
-        while (i < A.length) res[index++] = A[i++];
-        while (j < B.length) res[index++] = B[j++];
+        while(posA < lenA) res[pos++] = A[posA++];
+        while(posB < lenB) res[pos++] = B[posB++];
         return res;
     }
 }
