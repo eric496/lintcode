34df6bb099727406af6e5e7f2c1ba137736b3af4
eric496 <eric.mlengineer@gmail.com>
2017-09-03 20:32:13 -0400

    add thought process 56

--- arrays/548_intersection_of_two_arrays_II.java
+++ arrays/548_intersection_of_two_arrays_II.java
@@ -41,3 +41,10 @@ public class Solution {
         return result;
     }
 }
+
+/*
+Follow-up questions:
+    What if the given array is already sorted? How would you optimize your algorithm?
+    What if nums1's size is small compared to num2's size? Which algorithm is better?
+    What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+*/
--- arrays/56_two_sum.java
+++ arrays/56_two_sum.java
@@ -10,6 +10,16 @@ numbers=[2, 7, 11, 15], target=9
 return [1, 2]
 */
 
+/*
+Thought process:
+    1. O(n) time and O(n) space solution: use hash map to store <target-number, index> pairs. 
+       Loop through the array, check if the hash map contains key number. 
+       If it does, then n2 = target - n1, where n1 is the first number and n2 is the second. 
+       Retrieve the indices of these two numbers - the first is the value of the matched key, 
+       the second is just the current iteration number. 
+    2. Don't forget to add 1 to the result indices because the result indices are not zero-based.
+*/
+
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         if(numbers == null || numbers.length < 2) return null;
