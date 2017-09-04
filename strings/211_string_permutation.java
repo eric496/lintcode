58633ed9247dea37c9038657aed8a3fa9032a4cc
eric496 <eric.mlengineer@gmail.com>
2017-09-04 16:44:36 -0400

    add thought process 211

--- strings/211_string_permutation.java
+++ strings/211_string_permutation.java
@@ -5,14 +5,23 @@ Example
 abcd is a permutation of bcad, but abbe is not a permutation of abe
 */
 
+/*
+Thought process:
+    1. Same to 158
+*/
+
+// O(n) time and O(1) space - with an extra array
 public class Solution {
     public boolean Permutation(String A, String B) {
-        int[] counter = new int[200];
-        for (int i = 0; i < A.length(); i++) counter[A.charAt(i)]++;
-        for (int i = 0; i < B.length(); i++) counter[B.charAt(i)]--;
-        for (int cnt: counter) {
-            if (cnt != 0) return false;
+        if(A == null && B == null) return true;
+        if(A == null || B == null) return false;
+        if(A.length() != B.length()) return false;
+        int[] count = new int[256];
+        for (int i = 0; i < A.length(); i++) {
+            count[A.charAt(i)]++;
+            count[B.charAt(i)]--;
         }
+        for (int n : count) if (n != 0) return false;
         return true;
     }
 }
