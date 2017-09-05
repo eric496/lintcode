80b36b74cc06f815cecedcd14e6dc584ea09d0a4
eric496 <eric.mlengineer@gmail.com>
2017-09-05 13:57:40 -0400

    add thought process 646

--- strings/638_string_homomorphism.java
+++ strings/638_string_homomorphism.java
@@ -4,12 +4,17 @@ Two strings are isomorphic if the characters in s can be replaced to get t.
 All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
 
 Notice
-You may assume both s and t have the same length.
+    You may assume both s and t have the same length.
 
 Example
-Given s = "egg", t = "add", return true.
-Given s = "foo", t = "bar", return false.
-Given s = "paper", t = "title", return true.
+    Given s = "egg", t = "add", return true.
+    Given s = "foo", t = "bar", return false.
+    Given s = "paper", t = "title", return true.
+*/
+
+/*
+Thought process:
+
 */
 
 public class Solution {
--- strings/646_first_position_unique_character.java
+++ strings/646_first_position_unique_character.java
@@ -6,13 +6,21 @@ Given s = "lintcode", return 0.
 Given s = "lovelintcode", return 2.
 */
 
+/*
+Thought process:
+    1. If all characters are in ASCII set, we can do two passes: first one to populate
+       the array with count of appearance for each character in the string. Second loop 
+       to find the first non-repeating character, if its count == 1. 
+    2. If the problem applies to unicode, we should use hash map instead.
+*/
+
+// O(n) time and O(1) space - with fixed size array
 public class Solution {
     public int firstUniqChar(String s) {
-        // size of ASCII character set is 256
-        int[] ch = new int[256];
-        for(int i = 0; i < s.length(); i++) ch[s.charAt(i)]++;
-        for(int i = 0; i < s.length(); i++) if(ch[s.charAt(i)] == 1) return i;
+        int[] count = new int[256];
+        for(int i = 0; i < s.length(); i++) count[s.charAt(i)]++;
+        for(int i = 0; i < s.length(); i++) if(count[s.charAt(i)] == 1) return i;
         return -1;
-        }
+    }
 }
 
