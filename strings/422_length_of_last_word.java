b4302965ac3c7d359cb5cb15e78bf8d2432f573a
eric496 <eric.mlengineer@gmail.com>
2017-09-04 23:11:55 -0400

    add thought process 422

--- strings/422_length_of_last_word.java
+++ strings/422_length_of_last_word.java
@@ -9,6 +9,32 @@ Example
 Given s = "Hello World", return 5.
 */
 
+/*
+Thought process:
+    1. Use trim method to remove right padding and then loop through the string backward. 
+       Return length once a space is found.
+    2. Append an extra space to the string. Check both current element and the element to right are spaces.
+       If so, continue, if not return the length.
+*/
+
+// use string trim method
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s.length() == 0) return 0; 
+        String strim = s.trim();
+        int len = 0;
+        for(int i = strim.length() - 1; i >= 0; i--) {
+            if(strim.charAt(i) != ' ') {
+                len++;
+            } else {
+                break;
+            }
+        }
+        return len;
+    }
+}
+
+// append an extra space at the end of the string
 public class Solution {
     public int lengthOfLastWord(String s) {
         if(s.length() == 0) return 0; 
