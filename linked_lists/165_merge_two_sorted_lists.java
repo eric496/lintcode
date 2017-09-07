c4c79c723cfac27d6bb2b104d8b1339bfec86bc2
eric496 <eric.mlengineer@gmail.com>
2017-09-06 22:44:44 -0400

    add thought process 165

--- linked_lists/165_merge_two_sorted_lists.java
+++ linked_lists/165_merge_two_sorted_lists.java
@@ -2,7 +2,7 @@
 Merge two sorted (ascending) linked lists and return it as a new sorted list. The new sorted list should be made by splicing together the nodes of the two lists and sorted in ascending order.
 
 Example
-Given 1->3->8->11->15->null, 2->null , return 1->2->3->8->11->15->null.
+    Given 1->3->8->11->15->null, 2->null , return 1->2->3->8->11->15->null.
 */
 
 /**
@@ -16,29 +16,32 @@ Given 1->3->8->11->15->null, 2->null , return 1->2->3->8->11->15->null.
  *     }
  * }
  */
- 
+
+/*
+Thought process:
+    1. Create two head pointers, one pointing to the head node of the array, 
+       one as a runner to loop through two linked lists.
+       Check if two lists reach their end, if not, merge the rest of the list.
+*/
+
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
-        ListNode dummyhead = new ListNode(Integer.MIN_VALUE);
-        ListNode head = dummyhead;
+        ListNode runner = new ListNode(Integer.MIN_VALUE);
+        ListNode head = runner;
         while(l1 != null && l2 != null) {
             if(l1.val < l2.val) {
-                head.next = l1;
+                runner.next = l1;
                 l1 = l1.next;
             } else {
-                head.next = l2;
+                runner.next = l2;
                 l2 = l2.next;
             }
-            head = head.next;
+            runner = runner.next;
         }
-        if(l1 != null) {
-            head.next = l1;
-        } else {
-            head.next = l2;
-        }
-        return dummyhead.next;
+        if(l1 != null) runner.next = l1;
+        if(l2 != null) runner.next = l2;
+        return head.next;
     }
-}
-
+} 
