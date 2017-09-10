562bf5e3d4fb5992dafab67963c5aff245c56268
eric496 <eric.mlengineer@gmail.com>
2017-09-10 16:06:59 -0400

    add thought process 245

--- trees/245_subtree.java
+++ trees/245_subtree.java
@@ -1,22 +1,23 @@
 /*
-You have two every large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes. Create an algorithm to decide if T2 is a subtree of T1.
+You have two very large binary trees: T1, with millions of nodes, and T2, with hundreds of nodes. 
+Create an algorithm to decide if T2 is a subtree of T1.
 
 Notice
-A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2. That is, if you cut off the tree at node n, the two trees would be identical.
+    A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2. That is, if you cut off the tree at node n, the two trees would be identical.
 
 Example
-T2 is a subtree of T1 in the following case:
-       1                3
-      / \              / 
-T1 = 2   3      T2 =  4
-        /
-       4
-T2 isn't a subtree of T1 in the following case:
-       1               3
-      / \               \
-T1 = 2   3       T2 =    4
-        /
-       4
+    T2 is a subtree of T1 in the following case:
+           1                3
+          / \              / 
+    T1 = 2   3      T2 =  4
+            /
+           4
+    T2 isn't a subtree of T1 in the following case:
+           1               3
+          / \               \
+    T1 = 2   3       T2 =    4
+            /
+           4
 */
 
 /**
@@ -30,7 +31,12 @@ T1 = 2   3       T2 =    4
  *     }
  * }
  */
- 
+
+/*
+Thought process:
+    1. Recursive solution
+*/
+
 public class Solution {
     public boolean isSubtree(TreeNode T1, TreeNode T2) {
         if (T2 == null) return true;
