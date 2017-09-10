54f97b07ac8d93d326b8c36054c2486dadc332fd
eric496 <eric.mlengineer@gmail.com>
2017-09-09 22:22:03 -0400

    add recursive solution to 85

--- trees/85_insert_node_in_binary_search_tree.java
+++ trees/85_insert_node_in_binary_search_tree.java
@@ -2,15 +2,15 @@
 Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
 
 Notice
-You can assume there is no duplicate values in this tree + node.
+    You can assume there is no duplicate values in this tree + node.
 
 Example
-Given binary search tree as follow, after Insert node 6, the tree should be:
-  2             2
- / \           / \
-1   4   -->   1   4
-   /             / \ 
-  3             3   6
+    Given binary search tree as follow, after Insert node 6, the tree should be:
+      2             2
+     / \           / \
+    1   4   -->   1   4
+       /             / \ 
+      3             3   6
 */
 
 /**
@@ -24,17 +24,45 @@ Given binary search tree as follow, after Insert node 6, the tree should be:
  *     }
  * }
  */
+
+/*
+Thought process:
+    1. Recursive solution: if greater than value of the root, then recursely insert into the right subtree. Ohterwise, the left.
+    2. Iterative solution: create a runner and loop through the tree to insert the node. 
+*/
  
+// recursive solution
 public class Solution {
     public TreeNode insertNode(TreeNode root, TreeNode node) {
         if(root == null) return node;
-        
         if(root.val < node.val) {
             root.right = insertNode(root.right, node);
         } else {
             root.left = insertNode(root.left, node);
         }
-        
+        return root;
+    }
+}
+
+// iterative solution
+public class Solution {
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        if(node == null) return root;
+        if(root == null) return node;
+        TreeNode runner = root;
+        while(runner != null) {
+            if(node.val >= runner.val && runner.right == null) {
+                runner.right = node;
+                break;
+            } else if(node.val < runner.val && runner.left == null) {
+                runner.left = node;
+                break;
+            } else if(node.val >= runner.val) {
+                runner = runner.right;
+            } else if(node.val < runner.val) {
+                runner = runner.left;
+            }
+        }
         return root;
     }
 }
