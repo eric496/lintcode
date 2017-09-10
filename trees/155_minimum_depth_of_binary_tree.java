695d44943304322b9f8ba710dcf06b0129130617
eric496 <eric.mlengineer@gmail.com>
2017-09-10 15:59:25 -0400

    add thought process 155

--- trees/155_minimum_depth_of_binary_tree.java
+++ trees/155_minimum_depth_of_binary_tree.java
@@ -3,13 +3,13 @@ Given a binary tree, find its minimum depth.
 The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 
 Example
-Given a binary tree as follow:
-  1
- / \ 
-2   3
-   / \
-  4   5
-The minimum depth is 2.
+    Given a binary tree as follow:
+      1
+     / \ 
+    2   3
+       / \
+      4   5
+    The minimum depth is 2.
 */
 
 /**
@@ -24,13 +24,18 @@ The minimum depth is 2.
  * }
  */
  
+/*
+Thought process:
+    1. Recursive solution: check if left node is empty. If so, recursively loop the right child. 
+       Check if the right node is empty. If so, recursively loop the left child.
+       If both children nodes are non empty, recursively loop left and right children and get the min.
+*/
+
 public class Solution {
-    public int minDepth(TreeNode root) {
-        if(root == null) return 0;
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
-        if(root.left == null) return right + 1;
-        if(root.right == null) return left + 1;
-        return Math.min(left, right) + 1;
+    public static int minDepth(TreeNode root) {
+        if (root == null)   return 0;
+        if (root.left == null)  return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 }
