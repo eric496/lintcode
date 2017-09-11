f99937275531334e9437a8f3d5a9ffc766a6099d
eric496 <eric.mlengineer@gmail.com>
2017-09-10 20:17:35 -0400

    add 661

--- trees/661_convert_BST_to_greater_tree.java
+++ trees/661_convert_BST_to_greater_tree.java
@@ -2,14 +2,11 @@
 Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
 
 Example
-Given a binary search Tree ｀{5,2,13}｀:
-
+    Given a binary search Tree {5,2,13}:
               5
             /   \
            2     13
-
-Return the root of new tree
-
+    Return the root of new tree
              18
             /   \
           20     13
