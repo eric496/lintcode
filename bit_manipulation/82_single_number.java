1134ecc74e9b69942b8a32e318aa7b6b05f627be
eric496 <eric.mlengineer@gmail.com>
2017-09-13 11:54:13 -0400

    add thought process 82

--- bit_manipulation/82_single_number.java
+++ bit_manipulation/82_single_number.java
@@ -2,11 +2,19 @@
 Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
 
 Example
-Given [1,2,2,1,3,4,3], return 4
+    Given [1,2,2,1,3,4,3], return 4
 
-Challenge: One-pass, constant extra space.
+Challenge
+    One-pass, constant extra space.
 */
 
+/*
+Thought process:
+    1. A ^ A is 0, and XOR operation follows communicative law, e.g. A ^ B ^ A = A ^ A ^ B = B.
+*/
+
+
+// one-pass, O(n) time and O(1) space
 public class Solution {
     public int singleNumber(int[] A) {
         if(A == null || A.length == 0) return 0;
