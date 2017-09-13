c496a1b8cdefe1688e5f963da11e69c49deebe58
eric496 <eric.mlengineer@gmail.com>
2017-09-13 01:47:11 -0400

    change 1 format

--- bit_manipulation/1_a_plus_b_problem.java
+++ bit_manipulation/1_a_plus_b_problem.java
@@ -2,16 +2,18 @@
 Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
 
 Notice
-There is no need to read data from standard input stream. Both parameters are given in function aplusb, you job is to calculate the sum and return it.
-
+    There is no need to read data from standard input stream. 
+    Both parameters are given in function aplusb, 
+    you job is to calculate the sum and return it.
+ 
 Clarification
-Are a and b both 32-bit integers?
+    Are a and b both 32-bit integers?
     Yes.
-Can I use bit operation?
+    Can I use bit operation?
     Sure you can.
 
 Example
-Given a=1 and b=2 return 3
+    Given a=1 and b=2 return 3
 */
 
 class Solution {
