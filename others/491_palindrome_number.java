9f6840131e538ac10d8ff57ac9f672ffe7e75a4d
eric496 <eric.mlengineer@gmail.com>
2017-09-13 22:50:59 -0400

    add thought process 491

--- others/491_palindrome_number.java
+++ others/491_palindrome_number.java
@@ -10,10 +10,33 @@ Example
     23, 32, 1232 are not palindrome numbers.
 */
 
+/*
+Thought process:
+    1. Convert to string, then it becomes similar to 415. Valid Palindrome
+    2. If conversion is not allowed, then the solution is similar to 
+*/
+
+// convert to string and check if it is a valid palindrome
 public class Solution {
     public boolean isPalindrome(int num) {
         String s = String.valueOf(num);
         for(int i = 0, j = s.length()-1; i < j; i++, j--) if(s.charAt(i) != s.charAt(j)) return false;
         return true;
+    }
+}
+
+// The method to flip a number: take the remainder of n divided by 10 plus result multiplied by 10 to update result until n reaches 0. Update n by n/10 as well.
+public class Solution {
+    public boolean isPalindrome(int num) {
+        if(num < 0) return false;
+        if(num == 0) return true;
+        int result = 0;
+        int n = num; 
+        while(n != 0) {
+            result = n % 10 + result * 10;
+            n /= 10;
+        }
+        if(result == num) return true;
+        return false;
     }
 }
