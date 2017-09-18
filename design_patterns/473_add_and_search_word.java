f7d3a319e2c1687d60eb2696c2cdd7b3cbac8bf8
eric496 <eric.mlengineer@gmail.com>
2017-09-18 18:07:33 -0400

    add 473

---
+++ design_patterns/473_add_and_search_word.java
@@ -0,0 +1,58 @@
+/*
+Design a data structure that supports the following two operations: addWord(word) and search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or ..
+A . means it can represent any one letter.
+
+Notice
+    You may assume that all words are consist of lowercase letters a-z.
+
+Example
+    addWord("bad")
+    addWord("dad")
+    addWord("mad")
+    search("pad")  // return false
+    search("bad")  // return true
+    search(".ad")  // return true
+    search("b..")  // return true
+*/
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
+
+public class WordDictionary {
+    public class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public String item = "";
+    }
+
+    private TrieNode root = new TrieNode();
+
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) node.children[c - 'a'] = new TrieNode();
+            node = node.children[c - 'a'];
+        }
+        node.item = word;
+    }
+
+    public boolean search(String word) {
+        return match(word.toCharArray(), 0, root);
+    }
+
+    private boolean match(char[] chs, int k, TrieNode node) {
+        if (k == chs.length) return !node.item.equals("");   
+        if (chs[k] != '.') {
+            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
+        } else {
+            for (int i = 0; i < node.children.length; i++) {
+                if (node.children[i] != null) {
+                    if (match(chs, k + 1, node.children[i])) return true;
+                }
+            }
+        }
+        return false;
+    }
+}
