73f9b703f58fe4cc27b19cd7c6193eca3432c3ab
eric496 <eric.mlengineer@gmail.com>
2017-09-18 18:16:59 -0400

    add 150

---
+++ arrays/150_best_time_to_buy_and_sell_stock.java
@@ -0,0 +1,18 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Example
+    Given an example [2,1,2,0,1], return 2
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) return 0;
+        int profit = 0;
+        for(int i = 0; i < prices.length - 1; i++) {
+            if(prices[i+1] > prices[i]) profit += prices[i+1] - prices[i]; 
+        }
+        return profit;
+    }
+}
