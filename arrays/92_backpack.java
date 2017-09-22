1c90d147e3141e082e0fc7e3c8d1b5f3d82c4942
eric496 <eric.mlengineer@gmail.com>
2017-09-22 17:53:35 -0400

    add thought process 92

--- arrays/92_backpack.java
+++ arrays/92_backpack.java
@@ -13,19 +13,32 @@ Challenge
     O(n x m) memory is also acceptable if you do not know how to optimize memory.
 */
 
+/*
+Thought process:
+    1. DP solution: 2-D array with each column representing backpack size from 0 - m
+       each row represent item size. 
+    2. Compare current item size plus the max previous size (backpack size - current item size) with 
+       that excluding the current item. Keep the larger one as the current element value in the 2-D array.
+    3. Keep doing this until loop through all rows and columns, then the last element is the largest 
+       size we can fulfill the backpack.
+*/
+
 public class Solution {
     public int backPack(int m, int[] A) {
         int row = A.length;
         int col = m + 1;
         int[][] dp = new int[row][col];
-        for(int i = 0; i < row; i++) dp[i][0] = 0;
+        
+        for(int i = 0; i < col; i++) {
+            if(A[0] <= i) {
+                dp[0][i] = A[0];
+            } else {
+                dp[0][i] = 0;
+            }
+        }
+        
         for(int i = 1; i < row; i++) {
             for(int j = 1; j < col; j++) {
-                if(A[0] <= j) {
-                    dp[0][j] = A[0];
-                } else {
-                    dp[0][j] = 0;
-                }
                 if(A[i] > j) {
                     dp[i][j] = dp[i-1][j];
                 } else {
