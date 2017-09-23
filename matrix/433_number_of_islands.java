4e0c4c742b04e24bc35e9e8ae196806243ac94b0
eric496 <eric.mlengineer@gmail.com>
2017-09-23 17:13:07 -0400

    change description format 514

--- matrix/433_number_of_islands.java
+++ matrix/433_number_of_islands.java
@@ -1,5 +1,6 @@
 /*
-Given a boolean 2D matrix, 0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
+Given a boolean 2D matrix, 0 is represented as the sea, 1 is represented as the island. 
+If two 1 is adjacent, we consider them in the same island. We only consider up/down/left/right adjacent.
 Find the number of islands.
 
 Example
--- matrix/514_paint_fence.java
+++ matrix/514_paint_fence.java
@@ -19,14 +19,12 @@ Example
 
 public class Solution {
     public int numWays(int n, int k) {
-  	if (n <= 1) return n * k;
-  	int[] dp = new int[n + 1];
-  	dp[0] = 0;
-  	dp[1] = k;
-  	dp[2] = k + k*(k - 1);
-  	for (int i = 3; i <= n; i++) {
-  		dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2]);
-  	}
-  	return dp[n];
+  	    if (n <= 1) return n * k;
+  	    int[] dp = new int[n + 1];
+  	    dp[0] = 0;
+  	    dp[1] = k;
+    	dp[2] = k + k*(k - 1);
+  	    for (int i = 3; i <= n; i++) dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2]);
+  	    return dp[n];
     }
 }
