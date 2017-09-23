864e72c2ec9d08440a91cd072225a8e5f90fb127
eric496 <eric.mlengineer@gmail.com>
2017-09-23 16:56:05 -0400

    add thought process 110

--- matrix/110_minimum_path_sum.java
+++ matrix/110_minimum_path_sum.java
@@ -5,18 +5,34 @@ Notice
     You can only move either down or right at any point in time.
 */
 
+/*
+Thought process:
+    1. Create a 2-D array to keep track of the current min sum.
+    2. Compare sum of current grid item and its min sum from left
+       with sum of the current grid item and its min sum from up.
+       Keep the min sum as the current min sum. 
+    3. Remember to handle the first row and the first column
+       as they have only one option to sum (for the first row, 
+       it can only sum its left items, for the first column, it 
+       can only sum its up items.) 
+*/
+
 public class Solution {
     public int minPathSum(int[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
         int m = grid.length;
         int n = grid[0].length;
+        if(grid == null || m == 0 || n == 0) return -1;
         int[][] sum = new int[m][n];
         sum[0][0] = grid[0][0];
-        for(int i = 1; i < m; i++) sum[i][0] = sum[i-1][0] + grid[i][0];
-        for(int i = 1; i < n; i++) sum[0][i] = sum[0][i-1] + grid[0][i];
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                sum[i][j] = Math.min(sum[i-1][j], sum[i][j-1]) + grid[i][j];
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                if(row == 0 && col != 0) {
+                    sum[row][col] = sum[row][col-1] + grid[row][col];
+                } else if(col == 0 && row != 0) {
+                    sum[row][col] = sum[row-1][col] + grid[row][col];
+                } else if(row != 0 && col != 0) {
+                    sum[row][col] = Math.min(sum[row-1][col], sum[row][col-1]) + grid[row][col];
+                }
             }
         }
         return sum[m-1][n-1];
