fb3929e0b1b82858c47412fbd5013922136b426a
eric496 <eric.mlengineer@gmail.com>
2017-09-24 23:29:42 -0400

    add thought process 149

--- arrays/149_best_time_to_buy_and_sell_stock.java
+++ arrays/149_best_time_to_buy_and_sell_stock.java
@@ -4,17 +4,28 @@ If you were only permitted to complete at most one transaction (ie, buy one and 
 
 Example
     Given array [3,2,3,1,2], return 1.
+
+
+*/
+
+/*
+Thought process:
+   1. Create a min so far and a global max variables. 
+      If current price is greater than min so far, then compare update global max with the larger value of global max and current price minus min so far.
+      Otherwise, update min so far with current price.
 */
 
 public class Solution {
     public int maxProfit(int[] prices) {
         if(prices == null || prices.length == 0) return 0;
-        int min = Integer.MAX_VALUE;
-        int profit = 0;
-        for(int i : prices) {
-            min = i < min ? i : min;
-            profit = (i - min) > profit ? i - min : profit;
+        int sofarMin = prices[0], max = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] > sofarMin) {
+                max = Math.max(max, prices[i] - sofarMin);
+            } else {
+                sofarMin = prices[i];
+            }
         }
-        return profit;
+        return max;
     }
 }
