225df0dc44ad6da6f8f3991a0ef041ec19c84e32
eric496 <eric.mlengineer@gmail.com>
2017-10-01 15:11:04 -0400

    add 378 description

---
+++ strings/378_convert_palindrome.java
@@ -0,0 +1,9 @@
+/*
+Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and return the shortest palindrome you can find by performing this transformation.
+
+Example
+    Given "aacecaaa", return "aaacecaaa".
+    Given "abcd", return "dcbabcd".
+*/
+
+
