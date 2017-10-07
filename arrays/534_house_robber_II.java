0521c34e5aadf9e873c34e1f0c89d453354a6ad1
eric496 <eric.mlengineer@gmail.com>
2017-10-07 15:55:16 -0400

    add 534

---
+++ arrays/534_house_robber_II.java
@@ -0,0 +1,27 @@
+/*
+After robbing those houses on that street, the thief has found himself a new place for his thievery so that he will not get too much attention. This time, all houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, the security system for these houses remain the same as for those in the previous street.
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+Notice
+    This is an extension of House Robber.
+
+Example
+    nums = [3,6,4], return 6
+*/
+
+public class Solution {
+    private int rob(int[] num, int low, int high) {
+        int include = 0, exclude = 0;
+        for (int j = low; j <= high; j++) {
+            int i = include, e = exclude;
+            include = e + num[j];
+            exclude = Math.max(e, i);
+        }
+        return Math.max(include, exclude);
+    }
+    
+    public int houseRobber2(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+}
