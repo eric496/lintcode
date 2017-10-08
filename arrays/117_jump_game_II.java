d2ccdd82ed481a86508dd57ead52637168bc2746
eric496 <eric.mlengineer@gmail.com>
2017-10-08 18:22:44 -0400

    add 117

---
+++ arrays/117_jump_game_II.java
@@ -0,0 +1,25 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Your goal is to reach the last index in the minimum number of jumps.
+
+Example
+    Given array A = [2,3,1,1,4]
+    The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+*/
+
+// greedy
+public class Solution {
+    public int jump(int[] A) {
+	int jumps = 0, curEnd = 0, curFarthest = 0;
+	for (int i = 0; i < A.length - 1; i++) {
+	    curFarthest = Math.max(curFarthest, i + A[i]);
+	    if (i == curEnd) {
+		jumps++;
+		curEnd = curFarthest;
+		if (curEnd >= A.length - 1) break;
+	    }
+	}
+        return jumps;
+    }
+}
