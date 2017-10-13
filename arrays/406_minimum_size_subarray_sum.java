39e2dc50de49429784677091d9fd7845918990da
eric496 <eric.mlengineer@gmail.com>
2017-10-13 14:35:09 -0400

    add 406

---
+++ arrays/406_minimum_size_subarray_sum.java
@@ -0,0 +1,27 @@
+/*
+Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return -1 instead.
+
+Example
+    Given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal length under the problem constraint.
+
+Challenge
+    If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+*/
+
+// O(n) time by two pointers
+public class Solution {
+    public int minimumSize(int[] nums, int s) {
+        if (nums == null || nums.length == 0) return -1;
+        int sum = 0, from = 0, min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                min = Math.min(min, i - from + 1);
+                sum -= nums[from++];
+            }
+        }
+        return (min == Integer.MAX_VALUE) ? -1 : min;
+    }
+}
+
+// O(nlogn) time
