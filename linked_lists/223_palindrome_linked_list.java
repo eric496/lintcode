51fa8b34667dc1a84e36dbd3e0b83000cd0d3128
eric496 <eric.mlengineer@gmail.com>
2017-10-13 16:49:43 -0400

    add 223

---
+++ linked_lists/223_palindrome_linked_list.java
@@ -0,0 +1,51 @@
+/*
+Implement a function to check if a linked list is a palindrome.
+
+Example
+    Given 1->2->1, return true
+
+Challenge
+    Could you do it in O(n) time and O(1) space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null) return true;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast != null) // odd nodes: let right half smaller
+            slow = slow.next;
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val) return false;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+    
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+
