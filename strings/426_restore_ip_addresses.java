03ec3a50267f93401b865c3a5b5b9f11a23f80f9
eric496 <eric.mlengineer@gmail.com>
2017-10-15 14:33:50 -0400

    add 4263

---
+++ strings/426_restore_ip_addresses.java
@@ -0,0 +1,31 @@
+/*
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+Example
+    Given "25525511135", return
+    [
+      "255.255.11.135",
+      "255.255.111.35"
+    ]
+    Order does not matter.
+*/
+
+// DFS
+public class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> solutions = new ArrayList<String>();
+        restoreIp(s, solutions, 0, "", 0);
+        return solutions;
+    }
+    
+    private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4) return;
+        if (count == 4 && idx == ip.length()) solutions.add(restored);
+        for (int i=1; i<4; i++) {
+            if (idx+i > ip.length()) break;
+            String s = ip.substring(idx,idx+i);
+            if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+            restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        }
+    }
+}
