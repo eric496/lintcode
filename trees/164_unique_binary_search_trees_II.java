c971baaeb2080fdc0aabcf2faa5ee2daef2246d7
eric496 <eric.mlengineer@gmail.com>
2017-10-17 17:24:09 -0400

    add 164

---
+++ trees/164_unique_binary_search_trees_II.java
@@ -0,0 +1,54 @@
+/*
+Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+Example
+    Given n = 3, your program should return all 5 unique BST's shown below.
+       1         3     3      2      1
+        \       /     /      / \      \
+         3     2     1      1   3      2
+        /     /       \                 \
+       2     1         2                 3
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return genTrees(1,n);
+    }
+        
+    public List<TreeNode> genTrees (int start, int end) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if(start>end) {
+            list.add(null);
+            return list;
+        }
+        if(start == end) {
+            list.add(new TreeNode(start));
+            return list;
+        }
+        List<TreeNode> left, right;
+        for(int i = start; i <= end; i++) {
+            left = genTrees(start, i-1);
+            right = genTrees(i+1, end);
+            for(TreeNode lnode: left) {
+                for(TreeNode rnode: right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lnode;
+                    root.right = rnode;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
