a9dc9206beedc55eba2b3d405ae9ff26d666f35a
eric496 <eric.mlengineer@gmail.com>
2017-10-16 23:36:29 -0400

    add 381

---
+++ matrix/381_spiral_matrix_II.java
@@ -0,0 +1,45 @@
+/*
+Given an integer n, generate a square matrix filled with elements from 1 to n^2 in spiral order.
+
+Example
+    Given n = 3,
+    You should return the following matrix:
+    [
+      [ 1, 2, 3 ],
+      [ 8, 9, 4 ],
+      [ 7, 6, 5 ]
+    ]
+*/
+
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int cur = 1;
+        int rowBegin = 0;
+        int rowEnd = n - 1;
+        int colBegin = 0;
+        int colEnd = n - 1;
+        
+        while(cur <= n * n) {
+            int i = rowBegin;
+            int j = colBegin;
+            //left to right
+            for(j = colBegin ; j <= colEnd; j++)
+                res[rowBegin][j] = cur++;
+            rowBegin++;
+            //top to bot
+            for(i = rowBegin ; i <= rowEnd; i++)
+                res[i][colEnd] = cur++;
+            colEnd--;
+            //right to left
+            for(j = colEnd ; j >= colBegin; j--)
+                res[rowEnd][j] = cur++;
+            rowEnd--;
+            //bot to top
+            for(i = rowEnd; i >= rowBegin; i--)
+                res[i][colBegin] = cur++;
+            colBegin++;
+        }
+        return res;
+    }
+}
