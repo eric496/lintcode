31d7bfd9aa2f35917e397e89f70502b6dd6744e0
eric496 <eric.mlengineer@gmail.com>
2017-10-18 17:02:22 -0400

    add 632 thought

--- trees/632_binary_tree_maximum_node.java
+++ trees/632_binary_tree_maximum_node.java
@@ -11,10 +11,20 @@ Example
     return the node with value 3.
 */
 
-// in-order traversal
+/*
+Thought process:
+    In order to find the maximum node, we need to traverse the binary tree.
+    Tree traversal can be done recursively in a bottom-up order. 
+    Base case: return null if current node is null.
+    Recursive steps: Traverse left subtree and then the right. 
+                     If left child value is greater than root value, then set left child as root. 
+                     Similarly, if right child value is greater than root value, then set right child as root.
+                     Root node is always the current maximum node. 
+*/
+
 public class Solution {
     public TreeNode maxNode(TreeNode root) {
-        if (root == null) return null;
+        if(root == null) return null;
         TreeNode left = maxNode(root.left);
         TreeNode right = maxNode(root.right);
         if(left != null && left.val > root.val) root = left;
