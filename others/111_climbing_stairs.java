10223df2c161472805e7514910bbe3d9816a9675
eric496 <eric.mlengineer@gmail.com>
2017-10-18 17:55:58 -0400

    add thought

--- others/111_climbing_stairs.java
+++ others/111_climbing_stairs.java
@@ -10,16 +10,17 @@ Example
 /*
 Thought process:
     1. Recursive solution
-    2. Iterative solution
-    3. DP - similar to fibonacci sequence, 
-       1,1,2,3,5, ...
+           Base case: return 0 when n equals 0; return 1 when n equals 1; return 2 when n equals 2.
+           Recursive steps: distinct ways of climbing to n is the sum of distinct ways of climbing to n-1 and n-2, because 
+                            we can climb 1 or 2 steps each time. These two paths do not overlap.
+    2. Iterative solution: Refer to 366. Fibonacci
+    3. DP: bottom-up method
 */
 
 // recursive
 public class Solution {
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        if(n <= 2) return n;
         return climbStairs(n-1) + climbStairs(n-2);
     }
 }
@@ -27,11 +28,9 @@ public class Solution {
 // iterative
 public class Solution {
     public int climbStairs(int n) {
-        if(n <= 1) return 1;
-        int prevPrev = 1;
-        int prev = 1;
-        int cur = 0;
-        for(int i = 2; i <= n; i++) {
+        if(n <= 2) return n;
+        int prevPrev = 1, prev = 2, cur = prevPrev + prev;
+        for(int i = 3; i <= n; i++) {
             cur = prevPrev + prev;
             prevPrev = prev;
             prev = cur;
@@ -40,14 +39,14 @@ public class Solution {
     }
 }
 
-// DP
+// DP - bottom up
 public class Solution {
     public int climbStairs(int n) {
         if(n <= 2) return n;
-        int[] res = new int[n+1];
-        res[0] = 1;
-        res[1] = 1;
-        for(int i = 2; i <= n; i++) res[i] = res[i-1] + res[i-2];
-        return res[n];
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 }
