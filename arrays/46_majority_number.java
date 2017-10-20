ad16183225665fe8bdc0cfcaf5f92ecc37122188
eric496 <eric.mlengineer@gmail.com>
2017-10-20 19:24:54 -0400

    add comment

--- arrays/46_majority_number.java
+++ arrays/46_majority_number.java
@@ -2,17 +2,20 @@
 Given an array of integers, the majority number is the number that occurs more than half of the size of the array. Find it.
 
 Notice
-You may assume that the array is non-empty and the majority number always exist in the array.
+    You may assume that the array is non-empty and the majority number always exist in the array.
 
 Example
-Given [1, 1, 1, 1, 2, 2, 2], return 1
+    Given [1, 1, 1, 1, 2, 2, 2], return 1
+
+Challenge
+    O(n) time and O(1) extra space
 */
 
 /*
 Thought process:
-    1. O(n) time and O(n) space solution: use hash map to store <number, count> pairs.
-       Keep track of the max count in the for loop to avoid two passes. 
-    2. O(n) time and O(1) space solution: Boyer-Moore majority vote algorithm: (from Wikipedia: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm)
+    1. Intuitive solution would use hash map to store <number, count> pairs.
+       Keep track of the max count in the iteration to avoid two passes. 
+    2. Boyer-Moore majority vote algorithm: (https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm)
        The algorithm maintains in its local variables a sequence element and a counter, with the counter initially zero. 
        It then processes the elements of the sequence, one at a time. When processing an element x, if the counter is zero, 
        the algorithm stores x as its remembered sequence element and sets the counter to one. 
@@ -20,7 +23,7 @@ Thought process:
        At the end of this process, if the sequence has a majority, it will be the element stored by the algorithm.
 */
 
-//O(n) time and O(n) space via hash map
+//O(n) time and O(n) space using hash map
 public class Solution {
     public int majorityNumber(ArrayList<Integer> nums) {
         HashMap<Integer, Integer> res = new HashMap<Integer, Integer>();
@@ -40,7 +43,7 @@ public class Solution {
     }
 }
 
-//O(n) time and O(1) space
+//O(n) time and O(1) space - Boyer-Moore majority vote algorithm
 public class Solution {
     public int majorityNumber(ArrayList<Integer> nums) {
         int majority = Integer.MIN_VALUE, count = 0;
