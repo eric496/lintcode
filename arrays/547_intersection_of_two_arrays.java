b632eb1ea7eacc60388a60e188d1715516760c8f
eric496 <eric.mlengineer@gmail.com>
2017-10-20 12:25:10 -0400

    add 2 new solutions

--- arrays/547_intersection_of_two_arrays.java
+++ arrays/547_intersection_of_two_arrays.java
@@ -6,28 +6,85 @@ Notice
     The result can be in any order.
 
 Example
-Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+    Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Challenge
+    Can you implement it in three different algorithms?
 */
 
 /*
 Thought process:
-    1. Use two hash sets. Note that use hash set to store unique values as well
-       because this allows to automatically remove duplicates added to the set.
+    1. Use two sets. One pass stores all unique values from the first array in the first set.
+       The second pass stores all intersection values of both arrays in the second set.
+       Third pass copy values from set to array.
+    2. Sort the arrays first, then use two pointers to find all intersection values. 
+    3. Sort one of the arrays, then use binary search to find intersection values.
 */
 
-//O(n) time and O(n) space
+// O(n) time and O(n) space
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> hash = new HashSet<Integer>();
-        for(Integer n : nums1) hash.add(n);
-        HashSet<Integer> res = new HashSet<Integer>();
-        for(Integer n : nums2) {
-            //Actually !res.contains(n) is not necessary because it is set. Leave it here for better understanding.
-            if(hash.contains(n) && !res.contains(n)) res.add(n);
-        }
-        int[] result = new int[res.size()];
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(Integer n : nums1) set.add(n);
+        HashSet<Integer> intersect = new HashSet<Integer>();
+        for(Integer n : nums2)
+            if(set.contains(n)) intersect.add(n);
+        int[] result = new int[intersect.size()];
         int ix = 0;
-        for(Integer n : res) result[ix++] = n;
+        for(Integer n : intersect) result[ix++] = n;
         return result;
+    }
+}
+
+// O(nlogn) time and O(n) space 
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        HashSet<Integer> set = new HashSet<Integer>();
+        int ix1 = 0, ix2 = 0;
+        while(ix1 < nums1.length && ix2 < nums2.length) {
+            if(nums1[ix1] < nums2[ix2]) {
+                ix1++;
+            } else if(nums1[ix1] > nums2[ix2]) {
+                ix2++;
+            } else { 
+                set.add(nums1[ix1]);
+                ix1++;
+                ix2++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int ix = 0;
+        for(int n : set) result[ix++] = n;
+        return result;
+    }
+}
+
+// O(nlogn) time and O(n) space
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int n : nums2)
+            if(binarySearch(nums1, n)) set.add(n);
+        int[] result = new int[set.size()];
+        int ix = 0;
+        for(int n : set) result[ix++] = n;
+        return result;
+    }
+    
+    private boolean binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+                return true;
+            else if(nums[mid] > target) 
+                high = mid - 1;
+            else if(nums[mid] < target)
+                low = mid + 1;
+        }
+        return false;
     }
 }
