1d42b6b96cae33d1060fae44fd61407d5686c7e4
eric496 <eric.mlengineer@gmail.com>
2017-10-20 19:12:31 -0400

    add comment

--- arrays/133_longest_words.java
+++ arrays/133_longest_words.java
@@ -19,34 +19,38 @@ Example
       "yes"
     }
     the longest words are ["like", "love", "hate"].
+
+Challenge
+    It's easy to solve it in two passes, can you do it in one pass?
 */
 
 /*
 Thought process
-    1. An obvious approach is two passes: first for loop to count the max length of all words; 
-       second for loop to pick out the words which are of the max length.
-    2. Don't forget the corner case when the dictionary is null or empty.
+    1. A trivial solution: two passes
+           First pass counts the max length of all words.
+           Second pass adds all strings of the max length to the result list. 
+    2. One-pass method:
+           Iterate the dictionary, if the length of the current string is greater than that of the strings in the result list, then clear the result list and add the current string.
+           If the length of the current string equals that of the strings in the result list, then append it to the result list.
 */
 
 //two passes
 class Solution {
-    ArrayList<String> longestWords(String[] dictionary) {
+    public ArrayList<String> longestWords(String[] dictionary) {
         if(dictionary == null || dictionary.length == 0) return null;
         int max = 0;
-        for(String s : dictionary) {
+        for(String s : dictionary)
             if(s.length() > max) max = s.length();
-        }
         ArrayList<String> res = new ArrayList<String>();
-        for(String s : dictionary) {
+        for(String s : dictionary)
             if(s.length() == max) res.add(s);
-        }
         return res;
     }
 }
 
 //one pass
 class Solution {
-    ArrayList<String> longestWords(String[] dictionary) {
+    public ArrayList<String> longestWords(String[] dictionary) {
         if(dictionary == null || dictionary.length == 0) return null;
         ArrayList<String> res = new ArrayList<String>();
         for(String s : dictionary) {
