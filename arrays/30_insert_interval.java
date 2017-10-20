4d2a4b7913f34a4066b814501ffdc1661cd30e7b
eric496 <eric.mlengineer@gmail.com>
2017-10-20 11:06:52 -0400

    add comment

--- arrays/30_insert_interval.java
+++ arrays/30_insert_interval.java
@@ -9,7 +9,7 @@ Insert [3, 4] into [[1,2], [5,9]], we get [[1,2], [3,4], [5,9]].
 
 /**
  * Definition of Interval:
- * public classs Interval {
+ * public class Interval {
  *     int start, end;
  *     Interval(int start, int end) {
  *         this.start = start;
@@ -19,32 +19,40 @@ Insert [3, 4] into [[1,2], [5,9]], we get [[1,2], [3,4], [5,9]].
 
 /*
 Thought process
-1. There are 3 cases: 1) new interval is strictly smaller than the current interval (newInterval.end < i.start)
-                      2) new interval is strictly greater than the current interval (newInterval.start > i.end)
-                      3) new interval is overlapping with the current interval
-2. Set a position pointer to keep track of the insertion position so we can insert interval outside the for loop.
-   It should increment only when case 2) is true.
-   For case 3), update the new interval start with the smaller value of new interval start and the current interval start, 
-   similarly, update the new interval end with the greater value of new interval end and the current interval end.
+    1. There are 3 cases: 
+           1) new interval is strictly greater than the current interval (newInterval.start > i.end)
+           2) new interval is strictly smaller than the current interval (newInterval.end < i.start)
+           3) new interval is overlapping with the current interval (newInterval.end >= i.start or newInterval.start <= i.end)
+    2. Set a position pointer to keep track of the insertion position so we can insert interval outside the for loop.
+       The position pointer increments only in case 2).
+       For case 3), update the new interval start with new interval start or current interval start, whichever is smaller. 
+       Similarly, update the new interval end with new interval end or current interval end, whichever is greater.
 */
 
-class Solution {
-    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         if(intervals == null || newInterval == null) return intervals;
-        ArrayList<Interval> res = new ArrayList<Interval>();
+        List<Interval> result = new ArrayList<Interval>();
         int pos = 0;
         for(Interval i : intervals) {
-            if(newInterval.end < i.start) {
-                res.add(i);
-            } else if(newInterval.start > i.end) {
-                res.add(i);
-		pos++;
+            // current interval is strictly smaller than new interval
+            // insert current interval to result list and increment insert position index by 1
+            if(newInterval.start > i.end) {
+                result.add(i);
+                pos++;
+            // current interval is strictly greater than new interval
+            // insert current interval to result list but do not change insert position index
+            } else if(newInterval.end < i.start) {
+                result.add(i);
+            // merge interval, take new interval start or current interval start, whichever is smaller,
+            // similarly, take new interval end or current interval end, whichever is greater.
             } else {
                 newInterval.start = Math.min(newInterval.start, i.start);
                 newInterval.end = Math.max(newInterval.end, i.end);
             }
         }
-        res.add(pos, newInterval);
-        return res;
+        // insert new interval or merged interval 
+        result.add(pos, newInterval);
+        return result;
     }
 }
