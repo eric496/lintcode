7be8942a1b65d64683939d17ccb2247764a82aae
eric496 <eric.mlengineer@gmail.com>
2017-10-20 14:07:29 -0400

    add 3rd solution

--- strings/422_length_of_last_word.java
+++ strings/422_length_of_last_word.java
@@ -3,18 +3,18 @@ Given a string s consists of upper/lower-case alphabets and empty space characte
 If the last word does not exist, return 0.
 
 Notice
-A word is defined as a character sequence consists of non-space characters only.
+    A word is defined as a character sequence consists of non-space characters only.
 
 Example
-Given s = "Hello World", return 5.
+    Given s = "Hello World", return 5.
 */
 
 /*
 Thought process:
-    1. Use trim method to remove right padding and then loop through the string backward. 
-       Return length once a space is found.
-    2. Append an extra space to the string. Check both current element and the element to right are spaces.
-       If so, continue, if not return the length.
+    1. Use trim method to remove right padding and then iterate the string from backward. Return length once a space is met.
+    2. Use trim and split method to return the length of the last word in the string array.
+    3. Two passes: First pass moves the position pointer to the first non-space character.
+                   Second pass moves on until the first space is met, during which the length counter keeps updating.
 */
 
 // use string trim method
@@ -24,34 +24,34 @@ public class Solution {
         String strim = s.trim();
         int len = 0;
         for(int i = strim.length() - 1; i >= 0; i--) {
-            if(strim.charAt(i) != ' ') {
+            if(strim.charAt(i) != ' ')
                 len++;
-            } else {
+            else
                 break;
-            }
         }
         return len;
     }
 }
 
-// append an extra space at the end of the string
+// use string trim and split methods
 public class Solution {
     public int lengthOfLastWord(String s) {
         if(s.length() == 0) return 0; 
-        int len = 0;
-        int count = 0;
-        s = s + ' ';
-        char[] c = s.toCharArray();
-        for(int i = c.length - 2; i >= 0; i--) {
-            if(c[i] == ' ') {
-                if(c[i+1] == ' ') {
-                    continue;
-                } else {
-                    break;
-                }
-            } else {
-                len++;
-            }
+        String strim = s.trim();
+        String[] sarray = strim.split(" ");
+        return sarray[sarray.length-1].length();
+    }
+}
+
+//  
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        if(s.length() == 0) return 0; 
+        int tail = s.length() - 1, len = 0;
+        while(tail >= 0 && s.charAt(tail) == ' ') tail--;
+        while(tail >= 0 && s.charAt(tail) != ' ') {
+            tail--;
+            len++;
         }
         return len;
     }
