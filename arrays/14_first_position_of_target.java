d8f6163f8c3f5b8c975a50bcbf3bf20114597efa
eric496 <eric.mlengineer@gmail.com>
2017-10-19 22:27:55 -0400

    add comment

--- arrays/14_first_position_of_target.java
+++ arrays/14_first_position_of_target.java
@@ -3,32 +3,30 @@ For a given sorted array (ascending order) and a target number, find the first i
 If the target number does not exist in the array, return -1.
 
 Example
-If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
+    If the array is [1, 2, 3, 3, 4, 5, 10], for given target 3, return 2.
 */
 
 /*
 Thought process
-1. A classical binary search problem when you see O(log n)
-2. Use start + 1 < end in while condition
-3. Notice it is required to find the FIRST index, so instead of "return mid" we should instead let "end = mid" and check the value at the end
+    It is nothing more than a classical binary search. The only thing that should be careful about is return the "first" index of the target number. 
+    So after the while loop, it is possible the low-end and high-end runners can be both the target number. We need to check the low-end first, 
+    and then the high-end in order to return the "first" index of the target number.
 */
 
 class Solution {
     public int binarySearch(int[] nums, int target) {
-        if(nums.length == 0 || nums == null) return -1;
-        int start = 0, end = nums.length - 1;
-        while(start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if(target < nums[mid]) {
-                end = mid;
-            } else if(target > nums[mid]) {
-                start = mid;
-            } else if(target == nums[mid]) {
-                end = mid;
-            }
+        int low = 0, high = nums.length - 1;
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+                high = mid;
+            else if(nums[mid] > target)
+                high = mid - 1;
+            else 
+                low = mid + 1;
         }
-        if(target == nums[start]) return start;
-        if(target == nums[end]) return end;
+        if(nums[low] == target) return low;
+        if(nums[high] == target) return high;
         return -1;
     }
 }
