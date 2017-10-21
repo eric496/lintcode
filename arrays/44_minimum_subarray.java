029c8e9522dc193f22edbc988a54c0dd73811d10
eric496 <eric.mlengineer@gmail.com>
2017-10-21 16:04:13 -0400

    add comment

--- arrays/44_minimum_subarray.java
+++ arrays/44_minimum_subarray.java
@@ -3,19 +3,21 @@ Given an array of integers, find the subarray with smallest sum.
 Return the sum of the subarray.
 
 Notice
-The subarray should contain one integer at least.
+    The subarray should contain one integer at least.
 */
 
 /*
-Thought process: the same to 41, using Kadane's algorithm
+Thought process: 
+     Refer to 41. Maximum Subarray
 */
 
+// greedy by Kadane's algorithm
 public class Solution {
-    public int minSubArray(ArrayList<Integer> nums) {
-        int localMin = nums.get(0), globalMin = nums.get(0);
+    public int minSubArray(List<Integer> nums) {
+        int minSofar = nums.get(0), globalMin = nums.get(0);
         for(int i = 1; i < nums.size(); i++) {
-            localMin = Math.min(localMin + nums.get(i), nums.get(i));
-            if(localMin < globalMin) globalMin = localMin;
+            minSofar = Math.min(minSofar + nums.get(i), nums.get(i));
+            if(minSofar < globalMin) globalMin = minSofar;
         }
         return globalMin;
     }
