4fdb57979bce626e474434ed4929271cad475af9
eric496 <eric.mlengineer@gmail.com>
2017-10-21 14:31:08 -0400

    add 2nd solution

--- arrays/41_maximum_subarray.java
+++ arrays/41_maximum_subarray.java
@@ -6,27 +6,46 @@ Notice
 
 Example
     Given the array [−2,2,−3,4,−1,2,1,−5,3], the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+Challenge
+    Can you do it in time complexity O(n)?
 */
 
 /*
 Thought process
-1. Kadane's algorithm: 
-   the maximum sum of a contiguous subarray ending at a specific element is either 
-   1) the maximum sum of a contiguous subarray ending at its previous element plus the current element, OR
-   2) the specific element itself, 
-   depending on which is larger. 
-   So, the algorithm bascially iterates the array, compares each element with max sum of subarray ending at its previous element, 
-   keep the larger one and update the global maximum.
+    1. DP: 
+    2. Greedy - Kadane's algorithm: 
+            The maximum sum of a contiguous subarray ending at a specific element is either 
+                1) the maximum sum of a contiguous subarray ending at its previous element plus the current element, OR
+                2) the specific element itself, 
+                whichever is larger. 
+            Iterate the array, compares each element with max sum of subarray ending at its previous element, 
+            keep the larger one and update the global maximum.
 */
 
-//Kadane's algorithm
+// O(n) time and O(n) space by DP
 public class Solution {
     public int maxSubArray(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
-        int localMax = nums[0], globalMax = nums[0];
+        int[] dp = new int[nums.length+1];
+        dp[0] = nums[0];
+        int max = dp[0];
         for(int i = 1; i < nums.length; i++) {
-            localMax = Math.max(localMax + nums[i], nums[i]);
-            if(localMax > globalMax) globalMax = localMax;
+            dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
+            if(dp[i] > max) max = dp[i];
+        }
+        return max;
+    }
+}
+
+// O(n) time and O(1) space by Kadane's algorithm
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int maxSofar = nums[0], globalMax = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            maxSofar = Math.max(maxSofar + nums[i], nums[i]);
+            if(maxSofar > globalMax) globalMax = maxSofar;
         }
         return globalMax;
     }
