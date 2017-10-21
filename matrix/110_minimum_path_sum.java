802d19252ec63fc7f58375ee14eb3bc4a110b60b
eric496 <eric.mlengineer@gmail.com>
2017-10-21 15:54:18 -0400

    add comment

--- matrix/110_minimum_path_sum.java
+++ matrix/110_minimum_path_sum.java
@@ -7,34 +7,31 @@ Notice
 
 /*
 Thought process:
-    1. Create a 2-D array to keep track of the current min sum.
-    2. Compare sum of current grid item and its min sum from left
-       with sum of the current grid item and its min sum from up.
-       Keep the min sum as the current min sum. 
-    3. Remember to handle the first row and the first column
-       as they have only one option to sum (for the first row, 
-       it can only sum its left items, for the first column, it 
-       can only sum its up items.) 
+    DP: Create a 2D array to store the minimum sum so far at each position.
+        Since we can move either right or down, the basic idea to calculate minimum sum so far is by adding the current element 
+        to minimum sum so far at its left position or its top position, whichever is smaller.
+        Remember to handle the first row and the first column separately as they have only one direction to move forward 
+        (right for the first row and down for the first column). This is a simplified case, because we do not need to make comparison,
+        instead we just add the current element to the previous sum. 
 */
 
+// O(mn) time and O(mn) space by DP
 public class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if(grid == null || m == 0 || n == 0) return -1;
-        int[][] sum = new int[m][n];
-        sum[0][0] = grid[0][0];
-        for(int row = 0; row < m; row++) {
-            for(int col = 0; col < n; col++) {
-                if(row == 0 && col != 0) {
-                    sum[row][col] = sum[row][col-1] + grid[row][col];
-                } else if(col == 0 && row != 0) {
-                    sum[row][col] = sum[row-1][col] + grid[row][col];
-                } else if(row != 0 && col != 0) {
-                    sum[row][col] = Math.min(sum[row-1][col], sum[row][col-1]) + grid[row][col];
-                }
+        if(grid == null || grid[0] == null || grid.length == 0 || grid[0].length == 0) return -1;
+        int numRows = grid.length, numCols = grid[0].length;
+        int[][] dp = new int[numRows][numCols];
+        dp[0][0] = grid[0][0];
+        for(int rowix = 0; rowix < numRows; rowix++) {
+            for(int colix = 0; colix < numCols; colix++) {
+                if(rowix == 0 && colix != 0)
+                    dp[rowix][colix] = dp[rowix][colix-1] + grid[rowix][colix];
+                else if(colix == 0 && rowix != 0)
+                    dp[rowix][colix] = dp[rowix-1][colix] + grid[rowix][colix];
+                else if(rowix != 0 && colix != 0)
+                    dp[rowix][colix] = Math.min(dp[rowix-1][colix], dp[rowix][colix-1]) + grid[rowix][colix];
             }
         }
-        return sum[m-1][n-1];
+        return dp[numRows-1][numCols-1];
     }
 }
