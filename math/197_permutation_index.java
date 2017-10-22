8a8444e609ea479c80fdd21141d6e74462ad01a2
eric496 <eric.mlengineer@gmail.com>
2017-10-22 15:18:33 -0400

    add comment

--- math/197_permutation_index.java
+++ math/197_permutation_index.java
@@ -2,35 +2,37 @@
 Given a permutation which contains no repeated number, find its index in all the permutations of these numbers, which are ordered in lexicographical order. The index begins at 1.
 
 Example
-Given [1,2,4], return 1.
+    Given [1,2,4], return 1.
 */
 
 /*
 Thought process
-1. Count the number of the succeeding numbers which are smaller than current number (each one from left to right), set it to smaller_succeed.
-2. Suppose the index of the current number is i, we exchange the current number with one smaller succeeding number.
-   After the exchange, if the length from the current number (exclusive) to the end of the array is n, then there are n! eligible permutations.
-   Multiply smaller_succeed * n!, we get the possible permutations for the current number.
-   Add the statistic up for every number, we get the total number of permutations ahead of the current permutation. (initialize index = 1 will assure to return the current index.)
+    Count the number of the succeeding numbers of the current number which are smaller than the current number (each one from left to right), 
+    set it to smaller_succeed.
+    Suppose the index of the current number is i, we exchange the current number with one smaller succeeding number.
+    After the exchange, if the length from the current number (exclusive) to the end of the array is n, 
+    then there are n! eligible permutations.
+    Multiply smaller_succeed * n!, we get the possible permutations for the current number.
+    Add the statistic up for every number, we get the total number of permutations ahead of the current permutation. 
+    (initialize index = 1 will assure to return the current index.)
 */
 
 public class Solution {
-    public long factorial(long n) {
-        if(n == 0) return 1;
-        if(n <= 2) return n;
-        return n * factorial(n-1);
-    }
-    
     public long permutationIndex(int[] A) {
         if (A == null || A.length == 0) return 0;
         long index = 1;
-        for (int i = 0; i < A.length; i++) {
-            int smaller_succeed = 0;
-            for (int j = i + 1; j < A.length; j++) {
-                if(A[i] > A[j]) smaller_succeed++;
-            }
-            index += smaller_succeed * factorial(A.length - i - 1);
+        for(int i = 0; i < A.length; i++) {
+            int rank = 0;
+            for(int j = i + 1; j < A.length; j++)
+                if(A[i] > A[j]) rank++;
+            index += rank * factorial(A.length-i-1);
         }
         return index;
+    }
+    
+    private long factorial(long n) {
+        if(n == 0) return 1;
+        if(n <= 2) return n;
+        return n * factorial(n-1);
     }
 }
