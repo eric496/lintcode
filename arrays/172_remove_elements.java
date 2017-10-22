076a7a53b0a01a7db0078243eb1553007adc54de
eric496 <eric.mlengineer@gmail.com>
2017-10-22 18:21:09 -0400

    add comment

--- arrays/172_remove_elements.java
+++ arrays/172_remove_elements.java
@@ -3,26 +3,24 @@ Given an array and a value, remove all occurrences of that value in place and re
 The order of elements can be changed, and the elements after the new length don't matter.
 
 Example
-Given an array [0,4,4,0,0,2,4,4], value=4
-return 4 and front four elements of the array is [0,0,0,2]
+    Given an array [0,4,4,0,0,2,4,4], value=4
+    return 4 and front four elements of the array is [0,0,0,2]
 */
 
 /*
-Thought process
-1. IN PLACE: probably use two pointers.
-2. Intuitively, we would like judge if value = k. But it is not straightforward programmatically this way. 
-   Instead we will judge if value != k, and keep moving these values in the front of the array.
+Thought process:
+    Two pointers: A counter as well as a position pointer points to the last position so far for a non-target value.
+                  Iterate the array, if the current element is not the target value, copy it to the counter position.
 */
 
+// two pointers
 public class Solution {
     public int removeElement(int[] A, int elem) {
         if(A == null || A.length == 0) return 0;
-        int i = 0;
-        int j = 0;
-        while(i < A.length) {
-            if(A[i] != elem) A[j++] = A[i];
-            i++;
+        int count = 0;
+        for(int i = 0; i < A.length; i++) {
+            if(A[i] != elem) A[count++] = A[i];
         }
-        return j;
+        return count;
     }
 }
