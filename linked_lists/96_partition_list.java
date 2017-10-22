d61a5a54f031c90c036e7d2bdde5ee7decbdb053
eric496 <eric.mlengineer@gmail.com>
2017-10-22 15:09:51 -0400

    add comment

--- linked_lists/96_partition_list.java
+++ linked_lists/96_partition_list.java
@@ -21,28 +21,34 @@ Example
 
 /*
 Thought process:
-    Create two lists, one storing every node whose value is less than x, the other storing those whose value is greater than or equals to x.
+    Create two lists, one storing every node whose value is less than x, 
+    the other storing those whose value is greater than or equals to x.
+    Two dummy heads and two running pointers will be needed because:
+        1) The head of the first list will be returned as the result.
+        2) The head of the second list will be appended to the tail of the first list.
+        3) Two running pointers will iterate the whole list to select candidate node.
+    Don't forget to set the second list's tail to null for proper ending of a linked list.
 */
- 
+
 public class Solution {
     public ListNode partition(ListNode head, int x) {
         if(head == null) return null;
-        ListNode lhead = new ListNode(Integer.MIN_VALUE);
-        ListNode lrunner = lhead;
-        ListNode rhead = new ListNode(Integer.MIN_VALUE);
-        ListNode rrunner = rhead;
+        ListNode head1 = new ListNode(Integer.MIN_VALUE);
+        ListNode head2 = new ListNode(Integer.MIN_VALUE);
+        ListNode runner1 = head1;
+        ListNode runner2 = head2;
         while(head != null) {
             if(head.val < x) {
-                lrunner.next = head;
-                lrunner = lrunner.next;
+                runner1.next = head;
+                runner1 = runner1.next;
             } else {
-                rrunner.next = head;
-                rrunner = rrunner.next;
+                runner2.next = head;
+                runner2 = runner2.next;
             }
             head = head.next;
         }
-        rrunner.next = null;
-        lrunner.next = rhead.next;
-        return lhead.next;
+        runner2.next = null;
+        runner1.next = head2.next;
+        return head1.next;
     }
-}
+} 
