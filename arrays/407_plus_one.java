ad6211a9c736aa3ce6f32603d173d5f60c47cf1f
eric496 <eric.mlengineer@gmail.com>
2017-10-22 15:39:38 -0400

    add comment

--- arrays/407_plus_one.java
+++ arrays/407_plus_one.java
@@ -3,31 +3,36 @@ Given a non-negative number represented as an array of digits, plus one to the n
 The digits are stored such that the most significant digit is at the head of the list.
 
 Example
-Given [1,2,3] which represents 123, return [1,2,4].
-Given [9,9,9] which represents 999, return [1,0,0,0].
+    Given [1,2,3] which represents 123, return [1,2,4].
+    Given [9,9,9] which represents 999, return [1,0,0,0].
 */
 
 /*
 Thought process
-1. To solve this type of problems in 3 steps:
-   1) sum = current number + carry;
-   2) sum % 10 as the number at ones place / tens place / hundreds place ...
-   3) carry sum / 10 to the next higher place.  
+   Create a sum and a carry variables. Each number will be processed in 3 steps:
+       1) sum = current number + carry;
+       2) sum % 10 as the number at ones place / tens place / hundreds place ...
+       3) carry sum / 10 to the next higher place.
+   Be careful about two things: 
+       First, the default value of carry is 1, because 1 will be added to the number.
+       Second, iterate the array in backward order because addition operates from ones place, tens place, ...
+       Third, because of the second, the result array is in reverse order, we need to reverse it back to the correct order.  
 */
 
 public class Solution {
     public int[] plusOne(int[] digits) {
+        if(digits == null || digits.length == 0) return digits;
         int sum = 0, carry = 1;
-        ArrayList<Integer> res = new ArrayList<Integer>();
+        List<Integer> rev = new ArrayList<Integer>();
         for(int i = digits.length - 1; i >= 0; i--) {
             sum = digits[i] + carry;
-            res.add(sum % 10);
             carry = sum / 10;
+            rev.add(sum % 10);
         }
-        if(carry != 0) res.add(carry);
-        Collections.reverse(res);
-        int[] result = new int[res.size()];
-        for(int i = 0; i < res.size(); i++) result[i] = res.get(i);
+        if(carry != 0) rev.add(carry);
+        int[] result = new int[rev.size()];
+        int ix = rev.size() - 1;
+        for(int i : rev) result[ix--] = i;
         return result;
     }
 }
