3e9830a91b0ad2a9bfd9584aa131acd4ad0da301
eric496 <eric.mlengineer@gmail.com>
2017-10-22 17:55:37 -0400

    add comment

--- arrays/101_remove_duplicates_from_sorted_array_II.java
+++ arrays/101_remove_duplicates_from_sorted_array_II.java
@@ -7,13 +7,23 @@ Example
     Your function should return length = 5, and A is now [1,1,2,2,3].
 */
 
+/*
+Thought process:
+    Similar to 100. Remove Duplicates from sorted array. 
+    The only difference is to check the equality of the current element and the element at [position pointer-2].
+    Generalization:
+        What if at most k duplicates are allowed?
+        Same idea, but check equality at position pointer - k 
+*/
+
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums == null) return 0;
-        if(nums.length < 2) return nums.length;
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length <= 2) return nums.length;
         int count = 2;
         for(int i = 2; i < nums.length; i++) {
-            if(nums[i] != nums[count-2]) nums[count++] = nums[i];
+            if(nums[i] != nums[count-2]) 
+		nums[count++] = nums[i];
         }
         return count;
     }
@@ -22,20 +32,13 @@ public class Solution {
 //Generalization: what if at most k duplicates are allowed?
 public class Solution {
     public removeDuplicates(int[] nums, int k) {
+        if(nums == null || nums.length == 0) return 0;
         if(nums.length <= k) return nums.length;
-        int i = 1, j = 1, cnt = 1;
-        while(j < nums.length) {
-            if(nums[j] != A[j-1]) {
-                cnt = 1;
-                A[j++] = A[j];
-            } else {
-                if(cnt < k) {
-                    A[i++] = A[j];
-                    cnt++;
-                }
-            }
-            j++;
-        }
-        return i;
+	int count = k;
+        for(int i = k; i < nums.length; i++) {
+  	    if(nums[i] != nums[count-k]) 
+		nums[count++] = nums[i];
+	}
+	return count;
     }
 }
