f5912246ab22e33bce97cd86f732585ac49717fa
eric496 <eric.mlengineer@gmail.com>
2017-10-23 16:43:59 -0400

    change variable name

--- matrix/114_unique_paths.java
+++ matrix/114_unique_paths.java
@@ -22,13 +22,13 @@ Thought process:
 public class Solution {
     public int uniquePaths(int m, int n) {
         if (m <= 1 || n <= 1) return 1;
-        int[][] matrix = new int[m][n];
-        for (int i = 0; i < m; i++) matrix[i][0] = 1;
-        for (int i = 0; i < n; i++) matrix[0][i] = 1;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++)
-                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
-        return matrix[m - 1][n - 1];
+        return dp[m - 1][n - 1];
     }
 }
