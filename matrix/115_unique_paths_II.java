56419e8f48b602653c6f4eec1e79ddfd61fc7096
eric496 <eric.mlengineer@gmail.com>
2017-10-23 17:22:36 -0400

    change 115 solution and add comment

--- matrix/115_unique_paths_II.java
+++ matrix/115_unique_paths_II.java
@@ -17,45 +17,48 @@ Example
     The total number of unique paths is 2.
 */
 
+/*
+Thought process:
+    There are 4 cases for each element in the matrix:
+        1) Its top and left elements are both ones (it is a dead end and it has no path).
+        2) Its top element is zero, but its left element is one (it inherits the unique paths of its top element).
+        3) Its left element is zero, but its top element is one (it inherits the unique paths of its left element).
+        4) Its top and left elements are both zeroes (it inherits the unique paths of its top AND left elements).
+    Remember to handle the first row and column because they have no top or left elements.
+*/
+
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid==null||obstacleGrid.length==0) return 0;
+        if(obstacleGrid == null || obstacleGrid.length == 0) return 0;
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1) return 0;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
         int[][] dp = new int[m][n];
-        dp[0][0]=1;
-     
-        //left column
-        for(int i=1; i<m; i++){
-            if(obstacleGrid[i][0]==1){
-                dp[i][0] = 0;
-            }else{
-                dp[i][0] = dp[i-1][0];
+        dp[0][0] = 1;
+        for(int col = 1; col < n; col++) {
+            if(obstacleGrid[0][col-1] == 1) 
+                dp[0][col] = 0;
+            else 
+                dp[0][col] = dp[0][col-1];
+        }
+        for(int row = 1; row < m; row++) {
+            if(obstacleGrid[row-1][0] == 1) 
+                dp[row][0] = 0;
+            else 
+                dp[row][0] = dp[row-1][0];
+        }
+        for(int row = 1; row < m; row++) {
+            for(int col = 1; col < n; col++) {
+                if(obstacleGrid[row-1][col] == 1 && obstacleGrid[row][col-1] == 1)
+                    dp[row][col] = 0;
+                else if(obstacleGrid[row-1][col] == 1) 
+                    dp[row][col] = dp[row][col-1];
+                else if(obstacleGrid[row][col-1] == 1)
+                    dp[row][col] = dp[row-1][col];
+                else 
+                    dp[row][col] = dp[row-1][col] +dp[row][col-1];
             }
         }
-     
-        //top row
-        for(int i=1; i<n; i++){
-            if(obstacleGrid[0][i]==1){
-                dp[0][i] = 0;
-            }else{
-                dp[0][i] = dp[0][i-1];
-            }
-        }
-     
-        //fill up cells inside
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
-                }else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-     
-            }
-        }
-     
         return dp[m-1][n-1];
     }
 }
