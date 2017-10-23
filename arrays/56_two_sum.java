1909c781d8f211b06c74deab1ec02e30f29764ea
eric496 <eric.mlengineer@gmail.com>
2017-10-23 16:07:08 -0400

    add comment

--- arrays/56_two_sum.java
+++ arrays/56_two_sum.java
@@ -3,21 +3,25 @@ Given an array of integers, find two numbers such that they add up to a specific
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are NOT zero-based.
 
 Notice
-You may assume that each input would have exactly one solution
+    You may assume that each input would have exactly one solution
 
 Example
-numbers=[2, 7, 11, 15], target=9
-return [1, 2]
+    numbers=[2, 7, 11, 15], target=9
+    return [1, 2]
+
+Challenge
+    Either of the following solutions are acceptable:
+        O(n) Space, O(nlogn) Time
+        O(n) Space, O(n) Time
 */
 
 /*
 Thought process:
-    1. O(n) time and O(n) space solution: use hash map to store <target-number, index> pairs. 
-       Loop through the array, check if the hash map contains key number. 
-       If it does, then n2 = target - n1, where n1 is the first number and n2 is the second. 
-       Retrieve the indices of these two numbers - the first is the value of the matched key, 
-       the second is just the current iteration number. 
-    2. Don't forget to add 1 to the result indices because the result indices are not zero-based.
+    O(n) time and O(n) space solution: use hash map to store <target-number, index> pairs. 
+    Loop through the array, check if the hash map contains key number. 
+    If it does, then n2 = target - n1, where n1 is the first number and n2 is the second. 
+    Retrieve the indices of these two numbers - the first is the matched key, the second is just the current iteration number. 
+    Don't forget to add 1 to the result indices because the result indices are not zero-based.
 */
 
 public class Solution {
