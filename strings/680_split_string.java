c8331845f1b77cd630a73dcb69acb947518ef7de
eric496 <eric.mlengineer@gmail.com>
2017-10-27 16:27:17 -0400

    add solution

--- strings/680_split_string.java
+++ strings/680_split_string.java
@@ -6,4 +6,30 @@ Example
     return [["1","2","3"],["12","3"],["1","23"]]
 */
 
-
+public class Solution {
+    public List<List<String>> splitString(String s) {
+        List<List<String>> result = new ArrayList<>();
+        if (s == null) {
+            return result;
+        } else if (s.length() == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+        dfs(result, new ArrayList<>(), 0, s);
+        return result;
+    }
+    
+    private void dfs(List<List<String>> results, List<String> result, int index, String s) {
+        if (index == s.length()) {
+            results.add(new ArrayList<>(result));
+            return;
+        }
+        
+        for (int i = index; i < index + 2 && i < s.length(); i++) {
+            String substring = s.substring(index, i+1);
+            result.add(substring);
+            dfs(results, result, i+1, s);
+            result.remove(result.size()-1);
+        }
+    }
+}
