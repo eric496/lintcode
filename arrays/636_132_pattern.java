1b54fb34c785e903548f7bc54d18b0dbad859340
eric496 <eric.mlengineer@gmail.com>
2017-10-27 18:36:00 -0400

    add O(n) solution

--- arrays/636_132_pattern.java
+++ arrays/636_132_pattern.java
@@ -11,9 +11,66 @@ Example
 
 /*
 Thought process:
-    
+    1. Brute-force: a no-brainer O(n^3) solution, iterate every (i, j, k) combination and return true if an 132 pattern occurs.
+    2. O(n^2) time improved solution:
+           Basic idea is for any fixed j, we would like to obtain the minimum element in nums[0 : j] (j exclusive), such that (nums[argmin], nums[j])
+           gives the largest difference range possible so far. 
+           Then in the inner loop, we search for the an element that is greater than the nums[argmin] and smaller than the current element nums[j].
+    3. 
 */
 
+// O(n^3) time - brute-force
+// this will cause TLE
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[k] && nums[k] < nums[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// O(n^2) time
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] < min) min = nums[j];
+            for (int k = j + 1; k < nums.length; k++) {
+                if (nums[k] > min && nums[j] > nums[k]) return true;
+            }
+        }
+        return false;
+    }
+}
+
+// O(n) time with two passes
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        int[] minSofar = new int[nums.length];
+        minSofar[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            minSofar[i] = Math.min(nums[i], minSofar[i-1]);
+        }
+        int top = nums.length;
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] <= minSofar[j]) continue;
+            while (top < nums.length && minSofar[top] <= minSofar[j]) top++;
+            if (top < nums.length && nums[j] > minSofar[top]) return true;
+            minSofar[--top] = nums[j];
+        }
+        return false;
+    }
+}
+
+// O(n) time using stack
 public class Solution {
     public boolean find132pattern(int[] nums) {
         Stack<Integer> stack = new Stack<Integer>();
