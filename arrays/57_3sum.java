5bff7a514a9961da756b601c26db73340d24cac6
eric496 <eric.mlengineer@gmail.com>
2017-10-27 18:51:20 -0400

    add comment

--- arrays/57_3sum.java
+++ arrays/57_3sum.java
@@ -13,35 +13,34 @@ Example
 
 /*
 Thought process:
-    1. Sort the array first and loop through it. For each element, 
-       loop through from the the element right to it to the end of the array.
-       If 3sum equals 0, keep them in the result. If it is greater than 0, reduce the high end; 
-       if it is less than 0, increment the low end. 
-    2. Remember to skip the same result, since once num[i] is fixed for each loop, if num[low] equals
-       num[low-1] or num[high] = num[high+1] then we will get the extacly same result as previous loop. 
+    1. Sort the array, for any fixed i, iterate all elements on its right using two pointers (iterate from head and tail simultaneously).
+       Add 3sum to result list if 3sum equals 0. Otherwise, increment low pointer if 3sum is smaller than 0, decrease high pointer if 3sum is greater than 0.
+    2. Don't forget to skip duplicate element during iteration.
 */
 
 // O(n^2) time
 public class Solution {
     public List<List<Integer>> threeSum(int[] num) {
         if(num == null || num.length < 3) return null;
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(num);
-        for(int i = 0; i < num.length; i++) {
-            if(i != 0 && num[i] == num[i-1]) continue;
+        for (int i = 0; i < num.length; i++) {
+            // skip duplicate element
+            if (i != 0 && num[i] == num[i-1]) continue;
             int low = i + 1, high = num.length - 1;
-            while(low < high) {
-                if(num[i] + num[low] + num[high] == 0) {
+            while (low < high) {
+                if (num[i] + num[low] + num[high] == 0) {
                     res.add(Arrays.asList(num[i], num[low++], num[high--]));
+                    // skip duplicate element
                     while(low < high && num[low] == num[low-1]) low++;
                     while(low < high && num[high] == num[high+1]) high--;
-                } else if(num[i] + num[low] + num[high] < 0) {
+                } else if (num[i] + num[low] + num[high] < 0) {
                     low++;
                 } else {
                     high--;  
                 }
             }
         }
-        return res;
+        return result;
     }
 }
