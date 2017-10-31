14950897060cd009a8f6cf68e677f11b0ecb64df
eric496 <eric.mlengineer@gmail.com>
2017-10-31 17:51:22 -0400

    add generalization

--- arrays/151_best_time_to_buy_and_sell_stock_III.java
+++ arrays/151_best_time_to_buy_and_sell_stock_III.java
@@ -45,3 +45,20 @@ public class Solution {
         return profit;
     }
 }
+
+// a generalization to at most K transactions
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int K = 2;
+        int[][] dp = new int[K+1][prices.length];
+        for (int i = 1; i < dp.length; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + maxDiff);
+                maxDiff = Math.max(maxDiff, dp[i-1][j] - prices[j]);
+            }
+        }
+        return dp[K][prices.length-1];
+    }
+}
