4f90519901f32a28427d773611c9b370d384d262
eric496 <eric.mlengineer@gmail.com>
2017-11-01 18:30:13 -0400

    add solution

--- strings/512_decode_ways.java
+++ strings/512_decode_ways.java
@@ -13,21 +13,21 @@ Example
 
 /*
 Thought process:
-    1. DP
+    DP
 */
 
 public class Solution {
     public int numDecodings(String s) {
         if (s == null || s.length() == 0) return 0;
-        int[] memo = new int[s.length() + 1];
-        memo[0] = 1;
-        memo[1] = s.charAt(0) != '0' ? 1 : 0;
+        int[] dp = new int[s.length()+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
         for (int i = 2; i <= s.length(); i++) {
             int one = s.charAt(i - 1) - '0';
             int two = one + (s.charAt(i - 2) - '0') * 10; 
-            if(one >= 1 && one <= 9) memo[i] += memo[i-1];
-            if(two >= 10 && two <= 26) memo[i] += memo[i-2];
+            if (one >= 1 && one <= 9) dp[i] += dp[i-1];
+            if (two >= 10 && two <= 26) dp[i] += dp[i-2];
         }
-        return memo[s.length()];
+        return dp[s.length()];
     }
 }
