49511a46600dba0d6ff5effdea13a00041c28cef
eric496 <eric.mlengineer@gmail.com>
2017-11-02 21:48:42 -0400

    add solution

--- arrays/196_find_the_missing_number.java
+++ arrays/196_find_the_missing_number.java
@@ -8,34 +8,57 @@ Challenge
     Do it in-place with O(1) extra memory and O(n) time.
 */
 
-// bit manipulation
+/*
+Thought process:
+    1. Sort and iterate the array, if index != number, then it is the missing number.
+    2. Bit manipulation
+    3. The arithmetic progression summation formula: 1 + 2 + ... + n = n * (n + 1) / 2
+    4. Sort and binary search
+*/
+
+// O(n) time
+public class Solution {
+    public int findMissing(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i != nums[i]) return i;
+        }
+        // the last number is the missing number
+        return nums.length;
+    }
+}
+
+// O(n) time
 public class Solution {
     public int findMissing(int[] nums) {
         int xor = 0, i = 0;
-        for(i = 0; i < nums.length; i++) xor = xor ^ i ^ nums[i];
+        for (i = 0; i < nums.length; i++) {
+	    xor = xor ^ i ^ nums[i];
+	}
         return xor ^ i;
     }
 }
 
-// calculate sum by n * (n + 1)
+// O(n) time
 public class Solution {
     public int findMissing(int[] nums) {
         int n = nums.length;
         int sum = n * (n + 1) / 2;
-        for(int num : nums) sum -= num;
+        for (int num : nums) sum -= num;
         return sum;
     }
 }
 
-// binary search
+// O(logn) time
 public class Solution {
     public int findMissing(int[] nums) {
-        if(nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0) return -1;
         Arrays.sort(nums);
         int low = 0, high = nums.length;
-        while(low < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] > mid) {
+            if (nums[mid] > mid) {
                 high = mid;
             } else {
                 low = mid + 1;
