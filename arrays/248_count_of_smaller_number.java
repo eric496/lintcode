9252e8fa4cb1ed8574f4170ce1d2a1d81c09aa19
eric496 <eric.mlengineer@gmail.com>
2017-11-02 20:34:30 -0400

    change solution

--- arrays/248_count_of_smaller_number.java
+++ arrays/248_count_of_smaller_number.java
@@ -22,23 +22,25 @@ Thought process:
 public class Solution {
     public ArrayList<Integer> countOfSmallerNumber(int[] A, int[] queries) {
         Arrays.sort(A);
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        for (int q : queries) res.add(binarySearch(A, q));
-        return res;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int q : queries) {
+	    result.add(binarySearch(A, q));
+	}
+        return result;
     }
     
     private int binarySearch(int[] A, int val) {
         int start = 0, end = A.length - 1;
-        int res = 0;
+        int result = 0;
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (A[mid] >= val) {
                 end = mid - 1;
             } else {
-                res = mid + 1;
+                result = mid + 1;
                 start = mid + 1;
             }
         }
-        return res;
+        return result;
     }
 }
