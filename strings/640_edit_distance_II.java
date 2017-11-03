f72989361baab97a9928ad9d80e46baca0aee20d
eric496 <eric.mlengineer@gmail.com>
2017-11-02 20:55:45 -0400

    add comment

--- strings/640_edit_distance_II.java
+++ strings/640_edit_distance_II.java
@@ -8,15 +8,15 @@ Example
 
 /*
 Thought process:
-    1. There are two cases that two strings are one edit distance away from each other:
-           1) one string is one character less than the other, and the relative order of other characters are the same for the two strings
-              so by inserting an character in the short string we will get the long string.
-           2) two strings are of the same length with only one character different.
-    2. Notice two corner cases: return false if difference of lengths is greater than 1 or two strings are extactly the same (no distance)
-    3. Differentiate the shorter string from the longer one. Create two index variables and a count variable to keep the number of different characters.
-       If the current characters are the same, increment both short and long indices. Otherwise, if two strings have the same length, increment both indices as well, 
-       if they have different length, increment only the long index. Finally check if count of different characters are less than or equals one 
-       (there is a corner case that diff == 0 when the last character of the long string is the only different character)
+    There are two cases that two strings are one edit distance away from each other:
+        1) one string is one character less than the other, and the relative order of other characters are the same for the two strings
+           so by inserting an character in the short string we will get the long string.
+        2) two strings are of the same length with only one character different.
+    Notice two corner cases: return false if difference of lengths is greater than 1 or two strings are extactly the same (no distance)
+    Differentiate the shorter string from the longer one. Create two index variables and a count variable to keep the number of different characters.
+    If the current characters are the same, increment both short and long indices. Otherwise, if two strings have the same length, increment both indices as well.
+    If they have different length, increment only the long index. Finally check if count of different characters are less than or equals one 
+    (there is a corner case that diff == 0 when the last character of the long string is the only different character).
 */
 
 public class Solution {
@@ -25,13 +25,13 @@ public class Solution {
         String longer = s.length() >= t.length() ? s : t;
         String shorter = s.length() <= t.length() ? t : s;
         int shortIdx = 0, longIdx = 0, diff = 0;
-        while(shortIdx < shorter.length()) {
-            if(shorter.charAt(shortIdx) == longer.charAt(longIdx)) {
+        while (shortIdx < shorter.length()) {
+            if (shorter.charAt(shortIdx) == longer.charAt(longIdx)) {
                 shortIdx++;
                 longIdx++;
             } else {
-                if(++diff > 1) return false;
-                if(shorter.length() == longer.length()) {
+                if (++diff > 1) return false;
+                if (shorter.length() == longer.length()) {
                     shortIdx++;
                     longIdx++;
                 } else {
