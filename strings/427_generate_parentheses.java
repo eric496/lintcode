70bb463f675799566b8c82e979ec0746c79491a5
eric496 <eric.mlengineer@gmail.com>
2017-11-03 16:38:58 -0400

    add thought

--- strings/427_generate_parentheses.java
+++ strings/427_generate_parentheses.java
@@ -6,6 +6,11 @@ Example
     "((()))", "(()())", "(())()", "()(())", "()()()"
 */
 
+/*
+Thought process:
+    Backtrack
+*/
+
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<String>();
@@ -14,11 +19,15 @@ public class Solution {
     }
     
     public void backtrack(List<String> list, String str, int open, int close, int max) {
-        if(str.length() == max * 2){
+        if (str.length() == max*2){
             list.add(str);
             return;
         }
-        if(open < max) backtrack(list, str+"(", open+1, close, max);
-        if(close < open) backtrack(list, str+")", open, close+1, max);
+        if (open < max) {
+            backtrack(list, str+"(", open+1, close, max);
+        }
+        if (close < open) {
+            backtrack(list, str+")", open, close+1, max);
+        }
     }
 }
