f23abdfbb8fea8d343afbc3b1347ff292c00cd2b
eric496 <eric.mlengineer@gmail.com>
2017-11-03 16:58:22 -0400

    add comment

--- arrays/535_house_robber_III.java
+++ arrays/535_house_robber_III.java
@@ -17,13 +17,12 @@ Example
     Maximum amount of money the thief can rob = 4 + 5 = 9.
 */
 
-// DFS
+/*
+Thought process:
+    DFS
+*/
+
 public class Solution {
-    public int houseRobber3(TreeNode root) {
-        int[] res = rob(root);
-        return Math.max(res[0], res[1]);
-    }
-    
     private int[] rob(TreeNode root) {
         if (root == null) return new int[2];
         int[] left = rob(root.left);
@@ -32,5 +31,10 @@ public class Solution {
         res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
         res[1] = root.val + left[0] + right[0];
         return res;
+    }
+
+    public int houseRobber3(TreeNode root) {
+        int[] res = rob(root);
+        return Math.max(res[0], res[1]);
     }
 }
