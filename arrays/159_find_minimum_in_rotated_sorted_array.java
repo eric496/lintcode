894fb20a18b8bca40bcaffdf46effb8f36fcd68b
eric496 <eric.mlengineer@gmail.com>
2017-11-02 21:09:00 -0400

    add comment

--- arrays/159_find_minimum_in_rotated_sorted_array.java
+++ arrays/159_find_minimum_in_rotated_sorted_array.java
@@ -10,16 +10,22 @@ Example
     Given [4, 5, 6, 7, 0, 1, 2] return 0
 */
 
+/*
+Thought process:
+    Binary search
+*/
+
+// O(logn) time and O(1) space
 public class Solution {
     public int findMin(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
         int low = 0, high = nums.length - 1;
-        while(low < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if(nums[high] > nums[mid]) {
+            if (nums[high] > nums[mid]) {
                 // the min is in the left part
                 high = mid;
-            } else if(nums[high] < nums[mid]) {
+            } else if (nums[high] < nums[mid]) {
                 // the min is in the right part
                 low = mid + 1;
             }
