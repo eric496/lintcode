975cfd02a2cbe4d88d4463713cc4e5ba1971149e
eric496 <eric.mlengineer@gmail.com>
2017-11-02 20:58:33 -0400

    add comment

--- arrays/424_evaluate_reverse_polish_notation.java
+++ arrays/424_evaluate_reverse_polish_notation.java
@@ -9,17 +9,15 @@ Example
 
 /*
 Thought process:
-    1. Use a stack, push integers into the stack, once we meet an arithmetic operator, 
-       pop the top 2 elements and do the calculation and push the result back into the stack.
-       Finally we have only one element in the stack which is the result.
-    2. Notice for string comparison use .equals() method instead of "=="
+    Use a stack, push integers into the stack, once we meet an arithmetic operator, pop the top 2 elements and do the calculation and push the result back into the stack.
+    Finally we have only one element in the stack which is the result. Notice we use .equals() method instead of "==" to compare value instead of reference.
 */
 
 public class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> s = new Stack<Integer>();
-        for(String token : tokens) {
-            if(token.equals("+")) {
+        for (String token : tokens) {
+            if (token.equals("+")) {
                 s.push(s.pop() + s.pop());
             } else if(token.equals("-")) {
                 int num = s.pop();
