66399ca51f8679dae58962e7067797a97d3386d5
eric496 <eric.mlengineer@gmail.com>
2017-11-03 23:45:40 -0400

    change solution

--- arrays/117_jump_game_II.java
+++ arrays/117_jump_game_II.java
@@ -8,18 +8,22 @@ Example
     The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
 */
 
-// greedy
+/*
+Thought process:
+*/
+
 public class Solution {
     public int jump(int[] A) {
-	int jumps = 0, curEnd = 0, curFarthest = 0;
-	for (int i = 0; i < A.length - 1; i++) {
-	    curFarthest = Math.max(curFarthest, i + A[i]);
-	    if (i == curEnd) {
-		jumps++;
-		curEnd = curFarthest;
-		if (curEnd >= A.length - 1) break;
-	    }
-	}
-        return jumps;
+	int numJumps = 0;
+        int prevJumpMax = 0;
+        int curJumpMax = 0;
+        for(int i = 0; i < A.length - 1; i++) {
+            if (i + A[i] > curJumpMax) curJumpMax = i + A[i];
+            if (i == prevJumpMax) {
+                numJumps++;
+                prevJumpMax = curJumpMax;
+            } 
+        }
+        return numJumps;
     }
 }
