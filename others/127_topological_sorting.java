5536f7c01fb0335267fee77c0f39c056cc8c8e96
eric496 <eric.mlengineer@gmail.com>
2017-11-04 17:30:58 -0400

    add solution

--- others/127_topological_sorting.java
+++ others/127_topological_sorting.java
@@ -14,4 +14,45 @@ Challenge
     Can you do it in both BFS and DFS?
 */
 
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
 
+public class Solution {
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        ArrayList<DirectedGraphNode> result = new ArrayList<DirectedGraphNode>();
+        HashMap<DirectedGraphNode, Integer> map = new HashMap();
+        for (DirectedGraphNode node : graph) {
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                if (map.containsKey(neighbor)) {
+                    map.put(neighbor, map.get(neighbor) + 1);
+                } else {
+                    map.put(neighbor, 1); 
+                }
+            }
+        }
+        Queue<DirectedGraphNode> q = new LinkedList<DirectedGraphNode>();
+        for (DirectedGraphNode node : graph) {
+            if (!map.containsKey(node)) {
+                q.offer(node);
+                result.add(node);
+            }
+        }
+        while (!q.isEmpty()) {
+            DirectedGraphNode node = q.poll();
+            for (DirectedGraphNode n : node.neighbors) {
+                map.put(n, map.get(n) - 1);
+                if (map.get(n) == 0) {
+                    result.add(n);
+                    q.offer(n);
+                }
+            }
+        }
+        return result;
+    }
+}
