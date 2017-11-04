1d9ca2d659d736f570bf113f90e95913a93a194a
eric496 <eric.mlengineer@gmail.com>
2017-11-03 22:16:28 -0400

    change solution

--- strings/29_interleaving_string.java
+++ strings/29_interleaving_string.java
@@ -10,21 +10,26 @@ Challenge
     O(n2) time or better
 */
 
+/*
+Thought process:
+    DP
+*/
+
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if ((s1.length()+s2.length()) != s3.length()) return false;
+        if ((s1.length() + s2.length()) != s3.length()) return false;
         boolean[][] dp = new boolean[s2.length()+1][s1.length()+1];
         dp[0][0] = true;
-        for (int i = 1; i < dp[0].length; i++){
+        for (int i = 1; i < dp[0].length; i++) {
             dp[0][i] = dp[0][i-1] && (s1.charAt(i-1) == s3.charAt(i-1));
         }
-        for (int i = 1; i < dp.length; i++){
+        for (int i = 1; i < dp.length; i++) {
             dp[i][0] = dp[i-1][0] && (s2.charAt(i-1) == s3.charAt(i-1));
         }
-        for (int i = 1; i < dp.length; i++){
-            for (int j = 1; j < dp[0].length; j++){
-                dp[i][j] = (dp[i-1][j] && (s2.charAt(i-1) == s3.charAt(i+j-1)))
-                        || (dp[i][j-1] && (s1.charAt(j-1) == s3.charAt(i+j-1)));
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = (dp[i-1][j] && (s2.charAt(i-1) == s3.charAt(i+j-1))) 
+                    || (dp[i][j-1] && (s1.charAt(j-1) == s3.charAt(i+j-1)));
             }
         }
         return dp[s2.length()][s1.length()];
