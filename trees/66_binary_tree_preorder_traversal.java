6ea6f5dc4026a0c5670c963006b5be420614538e
eric496 <eric.mlengineer@gmail.com>
2017-11-05 10:50:51 -0500

    add comment

--- trees/66_binary_tree_preorder_traversal.java
+++ trees/66_binary_tree_preorder_traversal.java
@@ -25,15 +25,24 @@ Example
 
 /*
 Thought process:
-    1. DFS:
+    0. What is preorder traversal?
+           Traverse the nodes in the tree in root-left-right order.
+           For example, 
+			A
+		      /   \
+		     B     C
+		    / \   / \
+		   D   F G   H
+		  / \     \
+		 I   J     K
+           The preorder traversal is A-B-D-I-J-F-C-G-K-H.
+    1. Recursive solution: 
            Base case: return when current node is null
-           Recursive steps: add the value of the current node to the list,
-                            run recursively the left child first and then the right child.
-    2. Iterative solution using stack:
-           Push root into stack
-           Pop the top node in the stack, add its value to the result list
-           Push its RIGHT child first, then its left child into the stack
-       	   Because stack is a LIFO data structure, we need to push right child before the left in order for preorder traversal.
+           Recursive step: add the value of the current node to the result list, run recursively on the left subtree first and then the right.
+    2. Iterative solution:
+           Push root into the stack, As long as the stack is not empty, iteratively pop the top node in the stack, add its value to the result list.
+           Push its RIGHT child first, then its left child into the stack.
+       	   Stack is a LIFO data structure, so we need to push right child before the left in order to pop the left first then the right for preorder manner.
 */
 
 // recursive solution
@@ -45,7 +54,7 @@ public class Solution {
     }
     
     private void dfs(TreeNode root, ArrayList<Integer> result) {
-        if(root == null) return;
+        if (root == null) return;
         result.add(root.val);
         dfs(root.left, result);
         dfs(root.right, result);
@@ -55,15 +64,19 @@ public class Solution {
 // iterative solution 1
 public class Solution {
     public ArrayList<Integer> preorderTraversal(TreeNode root) {
-        if(root == null) return null;
+        if (root == null) return null;
         ArrayList<Integer> result = new ArrayList<Integer>();
         Stack<TreeNode> s = new Stack<TreeNode>();
         s.push(root);
-        while(!s.isEmpty()) {
+        while (!s.isEmpty()) {
             TreeNode node = s.pop();
             result.add(node.val);
-            if(node.right != null) s.push(node.right);
-            if(node.left != null) s.push(node.left);
+            if (node.right != null) {
+		s.push(node.right);
+	    }
+            if (node.left != null) {
+		s.push(node.left);
+	    }
         }
         return result;
     }
@@ -75,8 +88,8 @@ public class Solution {
         List<Integer> result = new ArrayList<>();
         Stack<TreeNode> s = new Stack<>();
         TreeNode node = root;
-        while(!s.isEmpty() || node != null) {
-            if(node != null) {
+        while (!s.isEmpty() || node != null) {
+            if (node != null) {
                 s.push(node);
                 result.add(node.val);    // Add before going to children
                 node = node.left;
