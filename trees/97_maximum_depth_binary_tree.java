c9f29623acb5ae70bd1c3803e949fc5efa4b5d6b
eric496 <eric.mlengineer@gmail.com>
2017-11-05 00:47:43 -0400

    add comment

--- trees/97_maximum_depth_binary_tree.java
+++ trees/97_maximum_depth_binary_tree.java
@@ -28,41 +28,41 @@ Example
 Thought process: 
     1. Recursive solution: 
            Base case: return 0 when current node is null.
-           Recursive steps: run recursively on the left and the right children of the current node, 
-                            return the larger value PLUS ONE
+           Recursive step: run recursively on the left and the right children of the current node, 
+                           return the larger value PLUS ONE
     2. Iterative solution: 
            BFS: Similar to level order traversal, with a count variable to keep the number of levels.
            DFS: Use two stacks, push each node into stack s, meanwhile push the current number of levels into stack v,
-                At each iteration, update a max variable. 
+                At each iteration, update the max depth. 
            
 */
 
 // recursive - trivial
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
 
-// iterative - DFS with two stacks
+// iterative - DFS using two stacks
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         Stack<TreeNode> s = new Stack<TreeNode>();
         Stack<Integer> v = new Stack<Integer>();
         s.push(root);
         v.push(1);
         int max = 1;
-        while(!s.isEmpty()) {
+        while (!s.isEmpty()) {
             TreeNode node = s.pop();
             int height = v.pop();
             max = Math.max(height, max);
-            if(node.left != null) {
+            if (node.left != null) {
                 s.push(node.left);
                 v.push(height + 1);
             }
-            if(node.right != null) {
+            if (node.right != null) {
                 s.push(node.right);
                 v.push(height + 1);
             }
@@ -71,19 +71,23 @@ public class Solution {
     }
 }
 
-// iterative - BFS with queue
+// iterative - BFS using queue
 public class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         Queue<TreeNode> q = new LinkedList<TreeNode>();
         q.offer(root);
         int count = 0;
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int size = q.size();
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                if(node.left != null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
+                if (node.left != null) {
+		    q.offer(node.left);
+		}
+                if (node.right != null) {
+		    q.offer(node.right);
+		}
             }
             count++;
         }
