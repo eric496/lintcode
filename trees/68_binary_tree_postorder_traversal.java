1736eeb494db7c47b90a0cae9931543118581b9b
eric496 <eric.mlengineer@gmail.com>
2017-11-05 12:51:21 -0500

    add comment

--- trees/68_binary_tree_postorder_traversal.java
+++ trees/68_binary_tree_postorder_traversal.java
@@ -12,6 +12,26 @@ Example
     return [3,2,1].
 */
 
+/*
+Thought process:
+    0. What is postorder traversal?
+           Traverse the nodes in the tree in left-right-root order.
+           For example, 
+                        A
+                      /   \
+                     B     C
+                    / \   / \
+                   D   F G   H
+                  / \     \
+                 I   J     K
+           The postorder traversal is I-J-D-F-B-K-G-H-C-A.
+    1. Recursive solution:
+           Base case: return when the current node is null.
+           Recursive step: run recursively on the left child first and then the right child, add the current node value to result list. 
+    2. Iterative solution:
+           Reverse the process of preorder traversal.
+*/
+
 /**
  * Definition of TreeNode:
  * public class TreeNode {
@@ -24,15 +44,6 @@ Example
  * }
  */
 
-/*
-Thought process:
-    1. DFS:
-           Base case: return if current node is null
-           Recursive steps: run recursively on the left child first and then the right child, add the value of the current node 
-    2. Iterative method using stack:
-           Reverse operations of preorder traversal.
-*/
-
 // recursive
 public class Solution {
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
@@ -41,10 +52,10 @@ public class Solution {
         return result;
     }
     
-    private void traverse(TreeNode node, ArrayList<Integer> result) {
-        if(node == null) return;
-        traverse(node.left, result);
-        traverse(node.right, result);
+    private void dfs(TreeNode node, ArrayList<Integer> result) {
+        if (node == null) return;
+        dfs(node.left, result);
+        dfs(node.right, result);
         result.add(node.val);
     }
 }
@@ -55,14 +66,15 @@ public class Solution {
         LinkedList<Integer> result = new LinkedList<>();
         Deque<TreeNode> s = new ArrayDeque<>();
         TreeNode node = root;
-        while(!s.isEmpty() || node != null) {
-            if(node != null) {
+        while (!s.isEmpty() || node != null) {
+            // reverse the process of preorder traversal
+            if (node != null) {
                 s.push(node);
-                result.addFirst(node.val);     // Reverse the process of preorder
-                node = node.right;             // Reverse the process of preorder
+                result.addFirst(node.val); 
+                node = node.right;        
             } else {
                 node = s.pop();
-                node = node.left;              // Reverse the process of preorder
+                node = node.left;        
             }
         }
         return result;
