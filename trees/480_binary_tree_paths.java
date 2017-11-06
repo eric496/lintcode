f823ce12e383e3b1dbfa2044612222a097288534
eric496 <eric.mlengineer@gmail.com>
2017-11-06 17:39:40 -0500

    change solution

--- trees/480_binary_tree_paths.java
+++ trees/480_binary_tree_paths.java
@@ -29,25 +29,31 @@ Example
 
 /*
 Thought process:
-    1. Recursive solution
+    1. Recursive solution:
+           Base case: return when current node is null.
+           Recursive step: add the path to result list if current node has no children (a leaf node).
+                           Otherwise, run recursively on the left and the right subtrees.
 */
 
 // recursive
 public class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<String>();
-        if(root == null) return result;
+        if (root == null) return result;
         findPaths(root, String.valueOf(root.val), result);
         return result;
     }
 
     private void findPaths(TreeNode node, String path, List<String> result) {
-        if(node == null) return;
-        if(node.left == null && node.right == null) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
             result.add(path);
-            return;
         }
-        if(node.left != null) findPaths(node.left, path + "->" + String.valueOf(node.left.val), result);
-        if(node.right != null) findPaths(node.right, path + "->" + String.valueOf(node.right.val), result);
+        if (node.left != null) {
+            findPaths(node.left, path+"->"+String.valueOf(node.left.val), result);
+        }
+        if (node.right != null) {
+            findPaths(node.right, path+"->"+String.valueOf(node.right.val), result);
+        }
     }
 }
