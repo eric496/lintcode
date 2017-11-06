88b60a160640d436d0d8b7a41a2dd14cb6bfd295
eric496 <eric.mlengineer@gmail.com>
2017-11-06 17:47:52 -0500

    add 2nd solution

--- trees/661_convert_BST_to_greater_tree.java
+++ trees/661_convert_BST_to_greater_tree.java
@@ -24,18 +24,32 @@ Example
  * }
  */
 
+// method 1
 public class Solution {
-    int sum = 0;
+    private int sum = 0;
     public TreeNode convertBST(TreeNode root) {
         dfs(root);
         return root;
     }
 
-    void dfs(TreeNode cur) {
-        if(cur == null) return;
+    private void dfs(TreeNode cur) {
+        if (cur == null) return;
         dfs(cur.right);
         sum += cur.val;
         cur.val = sum;
         dfs(cur.left);
+    }
+}
+
+// method 2
+public class Solution {
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) return null;
+        convertBST(root.right);
+        root.val += sum;
+        sum = root.val;
+        convertBST(root.left);
+        return root;
     }
 }
