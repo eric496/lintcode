74974b8817b86df26331366b90a588247777d175
eric496 <eric.mlengineer@gmail.com>
2017-11-05 23:28:02 -0500

    add comment

--- trees/163_unique_binary_search_trees.java
+++ trees/163_unique_binary_search_trees.java
@@ -10,15 +10,23 @@ Example
     2     1          2                  3
 */
 
+/*
+Thought process:
+    Suppose function f(n) maps the number of values n to the number of unique BSTs. Observe that f(n) = g(1, n) + g(2, n) + g(3, n) + ... + g(n-1, n) + g(n, n)
+    where g(i, n) is a function which maps the number of values n to the number of unique BSTs given i as the root. Observe that g(i, n) = f(i-1) * f(n-i). Given 
+    i as root, the total number of unique BSTs is just the product of the number of unique BSTs using all nodes left to i and the number of unique BSTs using all nodes right to i.
+*/
+
+// DP
 public class Solution {
     public int numTrees(int n) {
-        if(n == 0) return 1;
-        int [] G = new int[n+1];
-        G[0] = G[1] = 1;
-        for(int i=2; i<=n; ++i) {
-            for(int j=1; j<=i; ++j)
-                G[i] += G[j-1] * G[i-j];
+        if (n == 0) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++)
+                dp[i] += dp[j-1] * dp[i-j];
         }
-        return G[n];
+        return dp[n];
     }
 }
