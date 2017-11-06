fabd48fa90d4546daf45e18c9d8e5129eabba1a0
eric496 <eric.mlengineer@gmail.com>
2017-11-06 00:05:06 -0500

    add solution

--- trees/376_binary_tree_path_sum.java
+++ trees/376_binary_tree_path_sum.java
@@ -33,30 +33,56 @@ Thought process:
  * }
  */
 
+// method 1
 public class Solution {
     public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
     	List<List<Integer>> result = new ArrayList<List<Integer>>();
     	if (root == null) return result;
     	ArrayList<Integer> path = new ArrayList<Integer>();
     	path.add(root.val);
-    	traversal(result, path, root, root.val, target);
+    	traverse(result, path, root, root.val, target);
     	return result;
     }
 
-    public void traversal(List<List<Integer>> result, ArrayList<Integer> path, TreeNode node, int currentSum, int target) {
+    public void traverse(List<List<Integer>> result, ArrayList<Integer> path, TreeNode node, int currentSum, int target) {
     	if (node.left == null && node.right == null) {
-    	    if(currentSum == target) result.add(new ArrayList<Integer>(path));
+    	    if (currentSum == target) {
+		result.add(new ArrayList<Integer>(path));
+	    }
     	    return;
     	}
     	if (node.left != null) {
     	    path.add(node.left.val);
-    	    traversal(result, path, node.left, currentSum + node.left.val, target);
-    	    path.remove(path.size() - 1);
+    	    traverse(result, path, node.left, currentSum+node.left.val, target);
+    	    path.remove(path.size()-1);
     	}
     	if (node.right != null) {
     	    path.add(node.right.val);
-    	    traversal(result, path, node.right, currentSum + node.right.val, target);
-    	    path.remove(path.size() - 1);
+    	    traverse(result, path, node.right, currentSum+node.right.val, target);
+    	    path.remove(path.size()-1);
     	}
+    }
+}
+
+// method 2
+public class Solution {
+    public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
+    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+	if (root == null) return result; 
+        List<Integer> path = new ArrayList<Integer>(); 
+        traverse(root, target, path, result);
+        return result;
+    }
+    
+    public void traverse(TreeNode root, int target, List<Integer> path, List<List<Integer>> result){
+        if (root == null) return; 
+        path.add(root.val);
+        if (root.left == null && root.right == null && root.val == target){
+            result.add(new ArrayList(cur));
+        } else {
+            traverse(root.left, target-root.val, path, result);
+            traverse(root.right, target-root.val, path, result);
+        }
+        path.remove(path.size()-1);
     }
 }
