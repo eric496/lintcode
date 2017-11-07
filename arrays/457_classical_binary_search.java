76daed68bfa9fc731bbc39ae3c4513e86b09ce22
eric496 <eric.mlengineer@gmail.com>
2017-11-07 15:05:49 -0500

    add comment

--- arrays/457_classical_binary_search.java
+++ arrays/457_classical_binary_search.java
@@ -2,29 +2,32 @@
 Find any position of a target number in a sorted array. Return -1 if target does not exist.
 
 Example
-Given [1, 2, 2, 4, 5, 5].
-For target = 2, return 1 or 2.
-For target = 5, return 4 or 5.
-For target = 6, return -1.
+    Given [1, 2, 2, 4, 5, 5].
+    For target = 2, return 1 or 2.
+    For target = 5, return 4 or 5.
+    For target = 6, return -1.
 */
 
 /*
 Thought process: 
-    Set high = mid -1 and low = mid + 1 because nums[mid] is strictly greater than or less than target.
+    Think carefully about the edge case for while loop condition (low < high or low <= high or does not matter?) 
+    Use small + (large - small) / 2 to get the middle point because it can avoid integer overflow for large integer.
 */
 
 public class Solution {
     public int findPosition(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0) return -1;
         int low = 0, high = nums.length - 1;
-        while(low <= high) {
+        // same if low < high
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if(nums[mid] == target)
+            if (nums[mid] == target) {
                 return mid;
-            else if(nums[mid] > target)
+	    } else if (nums[mid] > target) {
                 high = mid - 1;
-            else if(nums[mid] < target) 
+	    } else if (nums[mid] < target) {
                 low = mid + 1;
+	    }
         }
         return -1;
     }
