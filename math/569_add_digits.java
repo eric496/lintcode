dc643487616c1724e6785cbf1036e767dca8d67e
eric496 <eric.mlengineer@gmail.com>
2017-11-07 16:53:34 -0500

    add comment

--- math/569_add_digits.java
+++ math/569_add_digits.java
@@ -4,17 +4,21 @@ Given a non-negative integer num, repeatedly add all its digits until the result
 Example
     Given num = 38.
     The process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return 2.
+
+Challenge
+    Could you do it without any loop/recursion in O(1) runtime?
 */
 
 public class Solution {
     public int addDigits(int num) {
-        while(num / 10 != 0) {
-            int ans = 0;
-            while(num != 0) {
-                ans += num % 10;
+        while (num / 10 != 0) {
+            int sum = 0;
+            while (num != 0) {
+                sum += num % 10;
                 num /= 10;
             }
-            num = ans;
+            // update num value by its digit sum
+            num = sum;
         }
         return num;
     }
