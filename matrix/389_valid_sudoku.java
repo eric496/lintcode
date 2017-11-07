15a38d36f4a2c7a03d71a5a887d9420e8c342860
eric496 <eric.mlengineer@gmail.com>
2017-11-06 22:18:03 -0500

    add 2nd solution

--- matrix/389_valid_sudoku.java
+++ matrix/389_valid_sudoku.java
@@ -6,10 +6,10 @@ Notice
     A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 */
 
+// method 1
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         if (board == null || board.length == 0) return false;
-        
         // check row
         for (int i = 0; i < 9; i++) {
             boolean[] numUsed = new boolean[9];
@@ -17,7 +17,6 @@ class Solution {
                 if (isDuplicate(board[i][j], numUsed)) return false;
             }
         }
-
         // check column
         for (int i = 0; i < 9; i++) {
             boolean[] numUsed = new boolean[9];
@@ -25,7 +24,6 @@ class Solution {
                 if (isDuplicate(board[j][i], numUsed)) return false;
             }
         }
-
         // check sub box
         for (int i = 0; i < 9; i = i + 3) {
             for (int j = 0; j < 9; j = j + 3) {
@@ -39,9 +37,7 @@ class Solution {
         boolean[] numUsed = new boolean[9];
         for (int i = x; i < x + 3; i++) {
             for (int j = y; j < y + 3; j++) {
-                if (isDuplicate(box[i][j], numUsed)) {
-                    return false;
-                }
+                if (isDuplicate(box[i][j], numUsed)) return false;
             }
         }
         return true;
@@ -50,11 +46,30 @@ class Solution {
     private boolean isDuplicate(char c, boolean[] numUsed) {
         if (c == '.') {
             return false;
-        } else if (numUsed[c - '1']) {
+        } else if (numUsed[c-'1']) {
             return true;
         } else {
-            numUsed[c - '1'] = true;
+            numUsed[c-'1'] = true;
             return false;
         }
+    }
+}
+
+// method 2
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            HashSet<Character> rows = new HashSet<Character>();
+            HashSet<Character> cols = new HashSet<Character>();
+            HashSet<Character> cube = new HashSet<Character>();
+            for (int j = 0; j < 9; j++){
+                if (board[i][j]!='.' && !rows.add(board[i][j])) return false;
+                if (board[j][i]!='.' && !cols.add(board[j][i])) return false;
+                int rowIndex = 3 * (i / 3);
+                int colIndex = 3 * (i % 3);
+                if (board[rowIndex + j/3][colIndex+j%3]!='.' && !cube.add(board[rowIndex+j/3][colIndex+j%3])) return false;
+            }
+        }
+        return true;
     }
 }
