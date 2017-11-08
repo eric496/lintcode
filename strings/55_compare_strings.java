7421dcc3d86477e09ef60d0b5cb394706b8bf1e9
eric496 <eric.mlengineer@gmail.com>
2017-11-08 17:02:35 -0500

    change solution

--- strings/55_compare_strings.java
+++ strings/55_compare_strings.java
@@ -12,19 +12,20 @@ For A = "ABCD", B = "AABC", return false.
 
 /*
 Thought process:
-    1. If the characters are all in ASCII, use an extra array with size 256.
-    2. If unicode, use a hash map.
+    If the characters are all in ASCII, use an extra array with size 256. Use a map otherwise.
 */
 
 // O(n) time and O(1) space by using array
 public class Solution {
     public boolean compareStrings(String A, String B) {
-        if(A.length() < B.length() || A == null || B == null) return false;
+        if (A.length() < B.length() || A == null || B == null) return false;
         int[] count = new int[256];
-        for(char c : A.toCharArray()) count[(int)c]++;
-        for(char c : B.toCharArray()) {
+        for (char c : A.toCharArray()) {
+	    count[(int)c]++;
+	}
+        for (char c : B.toCharArray()) {
             count[(int)c]--;
-            if(count[(int)c] < 0) return false;
+            if (count[(int)c] < 0) return false;
         }
         return true;
     }
@@ -33,19 +34,19 @@ public class Solution {
 // O(n) time and O(n) space by using hash map
 public class Solution {
     public boolean compareStrings(String A, String B) {
-        if(A.length() < B.length() || A == null || B == null) return false;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for(char c : A.toCharArray()) {
-            if(map.containsKey(c)) {
+        if (A.length() < B.length() || A == null || B == null) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : A.toCharArray()) {
+            if (map.containsKey(c)) {
                 map.put(c, map.get(c)+1);
             } else {
                 map.put(c, 1);
             }
         }
-        for(char c : B.toCharArray()) {
-            if(map.containsKey(c)) {
+        for (char c : B.toCharArray()) {
+            if (map.containsKey(c)) {
                 map.put(c, map.get(c)-1);
-                if(map.get(c) < 0) return false;
+                if (map.get(c) < 0) return false;
             } else {
                 return false;
             }
