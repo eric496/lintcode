bbf5e4500c1101b53cbca4d74fa2ffe212ec2f2d
eric496 <eric.mlengineer@gmail.com>
2017-11-09 01:07:11 -0500

    change solution

--- strings/646_first_position_unique_character.java
+++ strings/646_first_position_unique_character.java
@@ -2,56 +2,64 @@
 Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
 Example
-Given s = "lintcode", return 0.
-Given s = "lovelintcode", return 2.
+    Given s = "lintcode", return 0.
+    Given s = "lovelintcode", return 2.
 */
 
 /*
 Thought process:
-    1. If all characters are in ASCII set, we can do two passes: first one to populate
-       the array with count of appearance for each character in the string. Second loop 
-       to find the first non-repeating character, if its count equals one. 
-    2. If the problem applies to unicode, we should use hash map instead.
+    1. Use an array of size 256 if given ASCII. 
+       We can complete it with two passes: first pass stores the character frequency. 
+       Second pass looks for the first non-repeating character, where the frequency is 1. 
+    2. Use hash map if given a much larger or complicated encoding standard.
 */
 
-// O(n) time and O(1) space - with fixed size array
+// O(n) time and O(1) space using array
 public class Solution {
     public int firstUniqChar(String s) {
-	if(s == null || s.length() == 0) return -1;
+	if (s == null || s.length() == 0) return -1;
         int[] count = new int[256];
-        for(int i = 0; i < s.length(); i++) count[s.charAt(i)]++;
-        for(int i = 0; i < s.length(); i++) 
-	    if(count[s.charAt(i)] == 1) return i;
+        for (int i = 0; i < s.length(); i++) {
+	    count[s.charAt(i)]++;
+	}
+        for (int i = 0; i < s.length(); i++) { 
+	    if (count[s.charAt(i)] == 1) return i;
+	}
         return -1;
     }
 }
 
 
-// method 2
+// Same with the above method but more concise using for-each method
 public class Solution {
     public int firstUniqChar(String s) {
         if(s == null || s.length() == 0) return -1;
         int[] count = new int[256];
-        for(char c : s.toCharArray()) count[c] += 1;
-        for(char c : s.toCharArray())
-            if(count[c] == 1) return s.indexOf(c);
+        for (char c : s.toCharArray()) {
+	    count[c]++;
+	}
+        for (char c : s.toCharArray()) {
+            if (count[c] == 1) return s.indexOf(c);
+	}
         return -1;
     }
 }
 
-// Hashmap
+// O(n) time and O(n) space using hashmap
 public class Solution {
     public int firstUniqChar(String s) {
-        if(s == null || s.length() == 0) return -1;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for(char c : s.toCharArray()) {
-            if(map.containsKey(c)) 
+        if (s == null || s.length() == 0) return -1;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
                 map.put(c, map.get(c)+1);
-            else
+            } else {
                 map.put(c, 1);
+	    }
         }
-        for(char c : s.toCharArray())
+        for (char c : s.toCharArray()) {
             if(map.get(c) == 1) return s.indexOf(c);
+	}
         return -1;
     }
 }
