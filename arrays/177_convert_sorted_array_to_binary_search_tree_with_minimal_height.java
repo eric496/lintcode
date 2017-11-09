24cab5768fe42c3573236b97bb6d630d60069b2f
eric496 <eric.mlengineer@gmail.com>
2017-11-09 00:49:49 -0500

    change solution

--- arrays/177_convert_sorted_array_to_binary_search_tree_with_minimal_height.java
+++ arrays/177_convert_sorted_array_to_binary_search_tree_with_minimal_height.java
@@ -2,16 +2,20 @@
 Given a sorted (increasing order) array, Convert it to create a binary tree with minimal height.
 
 Notice
-There may exist multiple valid solutions, return any of them.
+    There may exist multiple valid solutions, return any of them.
 
 Example
-Given [1,2,3,4,5,6,7], return
+    Given [1,2,3,4,5,6,7], return
+         4
+       /   \
+      2     6
+     / \    / \
+    1   3  5   7
+*/
 
-     4
-   /   \
-  2     6
- / \    / \
-1   3  5   7
+/*
+Thought process:
+    In order to make the binary tree minimal height, we will make the tree as balanced as possible by splitting the array in half and build the tree recursively.
 */
 
 /**
@@ -26,21 +30,18 @@ Given [1,2,3,4,5,6,7], return
  * }
  */
 
-/*
-1. Build a tree recursively
-*/
-
 public class Solution {
-    public TreeNode sortedArrayToBST(int[] A) {  
-        if(A == null || A.length == 0) return null;
-        return buildTree(A, 0, A.length - 1);
-    }  
+    public TreeNode sortedArrayToBST(int[] A) {
+        if (A == null || A.length == 0) return null;
+        return buildTree(A, 0, A.length-1);
+    }
     
-    public TreeNode buildTree(int[] A, int start, int end) {
-        if(start > end) return null;
-        TreeNode node = new TreeNode(A[start + (end - start) / 2]);
-        node.left = buildTree(A, start, start + (end - start) / 2 - 1);
-        node.right = buildTree(A, start + (end - start) / 2 + 1, end);
+    private TreeNode buildTree(int[] A, int low, int high) {
+        if (low > high) return null; 
+        int mid = low + (high - low) / 2;
+        TreeNode node = new TreeNode(A[mid]);
+        node.left = buildTree(A, low, mid-1);
+        node.right = buildTree(A, mid+1, high);
         return node;
     }
-} 
+}
