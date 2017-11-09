abd7b888fb7a285646884040b28e9c275da34160
eric496 <eric.mlengineer@gmail.com>
2017-11-09 10:59:26 -0500

    add solution

--- trees/453_flatten_binary_tree_to_linked_list.java
+++ trees/453_flatten_binary_tree_to_linked_list.java
@@ -58,6 +58,23 @@ public class Solution {
     }
 }
 
+// equivalent but more OOD-friendly
+public class Solution {
+    public void flatten(TreeNode root) {
+        flatten(root, null);
+    }
+
+    private TreeNode flatten(TreeNode root, TreeNode pre) {
+        if (root==null) return pre;
+        pre = flatten(root.right, pre);    
+        pre = flatten(root.left, pre);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+        return pre;
+    }
+}
+
 // recursive 2 (top-down)
 public class Solution {
     public void flatten(TreeNode root) {
