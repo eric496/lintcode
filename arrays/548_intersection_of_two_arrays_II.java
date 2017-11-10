37b187a4fc3eecc59ba4d21cf7d15242adfb4ab9
eric496 <eric.mlengineer@gmail.com>
2017-11-10 17:02:07 -0500

    change soution

--- arrays/548_intersection_of_two_arrays_II.java
+++ arrays/548_intersection_of_two_arrays_II.java
@@ -18,61 +18,66 @@ Challenge
 Thought process: 
     1. Similar to 547, but keep track of the number of appearance of each element by using 
        hash map instead of hash set to store <number, count> pairs.
-    2. Similar to 547, use two pointers to iterate both arrays and select the intersection.
+    2. Similar to 547, use two pointers to iterate both arrays and store the intersection.
 */
 
 // O(n) time and O(n) space 
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        if(nums1 == null || nums2 == null) return null;
-        if(nums1.length == 0) return nums1;
-        if(nums2.length == 0) return nums2;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        List<Integer> li = new ArrayList<Integer>();
+        if (nums1 == null || nums2 == null) return null;
+        if (nums1.length == 0) return nums1;
+        if (nums2.length == 0) return nums2;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> resultList = new ArrayList<>();
         // first pass maps numbers to the number of occurence 
-        for(int n : nums1) {
-            if(map.containsKey(n))
+        for (int n : nums1) {
+            if (map.containsKey(n)) {
                 map.put(n, map.get(n)+1);
-            else
+            } else {
                 map.put(n, 1);
+	    }
         }
         // second pass selects the intersection
-        for(int n : nums2) {
+        for (int n : nums2) {
             // it is important to check key existence first or map.get(key) might cause null pointer error
-            if(map.containsKey(n) && map.get(n) > 0) {
-                li.add(n);
+            if (map.containsKey(n) && map.get(n) > 0) {
+                resultList.add(n);
                 map.put(n, map.get(n)-1);
             }
         }
-        int[] result = new int[li.size()];
-        for(int i = 0; i < li.size(); i++) result[i] = li.get(i);
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+	    result[i] = resultList.get(i);
+	}
         return result;
     }
 }
 
-// O(nlogn) time and O(n) space 
+// O(n) time and O(n) space 
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        if(nums1 == null || nums2 == null) return null;
-        if(nums1.length == 0) return nums1;
-        if(nums2.length == 0) return nums2;
+        if (nums1 == null || nums2 == null) return null;
+        if (nums1.length == 0) return nums1;
+        if (nums2.length == 0) return nums2;
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        List<Integer> li = new ArrayList<Integer>();
+        List<Integer> resultList = new ArrayList<>();
         int ix1 = 0, ix2 = 0;
-        while(ix1 < nums1.length && ix2 < nums2.length) {
-            if(nums1[ix1] < nums2[ix2]) {
+        while (ix1 < nums1.length && ix2 < nums2.length) {
+            if (nums1[ix1] < nums2[ix2]) {
                 ix1++;
-            } else if(nums1[ix1] > nums2[ix2]) {
+            } else if (nums1[ix1] > nums2[ix2]) {
                 ix2++;
             } else {
-                li.add(nums1[ix1]);
+                resultList.add(nums1[ix1]);
                 ix1++;
                 ix2++;
             }
         }
-        int[] result = new int[li.size()];
-        for(int i = 0; i < li.size(); i++) result[i] = li.get(i);
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+	    result[i] = resultList.get(i);
+	}
         return result;
     }
 }
