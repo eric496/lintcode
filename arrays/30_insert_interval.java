bf7dfe2ef5cddf5d21adcc726351b8746ba95fb0
eric496 <eric.mlengineer@gmail.com>
2017-11-10 16:05:53 -0500

    add comment

--- arrays/30_insert_interval.java
+++ arrays/30_insert_interval.java
@@ -3,8 +3,8 @@ Given a non-overlapping interval list which is sorted by start point.
 Insert a new interval into it, make sure the list is still in order and non-overlapping (merge intervals if necessary).
 
 Example
-Insert [2, 5] into [[1,2], [5,9]], we get [[1,9]].
-Insert [3, 4] into [[1,2], [5,9]], we get [[1,2], [3,4], [5,9]].
+    Insert [2, 5] into [[1,2], [5,9]], we get [[1,9]].
+    Insert [3, 4] into [[1,2], [5,9]], we get [[1,2], [3,4], [5,9]].
 */
 
 /**
@@ -23,8 +23,8 @@ Thought process
         1) new interval is strictly greater than the current interval (newInterval.start > i.end)
         2) new interval is strictly smaller than the current interval (newInterval.end < i.start)
         3) new interval is overlapping with the current interval (newInterval.end >= i.start or newInterval.start <= i.end)
-    Set a position pointer to keep track of the insertion position so we can insert interval outside the for loop.
-    The position pointer increments only in case 2).
+    Set a position pointer to keep track of the insert position so we can insert interval outside the for loop.
+    Increment position pointer if 1) occurs.
     For case 3), update the new interval start with new interval start or current interval start, whichever is smaller. 
     Similarly, update the new interval end with new interval end or current interval end, whichever is greater.
 */
@@ -33,13 +33,13 @@ public class Solution {
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
         if (intervals == null || newInterval == null) return intervals;
         List<Interval> result = new ArrayList<>();
-        int pos = 0;
+        int insertPos = 0;
         for (Interval i : intervals) {
             // current interval is strictly smaller than new interval
             // insert current interval to result list and increment insert position index by 1
             if (newInterval.start > i.end) {
                 result.add(i);
-                pos++;
+                insertPos++;
             // current interval is strictly greater than new interval
             // insert current interval to result list but do not change insert position index
             } else if (newInterval.end < i.start) {
@@ -52,7 +52,7 @@ public class Solution {
             }
         }
         // insert new interval or merged interval 
-        result.add(pos, newInterval);
+        result.add(insertPos, newInterval);
         return result;
     }
 }
