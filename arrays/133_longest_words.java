18776ff7727b3420c81b68612c975e2611679279
eric496 <eric.mlengineer@gmail.com>
2017-11-11 20:09:18 -0500

    change solution

--- arrays/133_longest_words.java
+++ arrays/133_longest_words.java
@@ -27,40 +27,48 @@ Challenge
 /*
 Thought process
     1. A trivial solution: two passes
-           First pass counts the max length of all words.
-           Second pass adds all strings of the max length to the result list. 
+           First pass counts the max length of all words. Second pass adds all strings of the max length to the result list. 
     2. One-pass method:
-           Iterate the dictionary, if the length of the current string is greater than that of the strings in the result list, then clear the result list and add the current string.
+           Iterate the dictionary, if the length of the current string is greater than that of the strings in the result list, 
+           then clear the result list and add the current string and update the max length.
            If the length of the current string equals that of the strings in the result list, then append it to the result list.
 */
 
-//two passes
+// two passes
 class Solution {
     public ArrayList<String> longestWords(String[] dictionary) {
-        if(dictionary == null || dictionary.length == 0) return null;
-        int max = 0;
-        for(String s : dictionary)
-            if(s.length() > max) max = s.length();
-        ArrayList<String> res = new ArrayList<String>();
-        for(String s : dictionary)
-            if(s.length() == max) res.add(s);
-        return res;
+        ArrayList<String> result = new ArrayList<>();
+        if (dictionary == null || dictionary.length == 0) return result;
+        int maxLen = 0;
+        for (String s : dictionary) {
+            if (s.length() > maxLen) {
+		maxLen = s.length();
+	    }
+	}
+        for (String s : dictionary) {
+            if (s.length() == max) {
+		result.add(s);
+	    }
+	}
+        return result;
     }
 }
 
-//one pass
+// one pass
 class Solution {
     public ArrayList<String> longestWords(String[] dictionary) {
-        if(dictionary == null || dictionary.length == 0) return null;
-        ArrayList<String> res = new ArrayList<String>();
-        for(String s : dictionary) {
-            if(res.isEmpty() || s.length() > res.get(0).length()) {
-                res.clear();
-                res.add(s);
-            } else if(s.length() == res.get(0).length()) {
-                res.add(s);
+	ArrayList<String> result = new ArrayList<>();
+        if (dictionary == null || dictionary.length == 0) return result;
+        int maxLen = 0;
+        for (String s : dictionary) {
+            if (s.length() > maxLen) {
+                result.clear();
+                result.add(s);
+		maxLen = s.length();
+            } else if (s.length() == maxLen) {
+                result.add(s);
             }
         }
-        return res;
+        return result;
     }
 }
