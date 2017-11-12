44ac9a10edd26283a25659043060c7c4d1fe9711
eric496 <eric.mlengineer@gmail.com>
2017-11-11 21:16:19 -0500

    modify solution

--- arrays/156_merge_intervals.java
+++ arrays/156_merge_intervals.java
@@ -2,50 +2,52 @@
 Given a collection of intervals, merge all overlapping intervals.
 
 Example
-Given intervals => merged intervals:
-[                     [
-  [1, 3],               [1, 6],
-  [2, 6],      =>       [8, 10],
-  [8, 10],              [15, 18]
-  [15, 18]            ]
-]
+    Given intervals => merged intervals:
+    [                     [
+      [1, 3],               [1, 6],
+      [2, 6],      =>       [8, 10],
+      [8, 10],              [15, 18]
+      [15, 18]            ]
+    ]
+
+Challenge
+    O(n log n) time and O(1) extra space.
 */
 
 /*
 Thought process
-1. Sort the interval list by interval start values.
-2. Two pointers: if current start value is smaller than the end value, 
-   compare the end value with the current end value, store the greater one.
-   If current start value is greater than the end value, 
-   add interval to the result list and update the start and end value.
-3. DO NOT forget to add the last interval to the result list.
+    Sort the interval list by interval start values.
+    Two pointers: If current start value is smaller than the end value, compare the end value with the current end value, store the greater one.
+                  If current start value is greater than the end value, add interval to the result list and update the start and end value.
+    DO NOT forget to add the last interval to the result list.
 */
 
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         if(intervals == null || intervals.size() <= 1) return intervals;
-        
+	intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+        /*
         Collections.sort(intervals, new Comparator<Interval>() {
             @Override
             public int compare(Interval i1, Interval i2) {
                 return i1.start - i2.start;
             }
         });
-        
-        List<Interval> res = new LinkedList<Interval>();
+        */
+        List<Interval> result = new LinkedList<Interval>();
         int start = intervals.get(0).start;
         int end = intervals.get(0).end;
         for (Interval i : intervals) {
             if (i.start <= end) {
                 end = Math.max(end, i.end);
             } else {
-                res.add(new Interval(start, end));
+                result.add(new Interval(start, end));
                 start = i.start;
                 end = i.end;
             }
         }
         //add the last interval!
-        res.add(new Interval(start, end));
-        return res;
+        result.add(new Interval(start, end));
+        return result;
     }
 }
--- arrays/80_median.java
+++ arrays/80_median.java
@@ -4,8 +4,8 @@ A median is the middle number of the array after it is sorted.
 If there are even numbers in the array, return the N/2-th number after sorted.
 
 Example
-Given [4, 5, 1, 2, 3], return 3.
-Given [7, 9, 4, 5], return 5.
+    Given [4, 5, 1, 2, 3], return 3.
+    Given [7, 9, 4, 5], return 5.
 */
 
 public class Solution {
