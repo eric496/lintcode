24c31520687480b73a2325653efeac4d4966d9d6
eric496 <eric.mlengineer@gmail.com>
2017-11-12 21:18:03 -0500

    modify solution

--- arrays/156_merge_intervals.java
+++ arrays/156_merge_intervals.java
@@ -17,15 +17,16 @@ Challenge
 /*
 Thought process
     Sort the interval list by interval start values.
-    Two pointers: If current start value is smaller than the end value, compare the end value with the current end value, store the greater one.
-                  If current start value is greater than the end value, add interval to the result list and update the start and end value.
+    Two pointers: Compare the current interval with the previous interval.
+		  If current start value is smaller than the previous end value (overlapping occurs), compare the previous end value with the current end value, store the greater one.
+                  If current start value is greater than the end value, add the previous interval to the result list and update the previous start and end value with the current ones.
     DO NOT forget to add the last interval to the result list.
 */
 
 class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         if(intervals == null || intervals.size() <= 1) return intervals;
-	intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
         /*
         Collections.sort(intervals, new Comparator<Interval>() {
             @Override
@@ -35,19 +36,19 @@ class Solution {
         });
         */
         List<Interval> result = new LinkedList<Interval>();
-        int start = intervals.get(0).start;
-        int end = intervals.get(0).end;
-        for (Interval i : intervals) {
-            if (i.start <= end) {
-                end = Math.max(end, i.end);
+        int prevStart = intervals.get(0).start;
+        int prevEnd = intervals.get(0).end;
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval curInterval = intervals.get(i);
+            if (curInterval.start <= prevEnd) {
+                prevEnd = Math.max(curInterval.end, prevEnd);
             } else {
-                result.add(new Interval(start, end));
-                start = i.start;
-                end = i.end;
+                result.add(new Interval(prevStart, prevEnd));
+                prevStart = curInterval.start;
+                prevEnd = curInterval.end;
             }
         }
-        //add the last interval!
-        result.add(new Interval(start, end));
+        result.add(new Interval(prevStart, prevEnd));
         return result;
     }
 }
