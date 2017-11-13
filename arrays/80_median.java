ef723be24960ebd1896f0c57cc7110c428d97a38
eric496 <eric.mlengineer@gmail.com>
2017-11-12 21:05:54 -0500

    add comment

--- arrays/80_median.java
+++ arrays/80_median.java
@@ -6,12 +6,15 @@ If there are even numbers in the array, return the N/2-th number after sorted.
 Example
     Given [4, 5, 1, 2, 3], return 3.
     Given [7, 9, 4, 5], return 5.
+
+Challenge
+    O(n) time.
 */
 
 public class Solution {
     public int median(int[] nums) {
         if (nums == null || nums.length == 0) return -1;
-        return quickSort(nums, 0, nums.length - 1, (nums.length + 1) / 2);
+        return quickSort(nums, 0, nums.length-1, (nums.length+1)/2);
     }
 
     private int quickSort(int[] nums, int low, int high, int size) {
@@ -31,9 +34,9 @@ public class Solution {
         if (mid - low + 1 == size) {
             return nums[mid];
         } else if (mid - low + 1 > size) {
-            return quickSort(nums, low, mid - 1, size);
+            return quickSort(nums, low, mid-1, size);
         } else {
-            return quickSort(nums, mid + 1, high, size - (mid - low + 1));
+            return quickSort(nums, mid+1, high, size-(mid-low+1));
         }
     }
 }
