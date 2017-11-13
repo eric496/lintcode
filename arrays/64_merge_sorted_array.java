3604c6d963fbc6789d470c2a182c1f55bd8c9d80
eric496 <eric.mlengineer@gmail.com>
2017-11-12 21:22:21 -0500

    add comment

--- arrays/64_merge_sorted_array.java
+++ arrays/64_merge_sorted_array.java
@@ -11,20 +11,20 @@ Example
 
 /*
 Thought process:
-    In-place solution: It is hard to compare elements and store them in A in forward order. But since A has enough space at the end,
-    we can compare elements from backward. 
-    Do a pair comparison of each two elements from the end, store the larger one at the end of A, whose position pointer should 
-    accordingly move one step. 
+    In-place solution: It is hard to compare elements and store them in A in forward order. But since A has enough space at the end, we can compare elements from backward. 
+    Do a pair comparison between each two elements starting from the end, at the end of A stores the larger one, whose position pointer should accordingly move backward by one step. 
     !! If there are more elements in B than A, we need to append the extra elements from B to A. 
-    On the other hand, we don't have to worry the extra elements in A if A has more elements than B, because they are already in position.
+    On the other hand, we don't have to worry the extra elements in A if A has more elements than B, because they are already in the correct order (sorted!).
 */
 
 class Solution {
     public void mergeSortedArray(int[] A, int m, int[] B, int n) {
         int ixa = m - 1, ixb = n - 1, ix = m + n - 1;
-        while(ixa >= 0 && ixb >= 0) 
+        while (ixa >= 0 && ixb >= 0) {
             A[ix--] = A[ixa] > B[ixb] ? A[ixa--] : B[ixb--];
-        while(ixb >= 0) 
+	}
+        while (ixb >= 0) {
             A[ix--] = B[ixb--];
+	}
     }
 }
