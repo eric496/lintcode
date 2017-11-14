dc44e34743acaf4dd7b40c2aeb74b1eacd10dd07
eric496 <eric.mlengineer@gmail.com>
2017-11-14 09:23:31 -0500

    add commnet

--- bit_manipulation/142_O1_check_power_of_2.java
+++ bit_manipulation/142_O1_check_power_of_2.java
@@ -8,16 +8,14 @@ Example
 
 /*
 Thought process:
-    1. O(1) time -> we should use bit operations.
-    2. If an integer n is a power of 2, then there is only one 1 in its binary form. 
-       n - 1 is flipping all its bits. n & (n - 1) will always be 0 if n is a power of 2. 
-    3. In general, say n is just a random integer, what n & (n-1) does is just unset 
-       its rightmost set bit (which means switching its rightmost 1 to 0).
+    O(1) time -> we should use bit operations.
+    If an integer n is a power of 2, then there is only one 1 in its binary form. (n-1) is flipping all its bits. n & (n-1) will always be 0 if n is a power of 2. 
+    In general, given a random integer n, what n & (n-1) does is just unset its rightmost set bit (which means switching its rightmost 1 to 0).
 */
 
 class Solution {
     public boolean checkPowerOf2(int n) {
-        if(n <= 0) return false;
-        return (n & (n - 1)) == 0 ? true : false;
+        if (n <= 0) return false;
+        return (n & (n-1)) == 0 ? true : false;
     }
 }
