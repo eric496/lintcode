0d221f01cdaa1a468d9fe4c143b58ab4da4fc90f
eric496 <eric.mlengineer@gmail.com>
2017-11-14 14:12:55 -0500

    modify solution

--- arrays/464_sort_integers_II.java
+++ arrays/464_sort_integers_II.java
@@ -2,7 +2,7 @@
 Given an integer array, sort it in ascending order. Use quick sort, merge sort, heap sort or any O(nlogn) algorithm.
 
 Example
-Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
+    Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
 */
 
 /*
@@ -15,17 +15,17 @@ Thought process:
 
 public class Solution {
     public void sortIntegers2(int[] A) {
-        quickSort(A, 0, A.length - 1);
+        quickSort(A, 0, A.length-1);
     }
     
     private void quickSort(int[] A, int start, int end) {
         if(start >= end) return;
         int left = start, right = end; 
-        int pivot = A[start + (end - start) / 2];
-        while(left <= right) {
-            while(left <= right && A[left] < pivot) left++;
-            while(left <= right && A[right] > pivot) right--;
-            if(left <= right) {
+        int pivot = A[start+(end-start)/2];
+        while (left <= right) {
+            while (left <= right && A[left] < pivot) left++;
+            while (left <= right && A[right] > pivot) right--;
+            if (left <= right) {
                 int temp = A[left];
                 A[left] = A[right];
                 A[right] = temp;
