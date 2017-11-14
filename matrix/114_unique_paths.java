f3ad5e5db123b6b12fa6921c8a710a9dcf05e18f
eric496 <eric.mlengineer@gmail.com>
2017-11-14 18:54:50 -0500

    add comment

--- matrix/114_unique_paths.java
+++ matrix/114_unique_paths.java
@@ -13,22 +13,26 @@ Example
 
 /*
 Thought process:
-    Similar to 111. Climb Stairs and 114. Minimum Path Sum. The total number of possible paths for the current position is
-    the sum of the number of possible paths of its left position and that of its top position,
-    because only left and down moves are allowed.
-    Remember to handle the corner cases - first row and column as they have only one possible path, because they have no top or left elements.  
+    Similar to 111. Climb Stairs and 114. Minimum Path Sum. 
+    The total number of possible paths for the current position is the sum of the number of possible paths of its left position and that of its top position, because only left and down moves are allowed.
+    Remember to handle the corner cases - first row and column as they have only one possible path - they have no top or left elements.  
 */
 
 public class Solution {
     public int uniquePaths(int m, int n) {
         if (m <= 1 || n <= 1) return 1;
         int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) dp[i][0] = 1;
-        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 0; i < m; i++) {
+	    dp[i][0] = 1;
+	}
+        for (int i = 0; i < n; i++) {
+	    dp[0][i] = 1;
+	}
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++)
+            for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+	    }
         }
-        return dp[m - 1][n - 1];
+        return dp[m-1][n-1];
     }
 }
