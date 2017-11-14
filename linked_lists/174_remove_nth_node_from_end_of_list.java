98562ca322ad6ad9f3643e0215e3ddb9dfa5eac4
eric496 <eric.mlengineer@gmail.com>
2017-11-14 12:48:22 -0500

    add comment

--- linked_lists/174_remove_nth_node_from_end_of_list.java
+++ linked_lists/174_remove_nth_node_from_end_of_list.java
@@ -23,26 +23,27 @@ Example
 
 /*
 Thought process:
-    Similar to 166. N-th to Last Node in List. But here, we should be careful about two things:
-    	First, it is possible to remove the head node, so we need a dummy head to keep track of the first node.
-        Second, different from 166, in order to remove the nth node from the end, we need to know (n+1)th node from the end.
-        Set n_plus_one_from_the_end.next = n_plus_one_from_the_end.next.next
-        Set the second running pointer (runner in this solution) to point to the dummy head, which means it is one step behind the first running pointer (head in this solution).
-        So when the first running pointer reaches the tail, the second pointer points right to the node before n-th node from the end, which allows the operation of removal. 
+    Similar to 166. N-th to Last Node in List. But be careful about two things:
+    First, it is possible to remove the head node, so we need a dummy head to keep track of the first node.
+    Second, different from 166, in order to remove the nth node from the end, we need to know (n+1)th node from the end.
+    Compare to 166, we need to traverse (n-1) steps first, this is done by redirect the running pointer to the dummyhead which is actually one step ahead of the real head.
 */
  
 public class Solution {
     ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null || n < 1) return null;
         ListNode dummyhead = new ListNode(Integer.MIN_VALUE);
-        dummy.next = head;
-        ListNode runner = dummy;
-        for(int i = 0; i < n; i++) head = head.next;
-        while(head != null) {
+        dummyhead.next = head;
+	head = dummyhead;
+        ListNode runner = head;
+        for (int i = 0; i < n; i++) {
+	    head = head.next;
+	}
+        while (head != null) {
             head = head.next;
             runner = runner.next;
         }
         runner.next = runner.next.next;
-        return dummy.next;
+        return dummyhead.next;
     }
 }
