83abc242f801c56ed5c0a94e1e8f4743a1549a13
eric496 <eric.mlengineer@gmail.com>
2017-11-14 10:53:42 -0500

    modify solution

--- linked_lists/96_partition_list.java
+++ linked_lists/96_partition_list.java
@@ -7,6 +7,15 @@ Example
     return 1->2->2->4->3->5->null.
 */
 
+/*
+Thought process:
+    Create two lists, one storing every node whose value is less than x, the other storing those whose value is greater than or equals to x.
+    Two dummy heads and two running pointers will be needed because:
+        1) The head of the first list will be returned as the result.
+        2) The head of the second list will be appended to the tail of the first list.
+    Don't forget to set the second list's tail to null for proper ending of a linked list.
+*/
+
 /**
  * Definition for ListNode.
  * public class ListNode {
@@ -19,36 +28,26 @@ Example
  * }
  */ 
 
-/*
-Thought process:
-    Create two lists, one storing every node whose value is less than x, 
-    the other storing those whose value is greater than or equals to x.
-    Two dummy heads and two running pointers will be needed because:
-        1) The head of the first list will be returned as the result.
-        2) The head of the second list will be appended to the tail of the first list.
-        3) Two running pointers will iterate the whole list to select candidate node.
-    Don't forget to set the second list's tail to null for proper ending of a linked list.
-*/
-
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head == null) return null;
-        ListNode head1 = new ListNode(Integer.MIN_VALUE);
-        ListNode head2 = new ListNode(Integer.MIN_VALUE);
-        ListNode runner1 = head1;
-        ListNode runner2 = head2;
-        while(head != null) {
-            if(head.val < x) {
-                runner1.next = head;
-                runner1 = runner1.next;
+        if (head == null) return null;
+        ListNode smallRunner = new ListNode(-1);
+        ListNode smallDummyhead = smallRunner;
+        ListNode largeRunner = new ListNode(-1);
+        ListNode largeDummyhead = largeRunner;
+        while (head != null) {
+            if (head.val < x) {
+                smallRunner.next = head;
+                smallRunner = smallRunner.next;
             } else {
-                runner2.next = head;
-                runner2 = runner2.next;
+                largeRunner.next = head;
+                largeRunner = largeRunner.next;
             }
             head = head.next;
         }
-        runner2.next = null;
-        runner1.next = head2.next;
-        return head1.next;
+        // end the list with null or it will cause MLE
+        largeRunner.next = null;
+        smallRunner.next = largeDummyhead.next;
+        return smallDummyhead.next;
     }
-} 
+}
