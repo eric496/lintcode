e942889596e8d899956749d71c8a0b47e2dd043a
eric496 <eric.mlengineer@gmail.com>
2017-11-14 11:00:17 -0500

    add comment

--- arrays/407_plus_one.java
+++ arrays/407_plus_one.java
@@ -9,30 +9,34 @@ Example
 
 /*
 Thought process
-   Create a sum and a carry variables. Each number will be processed in 3 steps:
+   Create a sum and a carry variables. Each number in the list will be processed in 3 steps:
        1) sum = current number + carry;
        2) sum % 10 as the number at ones place / tens place / hundreds place ...
        3) carry sum / 10 to the next higher place.
    Be careful about two things: 
        First, the default value of carry is 1, because 1 will be added to the number.
        Second, iterate the array in backward order because addition operates from ones place, tens place, ...
-       Third, because of the second, the result array is in reverse order, we need to reverse it back to the correct order.  
+       Third, because of the second, the result array is in reverse order, we need to reverse it so for the correct order.  
 */
 
 public class Solution {
     public int[] plusOne(int[] digits) {
-        if(digits == null || digits.length == 0) return digits;
+        if (digits == null || digits.length == 0) return digits;
         int sum = 0, carry = 1;
         List<Integer> rev = new ArrayList<Integer>();
-        for(int i = digits.length - 1; i >= 0; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             sum = digits[i] + carry;
             carry = sum / 10;
             rev.add(sum % 10);
         }
-        if(carry != 0) rev.add(carry);
+        if (carry != 0) {
+	    rev.add(carry);
+	}
         int[] result = new int[rev.size()];
         int ix = rev.size() - 1;
-        for(int i : rev) result[ix--] = i;
+        for (int i : rev) {
+	    result[ix--] = i;
+	}
         return result;
     }
 }
