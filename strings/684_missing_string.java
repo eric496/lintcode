dcdcac8af10e20659b562d0afbd6ad3a0df3e8db
eric496 <eric.mlengineer@gmail.com>
2017-11-13 21:56:27 -0500

    add comment

--- strings/684_missing_string.java
+++ strings/684_missing_string.java
@@ -7,48 +7,61 @@ Example
     Return ["This", "an"]
 */
 
+/*
+Thought process:
+    Use a set to store all strings in sentence 2. Iterate sentence 1 and store missing strings.
+*/
+
 // duplicates do not count towards missing string.
 public class Solution {
     public List<String> missingString(String str1, String str2) {
         HashSet<String> set = new HashSet<String>();
         String[] arr1 = str1.split("\\s+");
         String[] arr2 = str2.split("\\s+");
-        if(arr1.length < arr2.length) {
+        if (arr1.length < arr2.length) {
             String[] temp = arr1;
             arr1 = arr2;
             arr2 = temp;
         }
-        for(String s : arr2) set.add(s);
+        for (String s : arr2) {
+	    set.add(s);
+	}
         List<String> result = new ArrayList<String>();
-        for(String s : arr1)
-            if(!set.contains(s)) result.add(s);
+        for (String s : arr1) {
+            if (!set.contains(s)) {
+		result.add(s);
+	    }
+	}
         return result;
     }
 }
 
-// Duplicates count towards missing string, 
+// find the missing string including duplicate, 
 // e.g. str1 = "This is is an example", str2 = "is an example", return ["This", "is"].
 public class Solution {
     public List<String> missingString(String str1, String str2) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         String[] arr1 = str1.split("\\s+");
         String[] arr2 = str2.split("\\s+");
-        if(arr1.length < arr2.length) {
+        if (arr1.length < arr2.length) {
             String[] temp = arr1;
             arr1 = arr2;
             arr2 = temp;
         }
-        for(String s : arr2) {
-            if(map.containsKey(s))
+        for (String s : arr2) {
+            if (map.containsKey(s)) {
                 map.put(s, map.get(s)+1);
-            else 
+            } else { 
                 map.put(s, 1);
+	    }
         }
         List<String> result = new ArrayList<String>();
-        for(String s : arr1) {
-            if(map.containsKey(s)) {
+        for (String s : arr1) {
+            if (map.containsKey(s)) {
                 map.put(s, map.get(s)-1);
-                if(map.get(s) < 0) result.add(s);
+                if (map.get(s) < 0) {
+		    result.add(s);
+		}
             } else {
                 result.add(s);
             }
