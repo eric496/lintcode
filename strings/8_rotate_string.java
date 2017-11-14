a3dd5942dac9085e6cfb6a9e1e97b8af3bd73c51
eric496 <eric.mlengineer@gmail.com>
2017-11-14 13:03:00 -0500

    modify solution

--- strings/8_rotate_string.java
+++ strings/8_rotate_string.java
@@ -14,28 +14,29 @@ Challenge
 
 /*
 Thought process:
-    1. Two corner cases: 
-           1) Return if str is null or str is empty.
-           2) When offset is greater than the length of the string, take the modulo.
-    2. Use the three-step reversal. 
+    Two corner cases: 
+        1) Return if str is null or str is empty.
+        2) When offset is greater than the length of the string, take the modulo.
+    Use the three-step reversal method. 
 */
 
 public class Solution {
     public void rotateString(char[] str, int offset) {
-	if(str == null || str.length == 0) return;
+	if (str == null || str.length == 0) return;
+	// normalize offest
         offset %= str.length;
         reverse(str, 0, str.length-offset-1);
         reverse(str, str.length-offset, str.length-1);
         reverse(str, 0, str.length-1);
     }
     
-    private void reverse(char[] str, int start, int end) {
-        while(start < end) {
-            char temp = str[start];
-            str[start] = str[end];
-            str[end] = temp;
-            start++;
-            end--;
+    private void reverse(char[] str, int low, int high) {
+        while (low < high) {
+            char temp = str[low];
+            str[low] = str[high];
+            str[high] = temp;
+            low++;
+            high--;
         }
     }
 }
