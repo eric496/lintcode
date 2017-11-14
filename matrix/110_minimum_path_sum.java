fa42b9e7ebc5151a861612aeb0573b7f9ad197ad
eric496 <eric.mlengineer@gmail.com>
2017-11-13 21:45:30 -0500

    add comment

--- matrix/110_minimum_path_sum.java
+++ matrix/110_minimum_path_sum.java
@@ -8,11 +8,8 @@ Notice
 /*
 Thought process:
     DP: Create a 2D array to store the minimum sum so far at each position.
-        Since we can move either right or down, the basic idea to calculate minimum sum so far is by adding the current element 
-        to minimum sum so far at its left position or its top position, whichever is smaller.
-        Remember to handle the first row and the first column separately as they have only one direction to move forward 
-        (right for the first row and down for the first column). This is a simplified case, because we do not need to make comparison,
-        instead we just add the current element to the previous sum. 
+        Since we can only move either right or down, the min path sum at the current position is either the current element plus the min sum at its top or left position, whichever is smaller.
+        Remember to handle the first row and the first column separately as they have only one direction to move forward (right for the first row and down for the first column).
 */
 
 // O(mn) time and O(mn) space by DP
@@ -22,14 +19,15 @@ public class Solution {
         int numRows = grid.length, numCols = grid[0].length;
         int[][] dp = new int[numRows][numCols];
         dp[0][0] = grid[0][0];
-        for(int rowix = 0; rowix < numRows; rowix++) {
-            for(int colix = 0; colix < numCols; colix++) {
-                if(rowix == 0 && colix != 0)
+        for (int rowix = 0; rowix < numRows; rowix++) {
+            for (int colix = 0; colix < numCols; colix++) {
+                if (rowix == 0 && colix != 0) {
                     dp[rowix][colix] = dp[rowix][colix-1] + grid[rowix][colix];
-                else if(colix == 0 && rowix != 0)
+                } else if (colix == 0 && rowix != 0) {
                     dp[rowix][colix] = dp[rowix-1][colix] + grid[rowix][colix];
-                else if(rowix != 0 && colix != 0)
+                } else if (rowix != 0 && colix != 0) {
                     dp[rowix][colix] = Math.min(dp[rowix-1][colix], dp[rowix][colix-1]) + grid[rowix][colix];
+		}
             }
         }
         return dp[numRows-1][numCols-1];
