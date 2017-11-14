adac60f35c12147c69643c5be8da95234ae80107
eric496 <eric.mlengineer@gmail.com>
2017-11-14 14:11:05 -0500

    modify solution

--- implementation/204_singleton.java
+++ implementation/204_singleton.java
@@ -3,18 +3,20 @@ Singleton is a most widely used design pattern. If a class has and only has one 
 You job is to implement a getInstance method for given class, return the same instance of this class every time you call this method.
 
 Example
-In Java:
-A a = A.getInstance();
-A b = A.getInstance();
-a should equal to b.
+    In Java:
+    A a = A.getInstance();
+    A b = A.getInstance();
+    a should equal to b.
 */
 
 class Solution {
     public static Solution s = null;
     public static Solution getInstance() {
-        if(s == null){
-            synchronized (Solution.class){
-                if(s == null) s = new Solution();
+        if (s == null) {
+            synchronized (Solution.class) {
+                if (s == null) {
+		    s = new Solution();
+		}
             }
         }
         return s;
