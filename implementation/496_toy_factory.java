ba2a71d633c982ce54d8a889f3ac445df1ff334a
eric496 <eric.mlengineer@gmail.com>
2017-11-14 18:21:44 -0500

    modify solution

--- implementation/496_toy_factory.java
+++ implementation/496_toy_factory.java
@@ -2,13 +2,13 @@
 Factory is a design pattern in common usage. Please implement a ToyFactory which can generate proper toy based on the given type.
 
 Example
-ToyFactory tf = ToyFactory();
-Toy toy = tf.getToy('Dog');
-toy.talk(); 
->> Wow
-toy = tf.getToy('Cat');
-toy.talk();
->> Meow
+    ToyFactory tf = ToyFactory();
+    Toy toy = tf.getToy('Dog');
+    toy.talk(); 
+    >> Wow
+    toy = tf.getToy('Cat');
+    toy.talk();
+    >> Meow
 */
 
 /**
@@ -38,10 +38,10 @@ class Cat implements Toy {
 
 public class ToyFactory {
     public Toy getToy(String type) {
-        if(type == null) return null;
-        if(type.equalsIgnoreCase("Dog")) {
+        if (type == null) return null;
+        if (type.equalsIgnoreCase("Dog")) {
             return new Dog();
-        } else if(type.equalsIgnoreCase("Cat")) {
+        } else if (type.equalsIgnoreCase("Cat")) {
             return new Cat();
         }
         return null;
