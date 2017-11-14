6d490986fa4db5a8677387fb4efe309d4d0409c5
eric496 <eric.mlengineer@gmail.com>
2017-11-14 18:45:13 -0500

    add comment

--- arrays/56_two_sum.java
+++ arrays/56_two_sum.java
@@ -11,30 +11,28 @@ Example
 
 Challenge
     Either of the following solutions are acceptable:
-        O(n) Space, O(nlogn) Time
-        O(n) Space, O(n) Time
+    O(n) Space, O(nlogn) Time
+    O(n) Space, O(n) Time
 */
 
 /*
 Thought process:
-    O(n) time and O(n) space solution: use hash map to store <target-number, index> pairs. 
-    Loop through the array, check if the hash map contains key number. 
-    If it does, then n2 = target - n1, where n1 is the first number and n2 is the second. 
-    Retrieve the indices of these two numbers - the first is the matched key, the second is just the current iteration number. 
+    Use hashmap to store <target-number, index> pairs. 
+    Iterate the array, check if the hash map contains key number. If so, then n2 = target - n1, where n1 is the first number and n2 is the second. 
+    Retrieve the indices of these two numbers - the first is the matched key, the second is just the current index. 
     Don't forget to add 1 to the result indices because the result indices are not zero-based.
 */
 
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        if(numbers == null || numbers.length < 2) return null;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int[] result = new int[2];
-        for(int i = 0; i < numbers.length; i++) {
-            if(map.containsKey(numbers[i])) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(numbers[i])) {
                 result[0] = map.get(numbers[i]) + 1;
                 result[1] = i + 1;
             } else {
-                map.put(target - numbers[i], i);
+                map.put(target-numbers[i], i);
             }
         }
         return result;
