d903a5eacddc6036e800b9dbc320ad4062749a0b
eric496 <eric.mlengineer@gmail.com>
2017-11-14 21:30:52 -0500

    modify solution

--- implementation/499_word_count_map_reduce.java
+++ implementation/499_word_count_map_reduce.java
@@ -3,15 +3,15 @@ Using map reduce to count word frequency.
 https://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html#Example%3A+WordCount+v1.0
 
 Example
-chunk1: "Google Bye GoodBye Hadoop code"
-chunk2: "lintcode code Bye"
-Get MapReduce result:
-    Bye: 2
-    GoodBye: 1
-    Google: 1
-    Hadoop: 1
-    code: 2
-    lintcode: 1
+    chunk1: "Google Bye GoodBye Hadoop code"
+    chunk2: "lintcode code Bye"
+    Get MapReduce result:
+        Bye: 2
+        GoodBye: 1
+        Google: 1
+        Hadoop: 1
+        code: 2
+        lintcode: 1
 */
 
 /**
@@ -26,7 +26,7 @@ public class WordCount {
     public static class Map {
         public void map(String key, String value, OutputCollector<String, Integer> output) {
             StringTokenizer tokens = new StringTokenizer(value);
-            while(tokens.hasMoreTokens()) {
+            while (tokens.hasMoreTokens()) {
                 String s = tokens.nextToken();
                 output.collect(s, 1);
             }
@@ -36,7 +36,9 @@ public class WordCount {
     public static class Reduce {
         public void reduce(String key, Iterator<Integer> values, OutputCollector<String, Integer> output) {
             int sum = 0;
-            while(values.hasNext()) sum += values.next();
+            while (values.hasNext()) {
+		sum += values.next();
+	    }
             output.collect(key, sum);
         }
     }
