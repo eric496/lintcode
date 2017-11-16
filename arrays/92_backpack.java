138591f827b8a054ad3e41df0f78b7aea3c88fa0
eric496 <eric.mlengineer@gmail.com>
2017-11-16 17:54:14 -0500

    modify solution

--- arrays/92_backpack.java
+++ arrays/92_backpack.java
@@ -39,22 +39,17 @@ Thought process:
 public class Solution {
     public int backPack(int m, int[] A) {
         int[][] dp = new int[A.length][m+1];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < m + 1; j++) {
+        for (int row = 0; row < A.length; row++) {
+            for (int col = 1; col < m + 1; col++) {
                 // initialize the first row
-                if (i == 0) {
-                    if (A[0] > j) {
-                        dp[i][j] = 0;
-                    } else {
-                        dp[i][j] = A[0];
-                    }
+                if (row == 0) {
+                    dp[row][col] = col < A[row] ? 0 : A[row];
                 } else {
-                    // if current item size is greater than the current backpack size, inherit the max size from the previous row
-                    if (A[i] > j) {
-                        dp[i][j] = dp[i-1][j];
-                    } else {
-                        dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-A[i]] + A[i]);   
-                    }
+                    // if current pack size is greater than current item size, we need to compare 
+                    // 1) not to include it (inherit what we have from previous row)
+                    // 2) include it (current item size + what we can get most from (current pack size - current item size))
+                    // whichever is greater.
+                    dp[row][col] = col < A[row] ? dp[row-1][col] : Math.max(dp[row-1][col], dp[row-1][col-A[row]]+A[row]);
                 }
             }
         }
