c6446a4d03821305fa268830285c3d10766dfcb6
eric496 <eric.mlengineer@gmail.com>
2017-11-15 23:59:06 -0500

    modify solution

--- implementation/473_add_and_search_word.java
+++ implementation/473_add_and_search_word.java
@@ -32,8 +32,10 @@ public class WordDictionary {
     public void addWord(String word) {
         TrieNode node = root;
         for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) node.children[c - 'a'] = new TrieNode();
-            node = node.children[c - 'a'];
+            if (node.children[c-'a'] == null) {
+		node.children[c-'a'] = new TrieNode();
+	    }
+            node = node.children[c-'a'];
         }
         node.item = word;
     }
