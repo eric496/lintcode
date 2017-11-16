1d16af1cf24d6d78946e00a1fb5538c68c5cb0b8
eric496 <eric.mlengineer@gmail.com>
2017-11-16 18:46:44 -0500

    modify solution

--- arrays/150_best_time_to_buy_and_sell_stock_II.java
+++ arrays/150_best_time_to_buy_and_sell_stock_II.java
@@ -15,10 +15,10 @@ Thought process:
 
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length == 0) return 0;
+        if (prices == null || prices.length == 0) return 0;
         int profit = 0;
-        for(int i = 0; i < prices.length - 1; i++) {
-            if(prices[i+1] > prices[i]) {
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i+1] > prices[i]) {
                 profit += prices[i+1] - prices[i]; 
             }
         }
