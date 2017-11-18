38fec28e34791817c6fee0d4c0f3a4a6559a91e2
eric496 <eric.mlengineer@gmail.com>
2017-11-18 00:51:55 -0500

    add solution

--- linked_lists/105_copy_list_withh_random_pointer.java
+++ linked_lists/105_copy_list_withh_random_pointer.java
@@ -8,8 +8,7 @@ Challenge
 
 /*
 Thought process:
-    1. O(n) time and space solution: use a hashmap and two passes: one to copy
-       nodes, one to copy next and random pointers.
+    Use a hashmap and two passes: one pass copies nodes, one pass copies next and random pointers.
 */
 
 /**
@@ -25,14 +24,14 @@ Thought process:
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null) return null;
-        HashMap<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
         RandomListNode runner = head;
-        while(runner != null) {
+        while (runner != null) {
             map.put(runner, new RandomListNode(runner.label));
             runner = runner.next;
         }
         runner = head;
-        while(runner != null) {
+        while (runner != null) {
             map.get(runner).next = map.get(runner.next);
             map.get(runner).random = map.get(runner.random);
             runner = runner.next;
@@ -41,5 +40,41 @@ public class Solution {
     }
 }
 
-// O(1) space
-
+// O(1) space 
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        RandomListNode runner = head, next;
+    	// first round: make copy of each node,
+    	// and link them together side-by-side in a single list.
+    	while (runner != null) {
+    	    next = runner.next;
+    	    RandomListNode copy = new RandomListNode(runner.label);
+    	    runner.next = copy;
+    	    copy.next = next;
+    	    runner = next;
+    	}
+    	// second round: assign random pointers for the copy nodes.
+    	runner = head;
+    	while (runner != null) {
+    	    if (runner.random != null) {
+    		runner.next.random = runner.random.next;
+    	    }
+    	    runner = runner.next.next;
+    	}
+    	// third round: restore the original list, and extract the copy list.
+    	runner = head;
+    	RandomListNode dummyHead = new RandomListNode(-1);
+    	RandomListNode copy, copyRunner = dummyHead;
+    	while (runner != null) {
+    	    next = runner.next.next;
+    	    // extract the copy
+    	    copy = runner.next;
+    	    copyRunner.next = copy;
+    	    copyRunner = copy;
+    	    // restore the original list
+    	    runner.next = next;
+    	    runner = next;
+    	}
+    	return dummyHead.next;
+    }
+}
