e3404bed2ce10adca8adafb69c6b3f886663ef89
eric496 <eric.mlengineer@gmail.com>
2017-11-17 20:42:04 -0500

    add 2nd solution

--- math/719_calculate_maximum_value.java
+++ math/719_calculate_maximum_value.java
@@ -10,13 +10,19 @@ Example
 
 /*
 Thought process:
-    Create a current maximum variable to store the max so far. 
-    Iterate the array, the principle to get the global max is 
-        1) By adding the current digit to the max so far if the current digit and the max so far are both either 0 or 1 (otherwise, the result will be smaller by multiplication in these cases).
-        2) By multiplying the current digit by the max so far if the current digit and the max so far are both greater than 1.
-    Notice that there is a corner case that if the first digit is greater than 1, we should add it to the max so far instead of multiplying by the max so far since the initial value of max is set to 0.
+    Method 1:
+        Create a current maximum variable to store the max so far. Iterate the array, the principle to get the global max is 
+            1) By adding the current digit to the max so far if the current digit and the max so far are both either 0 or 1 (otherwise, the result will be smaller by multiplication in these cases).
+            2) By multiplying the current digit by the max so far if the current digit and the max so far are both greater than 1.
+        Notice that there is a corner case that if the first digit is greater than 1, we should add it to the max so far instead of multiplying by the max so far since the initial value of max is set to 0.
+    Method 2:
+        Iterate the string, if the current number is 0 or 1, addition always yields the best result.
+        If the current number is greater than 1, then there are 2 sub cases:
+            1) add it to sum if current sum is 0 or 1.
+            2) multiply it by sum if current sum is greater than 1.
 */
 
+// method 1
 public class Solution {
     public int calcMaxValue(String str) {
         if (str == null || str.length() == 0) return 0;
@@ -34,5 +40,26 @@ public class Solution {
             }
         } 
         return maxSofar;
+    }
+}
+
+// method 2
+public class Solution {
+    public int calcMaxValue(String str) {
+        if (str == null || str.length() == 0) return 0;
+        int sum = 0;
+        for (char c : str.toCharArray()) {
+            int num = (int)(c-'0');
+            if (num == 0 || num == 1) {
+                sum += num;
+            } else if (num > 1) {
+                if (sum == 0 || sum == 1) {
+                    sum += num;
+                } else {
+                    sum *= num;
+                }
+            }
+        }
+        return sum;
     }
 }
