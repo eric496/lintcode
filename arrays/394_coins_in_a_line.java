0057731e43ca27553da2e8d399d7d624127cd5d2
eric496 <eric.mlengineer@gmail.com>
2017-11-17 21:28:51 -0500

    add thought

--- arrays/394_coins_in_a_line.java
+++ arrays/394_coins_in_a_line.java
@@ -16,10 +16,10 @@ Challenge
 
 /*
 Thought process:
-    1. DP
-    2. Trick: in order to win, we would like leave 3 coins to the other player so that no matter 1 or 2 coins he takes, we will always win. This generalizes to each run actually, 
-       we'd like to leave 3x coins to the other player after each run. With this said, if we are the first play, win/lose completely depends on the initial number of coins, 
-       if it is not multiples of 3, we will always win by taking 1 or 2 coins to make the left 3x coins. Otherwise, we will always lose because the other player will take the same strategy.
+    1. DP: the current win/lose is consistent with the result 3 steps ahead. See 2. for explanation.
+    2. Trick: in order to win, in the last round before the other player's final draw, we'd like to take whatever number of coins so as to leave 3 coins to the other player and no matter 1 or 2 coins he takes, we win for sure. 
+       This generalizes to each run actually, we'd like to leave 3x coins to the other player after each run. With this said, if we are the first play, win/lose completely depends on the initial number of coins, 
+       if it is not multiples of 3, we will always win by taking 1 or 2 coins to make the left coins a multiple of 3s. Otherwise, we will always lose because the other player will take the same strategy.
 */
 
 // O(1) time and O(1) space
