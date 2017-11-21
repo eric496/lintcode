cf821f15f4988ca2aeb5d74ca15bac970b302b69
eric496 <eric.mlengineer@gmail.com>
2017-11-20 20:01:00 -0500

    add thought

--- strings/512_decode_ways.java
+++ strings/512_decode_ways.java
@@ -13,7 +13,9 @@ Example
 
 /*
 Thought process:
-    DP
+    DP: Similar to 111. Climbing Stairs. The current ways of decoding dp[i] = dp[i-1] + dp[i-2], 
+        because we can decode either in "Y" or "XY" ways. But only when "Y" not equals 0 can we decode it as "Y", 
+        and only when "XY" is in [10,26] can we decode it as "XY".
 */
 
 public class Solution {
@@ -23,10 +25,14 @@ public class Solution {
         dp[0] = 1;
         dp[1] = s.charAt(0) != '0' ? 1 : 0;
         for (int i = 2; i <= s.length(); i++) {
-            int one = s.charAt(i - 1) - '0';
-            int two = one + (s.charAt(i - 2) - '0') * 10; 
-            if (one >= 1 && one <= 9) dp[i] += dp[i-1];
-            if (two >= 10 && two <= 26) dp[i] += dp[i-2];
+            int one = s.charAt(i-1) - '0';
+            int two = one + (s.charAt(i-2) - '0') * 10; 
+            if (one != 0) {	
+		dp[i] += dp[i-1];
+	    }
+            if (two >= 10 && two <= 26) {
+		dp[i] += dp[i-2];
+	    }
         }
         return dp[s.length()];
     }
