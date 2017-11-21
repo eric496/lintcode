ec66cf3c69faf1433c4d550e7c371309862717f6
eric496 <eric.mlengineer@gmail.com>
2017-11-21 14:55:49 -0500

    add solution

--- strings/118_distinct_subsequences.java
+++ strings/118_distinct_subsequences.java
@@ -10,23 +10,42 @@ Challenge
     O(n2) memory is also acceptable if you do not know how to optimize memory.
 */
 
+/*
+Thought process:
+
+*/
 
 // O(n^2) time and O(n^2) space
 public class Solution {
     public int numDistinct(String S, String T) {
-        if (S == null || T == null) return 0;
-        int[][] nums = new int[S.length()+1][T.length()+1];
+        if (S == null || T == null || S.length() < T.length()) return 0;
+        int[][] dp = new int[S.length()+1][T.length()+1];
         for (int i = 0; i <= S.length(); i++) {
-            nums[i][0] = 1;
+            dp[i][0] = 1;
         }
         for (int i = 1; i <= S.length(); i++) {
             for (int j = 1; j <= T.length(); j++) {
-                nums[i][j] = nums[i-1][j];
+                dp[i][j] = dp[i-1][j];
                 if (S.charAt(i-1) == T.charAt(j-1)) {
-                    nums[i][j] += nums[i-1][j-1];
+                    dp[i][j] += dp[i-1][j-1];
                 }
             }
         }
-        return nums[S.length()][T.length()];
+        return dp[S.length()][T.length()];
+    }
+}
+
+// O(n^2) time and O(n) space
+public class Solution {
+    public int numDistinct(String S, String T) {
+        if (S == null || T == null || S.length() < T.length()) return 0;
+        int[] dp = new int[T.length()+1];
+        dp[0] = 1;
+        for (int j = 1; j <= S.length(); j++) {
+            for (int i = T.length(); i >= 1; i--) {  
+                dp[i] = dp[i] + (T.charAt(i-1) == S.charAt(j-1) ? dp[i-1] : 0);
+            }
+        }
+        return dp[T.length()];
     }
 }
