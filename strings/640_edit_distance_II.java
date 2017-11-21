7f835b7fdd30113a0a33a507ada5d319296e79e7
eric496 <eric.mlengineer@gmail.com>
2017-11-21 16:32:32 -0500

    add solution

--- strings/640_edit_distance_II.java
+++ strings/640_edit_distance_II.java
@@ -10,8 +10,8 @@ Example
 Thought process:
     There are two cases that two strings are one edit distance away from each other:
         1) one string is one character less than the other, and the relative order of other characters are the same for the two strings
-           so by inserting an character in the short string we will get the long string.
-        2) two strings are of the same length with only one character different.
+           so by inserting an character in the short string (or by deleting an character in the long string) we can convert one to the other. 
+        2) two strings are of the same length with only one character different. Conversion can be done by replacement.
     Notice two corner cases: return false if difference of lengths is greater than 1 or two strings are extactly the same (no distance)
     Differentiate the shorter string from the longer one. Create two index variables and a count variable to keep the number of different characters.
     If the current characters are the same, increment both short and long indices. Otherwise, if two strings have the same length, increment both indices as well.
@@ -19,23 +19,58 @@ Thought process:
     (there is a corner case that diff == 0 when the last character of the long string is the only different character).
 */
 
+// method 1
 public class Solution {
     public boolean isOneEditDistance(String s, String t) {
-        if(s == null || t == null || Math.abs(s.length() - t.length()) > 1 || s.equals(t)) return false;
-        String longer = s.length() >= t.length() ? s : t;
-        String shorter = s.length() <= t.length() ? t : s;
-        int shortIdx = 0, longIdx = 0, diff = 0;
-        while (shortIdx < shorter.length()) {
-            if (shorter.charAt(shortIdx) == longer.charAt(longIdx)) {
-                shortIdx++;
-                longIdx++;
+        if (s == null || t == null || Math.abs(s.length() - t.length()) > 1 || s.equals(t)) return false;
+        String strLong, strShort;
+        if (s.length() >= t.length()) {
+            strLong = s;
+            strShort = t;
+        } else {
+            strLong = t;
+            strShort = s;
+        }
+        int ixLong = 0, ixShort = 0, diff = 0;
+        while (ixShort < strShort.length()) {
+            if (strLong.charAt(ixLong) != strShort.charAt(ixShort)) {
+                diff++;
+                if (strLong.length() == strShort.length()) {
+                    ixLong++;
+                    ixShort++;
+                } else {
+                    ixLong++;
+                }
+            } else {
+                ixLong++;
+                ixShort++;
+            }
+	    if (diff > 1) return false;
+        }
+        if (diff == 0 && strLong.length() > strShort.length()) return true;
+        if (diff == 1) return true;
+        return false;
+    }
+}
+
+// method 2
+public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+        if (s == null || t == null || Math.abs(s.length() - t.length()) > 1 || s.equals(t)) return false;
+        String strLong = s.length() >= t.length() ? s : t;
+        String strShort = s.length() <= t.length() ? t : s;
+        int ixShort = 0, ixLong = 0, diff = 0;
+        while (ixShort < strShort.length()) {
+            if (strShort.charAt(ixShort) == strLong.charAt(ixLong)) {
+                ixShort++;
+                ixLong++;
             } else {
                 if (++diff > 1) return false;
-                if (shorter.length() == longer.length()) {
-                    shortIdx++;
-                    longIdx++;
+                if (strShort.length() == strLong.length()) {
+                    ixShort++;
+                    ixLong++;
                 } else {
-                    longIdx++;
+                    ixLong++;
                 }
             }
         }
