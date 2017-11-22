c3d4a950de196d3a6afbcce527f4914c27977800
eric496 <eric.mlengineer@gmail.com>
2017-11-22 00:50:23 -0500

    add comment

--- arrays/189_first_missing_positive.java
+++ arrays/189_first_missing_positive.java
@@ -11,10 +11,10 @@ Challenge
 
 /*
 Thought process:
-    Sort the positive integers swapping two integers, so for any positive integer, its value is 1 greater than its index, i.e. A[0] = 1, A[1] = 2, ... A[i] = i+1.
+    Sort the positive integers by swapping two integers, so for any positive integer, its value is 1 greater than its index, i.e. A[0] = 1, A[1] = 2, ... A[i] = i+1.
     Notice the conditions in the while loop:
         1) A[i] > 0              ~ (positive integer)
-        2) A[i] <= A.length      ~ (the positive integer should be in the range [1, n])
+        2) A[i] <= A.length      ~ (the positive integer should be in the range [1, n])  e.g. [1,2,4,-2,10]
         3) A[i] != i + 1         ~ (the positive integer is not in the right place) 
     If we do not find any missing positive integer in the sequence, we can conclude that it must be the last integer is missing, because that is the only one we have not checked yet.
     e.g. Given [0,1,2] return 3.
@@ -35,6 +35,7 @@ public class Solution {
         for (int i = 0; i < A.length; i ++) {
             if (A[i] != i + 1) return i + 1;
         }
+        // missing number is the last number
         return A.length + 1;
     }
 }
