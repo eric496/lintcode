cf0f8338d69c9df56bbe0887a980afe26145131b
eric496 <eric.mlengineer@gmail.com>
2017-11-22 01:07:44 -0500

    modify solution

--- others/666_guess_number_game_II.java
+++ others/666_guess_number_game_II.java
@@ -22,13 +22,13 @@ Thought process:
 
 public class Solution {
     public int getMoneyAmount(int n) {
-        int[][] dp = new int[n + 2][n + 2];
-        for (int l = 1; l < n; l++)
-            for (int i = 1; i <= n - l; i++) {
-                int j = i + l;
+        int[][] dp = new int[n+2][n+2];
+        for (int m = 1; m < n; m++)
+            for (int i = 1; i <= n - m; i++) {
+                int j = i + m;
                 dp[i][j] = Integer.MAX_VALUE;
                 for (int k = i; k <= j; k++) {
-                    dp[i][j] = Math.min(dp[i][j], k + Math.max(dp[i][k-1], dp[k+1][j]));
+                    dp[i][j] = Math.min(dp[i][j], k+Math.max(dp[i][k-1], dp[k+1][j]));
                 }
             }
         return dp[1][n];
