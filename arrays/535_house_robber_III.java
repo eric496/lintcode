de19014e8738cabfb5cec3beb8455599aa44214e
eric496 <eric.mlengineer@gmail.com>
2017-11-22 01:13:05 -0500

    modify solution

--- arrays/535_house_robber_III.java
+++ arrays/535_house_robber_III.java
@@ -23,18 +23,18 @@ Thought process:
 */
 
 public class Solution {
+    public int houseRobber3(TreeNode root) {
+        int[] result = rob(root);
+        return Math.max(result[0], result[1]);
+    }
+    
     private int[] rob(TreeNode root) {
         if (root == null) return new int[2];
         int[] left = rob(root.left);
         int[] right = rob(root.right);
-        int[] res = new int[2];
-        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        res[1] = root.val + left[0] + right[0];
-        return res;
-    }
-
-    public int houseRobber3(TreeNode root) {
-        int[] res = rob(root);
-        return Math.max(res[0], res[1]);
+        int[] result = new int[2];
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = root.val + left[0] + right[0];
+        return result;
     }
 }
