d563effd0fd67cfd59cf43471c2356c1136dbd85
eric496 <eric.mlengineer@gmail.com>
2017-11-24 23:19:15 -0500

    modify solution

--- implementation/40_implement_queue_by_two_stacks.java
+++ implementation/40_implement_queue_by_two_stacks.java
@@ -25,20 +25,26 @@ public class MyQueue {
     }
     
     private void stack2ToStack1() {
-		while (! stack2.empty()) stack1.push(stack2.pop());
+	while (!stack2.empty()) {
+	    stack1.push(stack2.pop());
 	}
+    }
 	
     public void push(int number) {
         stack2.push(number);
     }
 
     public int pop() {
-        if (stack1.empty() == true) this.stack2ToStack1();
+        if (stack1.isEmpty()) {
+	    this.stack2ToStack1();
+	}
         return stack1.pop();
     }
 
     public int top() {
-        if (stack1.empty() == true) this.stack2ToStack1();
+        if (stack1.isEmpty()) {
+	    this.stack2ToStack1();
+	}
         return stack1.peek();
     }
 }
