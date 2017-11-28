b94022e065da1bf5a962206382ee0f175e21b851
eric496 <eric.mlengineer@gmail.com>
2017-11-27 20:41:08 -0500

    modify solution

--- strings/384_longest_substring_without_repeating_characters.java
+++ strings/384_longest_substring_without_repeating_characters.java
@@ -14,18 +14,16 @@ public class Solution {
     public int lengthOfLongestSubstring(String s) {
         // map from character's ASCII to its last occured index
         int[] map = new int[256];
-        int j = 0;
-        int i = 0;
-        int ans = 0;
-        for (i = 0; i < s.length(); i++) {
-            while (j < s.length() && map[s.charAt(j)]==0) {
+        int j = 0, maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            while (j < s.length() && map[s.charAt(j)] == 0) {
                 map[s.charAt(j)] = 1;
-                ans = Math.max(ans, j-i + 1);
-                j ++;
+                maxLen = Math.max(maxLen, j-i+1);
+                j++;
             }
             map[s.charAt(i)] = 0;
         }
-        return ans;
+        return maxLen;
     }
 }
 
@@ -33,46 +31,47 @@ public class Solution {
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         if (s.length() == 0) return 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max=0;
-        for(int i=0, j=0; i<s.length(); ++i) {
-            if (map.containsKey(s.charAt(i)))
-                j = Math.max(j,map.get(s.charAt(i))+1);
-            map.put(s.charAt(i),i);
-            max = Math.max(max,i-j+1);
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i))+1);
+	    }
+            map.put(s.charAt(i), i);
+            maxLen = Math.max(maxLen, i-j+1);
         }
-        return max;
+        return maxLen;
     }
 }
 
 // use an extra hash set
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i = 0, j = 0, max = 0;
-        HashSet<Character> set = new HashSet<>();
+        int i = 0, j = 0, maxLen = 0;
+        Set<Character> set = new HashSet<>();
         while (j < s.length()) {
             if (!set.contains(s.charAt(j))) {
                 set.add(s.charAt(j++));
-                max = Math.max(max, set.size());
+                maxLen = Math.max(maxLen, set.size());
             } else {
                 set.remove(s.charAt(i++));
             }
         }
-        return max;
+        return maxLen;
     }
 }
 
 // DP
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int[] cache = new int[128];
+        int[] cache = new int[256];
         Arrays.fill(cache, -1);
-        int max = 0, start = 0;
+        int maxLen = 0, start = 0;
         for (int i = 0; i < s.length(); i++) {
-          start = Math.max(cache[s.charAt(i)] + 1, start);
-          max = Math.max(max, i - start + 1);
+          start = Math.max(cache[s.charAt(i)]+1, start);
+          maxLen = Math.max(maxLen, i-start+1);
           cache[s.charAt(i)] = i;
         }
-        return max;
+        return maxLen;
     }
 }
