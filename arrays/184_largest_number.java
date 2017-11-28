464ba3874af50cc146a9ed622d784b0137d95ada
eric496 <eric.mlengineer@gmail.com>
2017-11-27 19:01:01 -0500

    modify solution

--- arrays/184_largest_number.java
+++ arrays/184_largest_number.java
@@ -13,24 +13,29 @@ Challenge
 
 public class Solution {
     public String largestNumber(int[] nums) {
-        if(nums == null || nums.length == 0) return "";
-	    // Convert int array to String array, so we can sort later on
-	    String[] s_nums = new String[nums.length];
-	    for(int i = 0; i < nums.length; i++) s_nums[i] = String.valueOf(nums[i]);
-	    // Comparator to decide which string should come first in concatenation
-	    Comparator<String> comp = new Comparator<String>(){
-	        @Override
-		public int compare(String str1, String str2){
-		    String s1 = str1 + str2;
-			String s2 = str2 + str1;
-			return s2.compareTo(s1); // reverse order here, so we can do append() later
-	        }
-	    };
-	Arrays.sort(s_nums, comp);
+        if (nums == null || nums.length == 0) return "";
+	// Convert int array to String array, so we can sort later on
+	String[] strNums = new String[nums.length];
+	for (int i = 0; i < nums.length; i++) {
+	    strNums[i] = String.valueOf(nums[i]);
+	}
+	// Comparator to decide which string should come first in concatenation
+	Comparator<String> comp = new Comparator<String>(){
+	    @Override
+	    public int compare(String str1, String str2) {
+		String s1 = str1 + str2;
+		String s2 = str2 + str1;
+		// reverse order here, so we can do append() later
+		return s2.compareTo(s1);
+	    }
+	};
+	Arrays.sort(strNums, comp);
         // An extreme edge case by lc, say you have only a bunch of 0 in your int array
-        if(s_nums[0].charAt(0) == '0') return "0";
-	    StringBuilder sb = new StringBuilder();
-	    for(String s: s_nums) sb.append(s);
-	    return sb.toString();
+        if (strNums[0].charAt(0) == '0') return "0";
+	StringBuilder sb = new StringBuilder();
+	for (String s : strNums) {
+	    sb.append(s);
+	}
+	return sb.toString();
     }
 }
