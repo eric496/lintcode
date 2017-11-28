1a9e413757d13752ec0501fa0fdd1b9d514bb21a
eric496 <eric.mlengineer@gmail.com>
2017-11-28 13:34:42 -0500

    modify solution

--- linked_lists/104_merge_k_sorted_lists.java
+++ linked_lists/104_merge_k_sorted_lists.java
@@ -32,29 +32,29 @@ Thought process:
 // mergesort
 public class Solution {
     public ListNode mergeKLists(List<ListNode> lists) {  
-        if(lists == null || lists.size() == 0) return null;
-        return mergeKLists(lists, 0, lists.size() - 1);
+        if (lists == null || lists.size() == 0) return null;
+        return mergeKLists(lists, 0, lists.size()-1);
     }
     
     private ListNode mergeKLists(List<ListNode> lists, int low, int high) {
-        if(low == high) {
+        if (low == high) {
             return lists.get(low);
-        } else if (low < high){
+        } else if (low < high) {
             int mid = low + (high - low) / 2;
             ListNode left = mergeKLists(lists, low, mid);
-            ListNode right = mergeKLists(lists, mid + 1, high);
+            ListNode right = mergeKLists(lists, mid+1, high);
             return mergeTwoLists(left, right);
         }
         return null;
     }
     
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
         ListNode runner = new ListNode(Integer.MIN_VALUE);
         ListNode head = runner;
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val) {
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
                 runner.next = l1;
                 l1 = l1.next;
             } else {
@@ -63,8 +63,12 @@ public class Solution {
             }
             runner = runner.next;
         }
-        if(l1 != null) runner.next = l1;
-        if(l2 != null) runner.next = l2;
+        if (l1 != null) {
+	    runner.next = l1;
+	}
+        if (l2 != null) {
+	    runner.next = l2;
+	}
         return head.next;
     }
 }
