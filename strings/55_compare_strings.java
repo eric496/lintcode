19d6ebdb6494574aa056f5a8931966e8f811109f
eric496 <eric.mlengineer@gmail.com>
2017-11-28 18:26:52 -0500

    add solution

--- strings/55_compare_strings.java
+++ strings/55_compare_strings.java
@@ -15,23 +15,23 @@ Thought process:
     If the characters are all in ASCII, use an extra array with size 256. Use a map otherwise.
 */
 
-// O(n) time and O(1) space by using array
+// O(n) time and O(1) space
 public class Solution {
     public boolean compareStrings(String A, String B) {
         if (A.length() < B.length() || A == null || B == null) return false;
         int[] count = new int[256];
         for (char c : A.toCharArray()) {
-	    count[(int)c]++;
+	    count[c]++;
 	}
         for (char c : B.toCharArray()) {
-            count[(int)c]--;
-            if (count[(int)c] < 0) return false;
+            count[c]--;
+            if (count[c] < 0) return false;
         }
         return true;
     }
 }
 
-// O(n) time and O(n) space by using hash map
+// O(n) time and O(n) space
 public class Solution {
     public boolean compareStrings(String A, String B) {
         if (A.length() < B.length() || A == null || B == null) return false;
@@ -51,6 +51,27 @@ public class Solution {
                 return false;
             }
         }
+        return true;
+    }
+}
+
+// Sorting 
+public class Solution {
+    public boolean compareStrings(String A, String B) {
+        if (A.length() < B.length()) return false;
+        if (B == null || B.length() == 0) return true;
+        char[] charA = A.toCharArray();
+        char[] charB = B.toCharArray();
+        Arrays.sort(charA);
+        Arrays.sort(charB);
+        int ixa = 0, ixb = 0;
+        while (ixa < charA.length && ixb < charB.length) {
+            if (charA[ixa] == charB[ixb]) {
+                ixb++;
+            }
+            ixa++;
+        }
+        if (ixb < charB.length) return false;
         return true;
     }
 }
