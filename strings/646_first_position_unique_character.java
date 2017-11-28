2f24908742d679e310ba8399d682c3be8bf960df
eric496 <eric.mlengineer@gmail.com>
2017-11-28 18:58:01 -0500

    modify solution

--- strings/646_first_position_unique_character.java
+++ strings/646_first_position_unique_character.java
@@ -19,27 +19,11 @@ public class Solution {
     public int firstUniqChar(String s) {
 	if (s == null || s.length() == 0) return -1;
         int[] count = new int[256];
-        for (int i = 0; i < s.length(); i++) {
-	    count[s.charAt(i)]++;
-	}
-        for (int i = 0; i < s.length(); i++) { 
-	    if (count[s.charAt(i)] == 1) return i;
-	}
-        return -1;
-    }
-}
-
-
-// Same with the above method but more concise using for-each method
-public class Solution {
-    public int firstUniqChar(String s) {
-        if(s == null || s.length() == 0) return -1;
-        int[] count = new int[256];
         for (char c : s.toCharArray()) {
 	    count[c]++;
 	}
-        for (char c : s.toCharArray()) {
-            if (count[c] == 1) return s.indexOf(c);
+        for (char c : s.toCharArray()) { 
+	    if (count[c] == 1) return s.indexOf(c);
 	}
         return -1;
     }
