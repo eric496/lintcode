7ed03e54cd404989b2a98eea10429c859d22d711
eric496 <eric.mlengineer@gmail.com>
2017-11-27 19:33:12 -0500

    modify solution

--- arrays/124_longest_consecutive_sequence.java
+++ arrays/124_longest_consecutive_sequence.java
@@ -12,11 +12,11 @@ Example
 public class Solution {
     public int longestConsecutive(int[] num) {
         int result = 0;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for (int n : num) {
             if (!map.containsKey(n)) {
-                int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
-                int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+                int left = (map.containsKey(n-1)) ? map.get(n-1) : 0;
+                int right = (map.containsKey(n+1)) ? map.get(n+1) : 0;
                 // sum: length of the sequence n is in
                 int sum = left + right + 1;
                 map.put(n, sum);
@@ -25,8 +25,8 @@ public class Solution {
                 // extend the length to the boundary(s)
                 // of the sequence
                 // will do nothing if n has no neighbors
-                map.put(n - left, sum);
-                map.put(n + right, sum);
+                map.put(n-left, sum);
+                map.put(n+right, sum);
             }
             else {
                 // duplicates
