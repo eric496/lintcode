7fed9a425ba4595476542868c400d9b4613eeb94
eric496 <eric.mlengineer@gmail.com>
2017-11-27 19:12:15 -0500

    modify solution

--- arrays/425_letter_combinations_of_a_phone_number.java
+++ arrays/425_letter_combinations_of_a_phone_number.java
@@ -13,18 +13,20 @@ Example
 
 public class Solution {
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0 || digits == null) return new ArrayList<String>();
-        Queue<String> ans = new LinkedList<String>();
-        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits.length() == 0) return new ArrayList<String>();
+        Queue<String> ans = new LinkedList<>();
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         ans.add("");
-        for(int i =0; i<digits.length();i++){
+        for (int i = 0; i < digits.length(); i++) {
             int x = Character.getNumericValue(digits.charAt(i));
-            while(ans.peek().length()==i){
+            while (ans.peek().length() == i) {
                 String t = ans.remove();
-                for(char s : mapping[x].toCharArray()) ans.add(t+s);
+                for (char s : mapping[x].toCharArray()) {
+                    ans.add(t+s);
+                }
             }
         }
-        List<String> res = new ArrayList(ans);
-        return res;
+        List<String> result = new ArrayList(ans);
+        return result;
     }
 }
