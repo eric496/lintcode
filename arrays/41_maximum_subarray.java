255fba36642ed027845bdb13fb2542e381b12471
eric496 <eric.mlengineer@gmail.com>
2017-11-29 18:34:49 -0500

    modify solution

--- arrays/41_maximum_subarray.java
+++ arrays/41_maximum_subarray.java
@@ -28,7 +28,7 @@ Thought process
 public class Solution {
     public int maxSubArray(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        int[] dp = new int[nums.length+1];
+        int[] dp = new int[nums.length];
         dp[0] = nums[0];
         int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
