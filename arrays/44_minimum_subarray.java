3b778166c575556117e374b698ee71503e503a0e
eric496 <eric.mlengineer@gmail.com>
2017-11-29 19:34:20 -0500

    add solutioon

--- arrays/44_minimum_subarray.java
+++ arrays/44_minimum_subarray.java
@@ -11,16 +11,32 @@ Thought process:
      Refer to 41. Maximum Subarray
 */
 
-// greedy - Kadane's algorithm
+// O(n) time and O(n) space - DP
 public class Solution {
     public int minSubArray(List<Integer> nums) {
-        int minSofar = nums.get(0), globalMin = nums.get(0);
+        int[] dp = new int[nums.size()];
+        dp[0] = nums.get(0);
+        int min = nums.get(0);
         for (int i = 1; i < nums.size(); i++) {
-            minSofar = Math.min(minSofar + nums.get(i), nums.get(i));
-            if (minSofar < globalMin) {
-		globalMin = minSofar;
+            dp[i] = Math.min(nums.get(i), dp[i-1]+nums.get(i));
+            if (dp[i] < min) {
+                min = dp[i];
+            }
+        }
+        return min;
+    }
+}
+
+// O(n) time and O(1) space - Kadane's algorithm (greedy)
+public class Solution {
+    public int minSubArray(List<Integer> nums) {
+        int minSofar = nums.get(0), min = nums.get(0);
+        for (int i = 1; i < nums.size(); i++) {
+            minSofar = Math.min(minSofar+nums.get(i), nums.get(i));
+            if (minSofar < min) {
+		min = minSofar;
 	    }
         }
-        return globalMin;
+        return min;
     }
 }
