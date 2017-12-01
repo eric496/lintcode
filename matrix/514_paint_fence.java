207d18926b9453f94d1cd54e2a7345d72a6f6624
eric496 <eric.mlengineer@gmail.com>
2017-11-30 23:27:12 -0500

    modify solution

--- matrix/514_paint_fence.java
+++ matrix/514_paint_fence.java
@@ -38,7 +38,7 @@ public class Solution {
         dp[0] = k;
         dp[1] = k + k * (k - 1);
         for (int i = 2; i < n; i++) {
-            dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2]);
+            dp[i] = (k - 1) * (dp[i-1] + dp[i-2]);
         }
         return dp[n-1];
     }
