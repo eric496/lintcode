5d45f846bddb20c77af76fdf02e98fbb2aff84f7
eric496 <eric.mlengineer@gmail.com>
2017-12-01 23:15:57 -0500

    modify solution

--- arrays/407_plus_one.java
+++ arrays/407_plus_one.java
@@ -13,7 +13,7 @@ Thought process
        1) sum = current number + carry;
        2) sum % 10 as the number at ones place / tens place / hundreds place ...
        3) carry sum / 10 to the next higher place.
-   Be careful about two things: 
+   Be careful: 
        First, the default value of carry is 1, because 1 will be added to the number.
        Second, iterate the array in backward order because addition operates from ones place, tens place, ...
        Third, because of the second, the result array is in reverse order, we need to reverse it so for the correct order.  
@@ -23,19 +23,19 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         if (digits == null || digits.length == 0) return digits;
         int sum = 0, carry = 1;
-        List<Integer> rev = new ArrayList<Integer>();
+        List<Integer> resultList = new ArrayList<>();
         for (int i = digits.length - 1; i >= 0; i--) {
             sum = digits[i] + carry;
             carry = sum / 10;
-            rev.add(sum % 10);
+            resultList.add(sum%10);
         }
         if (carry != 0) {
-	    rev.add(carry);
+	    resultList.add(carry);
 	}
-        int[] result = new int[rev.size()];
-        int ix = rev.size() - 1;
-        for (int i : rev) {
-	    result[ix--] = i;
+        int[] result = new int[resultList.size()];
+        int ix = resultList.size() - 1;
+        for (int n : resultList) {
+	    result[ix--] = n;
 	}
         return result;
     }
