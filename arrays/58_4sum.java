811b89d1f622d8d92629c7e061fdd35e5563782e
eric496 <eric.mlengineer@gmail.com>
2017-12-03 12:51:51 -0500

    modify solution

--- arrays/58_4sum.java
+++ arrays/58_4sum.java
@@ -24,9 +24,9 @@ public class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(num);
         for (int i = 0; i < num.length - 3; i++) {
-            if (i != 0 && num[i] == num[i - 1]) continue;
+            if (i != 0 && num[i] == num[i-1]) continue;
             for (int j = i + 1; j < num.length - 2; j++) {
-                if (j != i + 1 && num[j] == num[j - 1]) continue;
+                if (j != i + 1 && num[j] == num[j-1]) continue;
                 int low = j + 1, high = num.length - 1;
                 while (low < high) {
                     int sum = num[i] + num[j] + num[low] + num[high];
@@ -34,8 +34,12 @@ public class Solution {
                         result.add(Arrays.asList(num[i], num[j], num[low], num[high]));
                         low++;
                         high--;
-                        while (low < high && num[low] == num[low - 1]) low++;
-                        while (low < high && num[high] == num[high + 1]) high--;
+                        while (low < high && num[low] == num[low-1]) {
+			    low++;
+			}
+                        while (low < high && num[high] == num[high+1]) {
+			    high--;
+			}
                     } else if (sum < target) {
                         low++;
                     } else {
