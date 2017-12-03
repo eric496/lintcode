9f353833913504191b19f192b10acaf1395b1dab
eric496 <eric.mlengineer@gmail.com>
2017-12-03 15:32:16 -0500

    modify solution

--- arrays/150_best_time_to_buy_and_sell_stock_II.java
+++ arrays/150_best_time_to_buy_and_sell_stock_II.java
@@ -10,16 +10,17 @@ Example
 
 /*
 Thought process:
-    Greedy: Buy and sell whenever it is profitable (prices[i+1] > prices[i]).
+    Greedy: Buy and sell whenever it is profitable (prices[i] > prices[i-1]).
 */
 
+// greedy
 class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0) return 0;
         int profit = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i+1] > prices[i]) {
-                profit += prices[i+1] - prices[i]; 
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
             }
         }
         return profit;
