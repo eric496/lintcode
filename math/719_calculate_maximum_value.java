3f578dfca1a4f12a7b9692449846db13c4fc0596
eric496 <eric.mlengineer@gmail.com>
2017-12-03 15:47:03 -0500

    add comment

--- math/719_calculate_maximum_value.java
+++ math/719_calculate_maximum_value.java
@@ -12,11 +12,11 @@ Example
 Thought process:
     Method 1:
         Create a current maximum variable to store the max so far. Iterate the array, the principle to get the global max is 
-            1) By adding the current digit to the max so far if the current digit and the max so far are both either 0 or 1 (otherwise, the result will be smaller by multiplication in these cases).
+            1) By adding the current digit to the max so far if the current digit and the max so far are both either 0 or 1 (otherwise, the result will be smaller by multiplication).
             2) By multiplying the current digit by the max so far if the current digit and the max so far are both greater than 1.
         Notice that there is a corner case that if the first digit is greater than 1, we should add it to the max so far instead of multiplying by the max so far since the initial value of max is set to 0.
     Method 2:
-        Iterate the string, if the current number is 0 or 1, addition always yields the best result.
+        Iterate the string, if the current number is 0 or 1, addition always yields the max result.
         If the current number is greater than 1, then there are 2 sub cases:
             1) add it to sum if current sum is 0 or 1.
             2) multiply it by sum if current sum is greater than 1.
@@ -28,7 +28,7 @@ public class Solution {
         if (str == null || str.length() == 0) return 0;
         int maxSofar = 0;
         for (int i = 0; i < str.length(); i++) {
-            int digit = (int)(str.charAt(i) - '0');
+            int digit = (int)(str.charAt(i)-'0');
             if (maxSofar == 0 && digit > 1) {
                 maxSofar = digit;
                 continue;
