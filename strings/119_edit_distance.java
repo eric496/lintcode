aa1cc4a618e0dedfb8c7801832468535419dc95e
eric496 <eric.mlengineer@gmail.com>
2017-12-03 23:48:14 -0500

    add thought

--- strings/119_edit_distance.java
+++ strings/119_edit_distance.java
@@ -12,23 +12,23 @@ Example
 /*
 Thought process:
     DP: For exmple word1 = "azced", and word2 = "abcdef" 
-         ""    "a"    "b"    "c"    "d"    "e"    "f"
-    ""   0      1      2      3      4      5      6
-    "a"  1      0      2      3      4      5      6
-    "z"  2      1      1      2      3      4      5
-    "c"  3      2      2      1      2      3      4
-    "e"  4      3      3      2      3      2      3
-    "d"  5      4      4      3      2      3      3
+             ""    "a"    "b"    "c"    "d"    "e"    "f"
+        ""   0      1      2      3      4      5      6
+        "a"  1      0      2      3      4      5      6
+        "z"  2      1      1      2      3      4      5
+        "c"  3      2      2      1      2      3      4
+        "e"  4      3      3      2      3      2      3
+        "d"  5      4      4      3      2      3      3
 
-    Using the characters in word2 as the first row, the characters in word1 as the first column. 
-    Add an extra row and column which represent empty strings. The row represent partial string. 
-    For example, row 2 and column 5 (which is 4) represents converting from "abcd" to "a".
-    There are two cases: 
-        1) The current character is different from the converted (insert, delete, replace are all possible operations)
-           To delete, dp[i][j] = dp[i][j-1]+1. To insert, dp[i][j] = dp[i-1][j]+1. To replace, dp[i][j] = dp[i-1][j-1]+1.
-           We pick whichever is minimal as the current cell value.
-        2) the current character is the same as the converted (nothing needs to be done)
-           dp[i][j] = dp[i-1][j-1]
+        Using the characters in word2 as the columns, the characters in word1 as the rows. Add an extra row and column which represent empty strings.
+        For example, row 2 and column 5 (which is 4) represents converting "a" to "abcd".
+        First row means converting an empty string (by inserting characters) to a string represented by column characters.
+        First columns means converting a string (by deleting characters) to an empty string.  
+        There are two cases: 
+            1) The current character is different from the converted (insert, delete, replace are all possible operations)
+               To delete, dp[i][j] = dp[i][j-1]+1. To insert, dp[i][j] = dp[i-1][j]+1. To replace, dp[i][j] = dp[i-1][j-1]+1.
+               We pick whichever is minimal as the current cell value.
+            2) The current character is the same as the converted (nothing needs to be done), the result is just the same as the shorter string without the new character dp[i][j] = dp[i-1][j-1].
 */
 
 // O(n^2) time and O(n^2) space
