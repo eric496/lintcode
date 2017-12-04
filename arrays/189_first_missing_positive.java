5b7d30fc6b35ab80778b9a1d0d0cdc89184d5695
eric496 <eric.mlengineer@gmail.com>
2017-12-04 15:53:27 -0500

    modify solution

--- arrays/189_first_missing_positive.java
+++ arrays/189_first_missing_positive.java
@@ -15,8 +15,8 @@ Thought process:
     Notice the conditions in the while loop:
         1) A[i] > 0              ~ (positive integer)
         2) A[i] <= A.length      ~ (the positive integer should be in the range [1, n])  e.g. [1,2,4,-2,10]
-        3) A[i] != i + 1         ~ (the positive integer is not in the right place) 
-    If we do not find any missing positive integer in the sequence, we can conclude that it must be the last integer is missing, because that is the only one we have not checked yet.
+        3) A[i] != i + 1         ~ (the positive integer is not in the correct position) 
+    If we do not find any missing positive integer in the sequence, the last integer must be the missing one, because that is the only one we have not checked yet.
     e.g. Given [0,1,2] return 3.
           
 */
@@ -25,17 +25,17 @@ public class Solution {
     public int firstMissingPositive(int[] A) {
         if (A == null) return 1;
         for (int i = 0; i < A.length; i++) {
-            while (A[i] > 0 && A[i] <= A.length && A[i] != (i+1)) {
+            while (A[i] > 0 && A[i] <= A.length && A[i] != i + 1) {
+                if (A[A[i]-1] == A[i]) break;
                 int tmp = A[A[i]-1];
-                if (tmp == A[i]) break;
                 A[A[i]-1] = A[i];
                 A[i] = tmp;
             }
         }
-        for (int i = 0; i < A.length; i ++) {
+        for (int i = 0; i < A.length; i++) {
             if (A[i] != i + 1) return i + 1;
         }
-        // missing number is the last number
+	// corner case: the last number is the missing one
         return A.length + 1;
     }
 }
