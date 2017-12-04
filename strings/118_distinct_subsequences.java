7df7acc6c10ff929339b106a643447a0727d5801
eric496 <eric.mlengineer@gmail.com>
2017-12-03 23:38:30 -0500

    add thought

--- strings/118_distinct_subsequences.java
+++ strings/118_distinct_subsequences.java
@@ -12,26 +12,42 @@ Challenge
 
 /*
 Thought process:
-
+    DP: Use characters in S as columns and characters in T as rows:
+             |  ""  |  "r"  |  "a"  |  "b"  |  "b"  |  "b"  |  "i"  |  "t"  |
+        ""   |   1  |   1   |   1   |   1   |   1   |   1   |   1   |   1   | 
+        "r"  |   0  |   1   |   1   |   1   |   1   |   1   |   1   |   1   |
+        "a"  |   0  |   0   |   1   |   1   |   1   |   1   |   1   |   1   | 
+        "b"  |   0  |   0   |   0   |   1   |   2   |   3   |   3   |   3   |
+        "b"  |   0  |   0   |   0   |   0   |   1   |   3   |   3   |   3   |
+        "i"  |   0  |   0   |   0   |   0   |   0   |   0   |   3   |   3   |
+        "t"  |   0  |   0   |   0   |   0   |   0   |   0   |   0   |   3   |
+    
+	The first row and column are empty strings. An empty string is 1 distinct subsequence of the empty string, so dp[0][0] = 1. 
+        An empty string is 1 distinct subsequence of any other non-empty strings, so dp[0][i] = 1, for all elements in the first row.
+        Any non-empty string is not a distinct subsequence of the empty string, so dp[i][0] = 0, for all elements in the first column execpt the first element.
+        For any dp[i][j] where i >= 1 and j >= 1 in the table, there are 2 cases:
+            1) if T[i-1] != S[j-1], then dp[i][j] equals the number of distinct subsequences without the new character (dp[i][j-1]).
+	    2) if T[i-1] == S[j-1], then dp[i][j] equals case 1) plus the number of subsequences one character shorter (dp[i-1][j-1]).
 */
 
 // O(n^2) time and O(n^2) space
 public class Solution {
     public int numDistinct(String S, String T) {
-        if (S == null || T == null || S.length() < T.length()) return 0;
-        int[][] dp = new int[S.length()+1][T.length()+1];
+        if (S == null || T == null) return 0;
+        int[][] nums = new int[T.length()+1][S.length()+1];
         for (int i = 0; i <= S.length(); i++) {
-            dp[i][0] = 1;
+            nums[0][i] = 1;
         }
-        for (int i = 1; i <= S.length(); i++) {
-            for (int j = 1; j <= T.length(); j++) {
-                dp[i][j] = dp[i-1][j];
-                if (S.charAt(i-1) == T.charAt(j-1)) {
-                    dp[i][j] += dp[i-1][j-1];
+        for (int i = 1; i <= T.length(); i++) {
+            for (int j = 1; j <= S.length(); j++) {
+                if (T.charAt(i-1) == S.charAt(j-1)) {
+                    nums[i][j] = nums[i-1][j-1] + nums[i][j-1];
+                } else {
+                    nums[i][j] = nums[i][j-1];
                 }
             }
         }
-        return dp[S.length()][T.length()];
+        return nums[T.length()][S.length()];
     }
 }
 
