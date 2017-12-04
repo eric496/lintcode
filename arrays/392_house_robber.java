4a9187b7503a3bff4db2b4fa5b7a2eb03b8552ba
eric496 <eric.mlengineer@gmail.com>
2017-12-04 16:36:16 -0500

    add solution

--- arrays/392_house_robber.java
+++ arrays/392_house_robber.java
@@ -11,19 +11,39 @@ Challenge
 
 /*
 Thought process:
-    DP
+    1. DP: Create a DP array to store the max money obtainable so far at the current house:
+           Whether to rob the current house is a binary decision based on:
+               1) dp[i-1] (does not rob the current one, so dp[i] = dp[i-1])
+               2) dp[i-2] + A[i-1] (rob the current one, notice the current house is A[i-1] because dp has length n+1)
+           whichever is greater.
 */
 
+// method 1
 public class Solution {
     public long houseRobber(int[] A) {
         int n = A.length;
         if (n == 0) return 0;
-        long[] result = new long[n+1];
-        result[0] = 0;
-        result[1] = A[0];
+        long[] dp = new long[n+1];
+        dp[0] = 0;
+        dp[1] = A[0];
         for (int i = 2; i <= n; i++) {
-            result[i] = Math.max(result[i-1], result[i-2]+A[i-1]);
+            dp[i] = Math.max(dp[i-1], dp[i-2]+A[i-1]);
         }
-        return result[n];
+        return dp[n];
+    }
+}
+
+// method 2
+public class Solution {
+    public long houseRobber(int[] A) {
+        long oddSum = 0, evenSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (i % 2 == 0) {
+                oddSum = Math.max(oddSum+A[i], evenSum);
+            } else {
+                evenSum = Math.max(evenSum+A[i], oddSum);
+            }
+        }
+        return Math.max(oddSum, evenSum);
     }
 }
