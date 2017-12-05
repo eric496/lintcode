f01c99a24e5a405c7c62001227cd41f578803990
eric496 <eric.mlengineer@gmail.com>
2017-12-04 19:30:47 -0500

    modify solution

--- matrix/401_kth_smallest_number_in_sorted_matrix.java
+++ matrix/401_kth_smallest_number_in_sorted_matrix.java
@@ -23,14 +23,14 @@ Thought process:
 public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
 	int low = matrix[0][0], high = matrix[matrix.length-1][matrix[0].length-1] + 1;
-        while(low < high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
             int count = 0,  j = matrix[0].length - 1;
             for (int i = 0; i < matrix.length; i++) {
-                while(j >= 0 && matrix[i][j] > mid) {
+                while (j >= 0 && matrix[i][j] > mid) {
 		    j--;
 		}
-                count += (j + 1);
+                count += j + 1;
             }
             if (count < k) {
                 low = mid + 1;
