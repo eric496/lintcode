b64bbb28dc6cbda59f52b71c3560d86a22d6b91b
eric496 <eric.mlengineer@gmail.com>
2017-12-06 01:28:32 -0500

    add solution

--- trees/88_lowest_common_ancestor.java
+++ trees/88_lowest_common_ancestor.java
@@ -29,6 +29,7 @@ Example
  * }
  */
 
+// recursive
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
         if (root == null || root == node1 || root == node2) return root;
@@ -40,5 +41,37 @@ public class Solution {
         if (left != null) return left;
         if (right != null) return right;
         return null;
+    }
+}
+
+// iterative
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
+        if (root == null || root == node1 || root == node2) return root;
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        parent.put(root, null);
+        stack.push(root);
+        while (!parent.containsKey(node1) || !parent.containsKey(node2)) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+        Set<TreeNode> ancestors = new HashSet<>();
+        TreeNode n1 = node1, n2 = node2;
+        while (n1 != null) {
+            ancestors.add(n1);
+            n1 = parent.get(n1);
+        }
+        while (!ancestors.contains(n2)) {
+            n2 = parent.get(n2);
+        }
+        return n2;
     }
 }
