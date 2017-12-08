89227a0f114259d0e359f61f21dd8f0c4595d9fc
eric496 <eric.mlengineer@gmail.com>
2017-12-08 00:09:02 -0500

    modify solution

--- linked_lists/170_rotate_list.java
+++ linked_lists/170_rotate_list.java
@@ -5,6 +5,13 @@ Example
     Given 1->2->3->4->5 and k = 2, return 4->5->1->2->3.
 */
 
+/*
+Thought process:
+    Three pointers: a dummy head, a fast runner, a slow runner. 
+    The fast runner traverses the list and counts the length of the list. It points to the last element in the list. The slow runner traverses (len-k) elements.
+    Finally rotate the list. Remember k = k%len in case k is larger than the length of the list.
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -19,18 +26,20 @@ Example
 
 public class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head==null || head.next==null) return head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode fast = dummy, slow = dummy;
-        int i;
-        for (i = 0; fast.next != null; i++)//Get the total length 
+        if (head == null || head.next == null) return head;
+        ListNode dummyhead = new ListNode(-1);
+        dummyhead.next = head;
+        ListNode fast = dummyhead, slow = dummyhead;
+        int len;
+        for (len = 0; fast.next != null; len++) { 
             fast = fast.next;
-        for (int j = i - k % i; j > 0; j--) //Get the i-n%i th node
-            slow=slow.next;
-        fast.next = dummy.next; //Do the rotation
-        dummy.next = slow.next;
+        }
+        for (int j = 0; j < len - k % len; j++) {
+       	    slow = slow.next;
+        }
+        fast.next = dummyhead.next;
+        dummyhead.next = slow.next;
         slow.next = null;
-        return dummy.next;
+        return dummyhead.next;
     }
 }
