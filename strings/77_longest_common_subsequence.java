2a4641f6291411d068337ed734abfe8905774f13
eric496 <eric.mlengineer@gmail.com>
2017-12-08 22:45:46 -0500

    add thought

--- strings/77_longest_common_subsequence.java
+++ strings/77_longest_common_subsequence.java
@@ -14,7 +14,14 @@ Example
 
 /*
 Thought process:
-    DP
+    DP: Create a table with rows representing one string and coloumns the other.
+              ""  |  "A"  |  "B"  |  "C"  |  "D"  |
+       ""  |  0   |   0   |   0   |   0   |   0   |
+       "E" |  0   |   0   |   0   |   0   |   0   |
+       "A" |  0   |   1   |   1   |   1   |   1   |
+       "C" |  0   |   1   |   1   |   2   |   2   |
+       "B" |  0   |   1   |   2   |   2   |   2   |
+        If two characters are the same, then dp[i][j] is dp[i-1][j-1] + 1. If two characters are different, then dp[i][j] equals either dp[i-1][j] or dp[i][j-1] whichever is greater.
 */
 
 public class Solution {
@@ -23,9 +30,10 @@ public class Solution {
         int dp[][] = new int[n+1][m+1];
         for (int i = 1; i <= n; i++){
             for (int j = 1; j <= m; j++){
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 if (A.charAt(i-1) == B.charAt(j-1)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
