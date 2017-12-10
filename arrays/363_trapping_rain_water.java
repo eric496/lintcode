f59f7dc4ac2aaa7c624efd80ff4e458ac8b78d6d
eric496 <eric.mlengineer@gmail.com>
2017-12-09 22:15:22 -0500

    modify solution

--- arrays/363_trapping_rain_water.java
+++ arrays/363_trapping_rain_water.java
@@ -1,6 +1,5 @@
 /*
 Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
-Trapping Rain Water
 
 Example
     Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
@@ -10,28 +9,31 @@ Challenge
     O(n) time and O(n) memory is also acceptable.
 */
 
+/*
+Thought process:
+
+*/
+
 public class Solution {
     public int trapRainWater(int[] heights) {
-        int n = heights.length;
-        int left = 0; int right = n - 1;
-        int res = 0;
-        int maxleft = 0, maxright = 0;
-        while(left <= right){
-            if(heights[left] <= heights[right]){
-                if(heights[left] >= maxleft) 
+        int left = 0, right = heights.length - 1, result = 0, maxleft = 0, maxright = 0;
+        while (left <= right) {
+            if (heights[left] <= heights[right]) {
+                if (heights[left] >= maxleft) {
                     maxleft = heights[left];
-                else 
-                    res += maxleft-heights[left];
+                } else {
+                    result += maxleft - heights[left];
+		}
                 left++;
-            }
-            else{
-                if(heights[right] >= maxright) 
+            } else {
+                if (heights[right] >= maxright) {
                     maxright = heights[right];
-                else 
-                    res += maxright-heights[right];
+                } else { 
+                    result += maxright - heights[right];
+		}
                 right--;
             }
         }
-        return res;
+        return result;
     }
 }
