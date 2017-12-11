fba0f7fb24c8d940d47026d17990af9c049eb11d
eric496 <eric.mlengineer@gmail.com>
2017-12-10 21:40:16 -0500

    add solution

--- linked_lists/104_merge_k_sorted_lists.java
+++ linked_lists/104_merge_k_sorted_lists.java
@@ -14,7 +14,8 @@ Example
 
 /*
 Thought process:
-    Mergesort
+    1. Recursive solution
+    2. Priority queue
 */
 
 /**
@@ -29,7 +30,7 @@ Thought process:
  * }
  */ 
 
-// mergesort
+// recursion
 public class Solution {
     public ListNode mergeKLists(List<ListNode> lists) {  
         if (lists == null || lists.size() == 0) return null;
@@ -41,8 +42,7 @@ public class Solution {
             return lists.get(low);
         } else if (low < high) {
             int mid = low + (high - low) / 2;
-            ListNode left = mergeKLists(lists, low, mid);
-            ListNode right = mergeKLists(lists, mid+1, high);
+            ListNode left = mergeKLists(lists, low, mid), right = mergeKLists(lists, mid+1, high);
             return mergeTwoLists(left, right);
         }
         return null;
@@ -51,8 +51,8 @@ public class Solution {
     private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
-        ListNode runner = new ListNode(Integer.MIN_VALUE);
-        ListNode head = runner;
+	ListNode dummyhead = new ListNode(-1);
+        ListNode runner = dummyhead;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 runner.next = l1;
@@ -69,6 +69,40 @@ public class Solution {
         if (l2 != null) {
 	    runner.next = l2;
 	}
-        return head.next;
+        return dummyhead.next;
+    }
+}
+
+// priority queue
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists == null || lists.size() == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val) {
+                    return -1;
+                } else if (o1.val==o2.val) {
+                    return 0;
+                } else { 
+                    return 1;
+                }
+            }
+        });
+        ListNode dummyhead = new ListNode(-1);
+        ListNode runner = dummyhead;
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+        while (!queue.isEmpty()) {
+            runner.next = queue.poll();
+            runner = runner.next;
+            if (runner.next != null) {
+                queue.add(runner.next);
+            }
+        }
+        return dummyhead.next;
     }
 }
