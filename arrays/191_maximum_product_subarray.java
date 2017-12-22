4a6c980f897ed1263e5594efee4b38d310fe0a4c
eric496 <eric.mlengineer@gmail.com>
2017-12-21 21:55:09 -0500

    add solution

--- arrays/191_maximum_product_subarray.java
+++ arrays/191_maximum_product_subarray.java
@@ -5,6 +5,12 @@ Example
     For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has the largest product = 6.
 */
 
+/*
+Thought process:
+
+*/
+
+// method 1
 public class Solution {
     public int maxProduct(int[] nums) {
         if (nums.length == 0) return 0;
@@ -17,5 +23,25 @@ public class Solution {
             minPrev = minSofar;
         }
         return max;
+    }
+}
+
+// method 2 (improvement)
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+        int maxSofar = nums[0], minSofar = nums[0], result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                maxSofar = Math.max(maxSofar*nums[i], nums[i]);
+                minSofar = Math.min(minSofar*nums[i], nums[i]);
+            } else {
+                int temp = maxSofar;
+                maxSofar = Math.max(minSofar*nums[i], nums[i]);
+                minSofar = Math.min(temp*nums[i], nums[i]);
+            }
+            result = Math.max(result, maxSofar);
+        }
+        return result;
     }
 }
