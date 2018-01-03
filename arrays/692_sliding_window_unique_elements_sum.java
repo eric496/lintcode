c59b200800e9464da4a7e4bb2ae67f783245bf25
eric496 <eric.mlengineer@gmail.com>
2018-01-03 15:52:05 -0500

    add solution

--- arrays/692_sliding_window_unique_elements_sum.java
+++ arrays/692_sliding_window_unique_elements_sum.java
@@ -10,3 +10,39 @@ Example
     Return 5
 */
 
+public class Solution {
+    private int result = 0;
+    public int slidingWindowUniqueElementsSum(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 1) return -1;
+        if (k > nums.length) {
+            k = nums.length;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i])+1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        countUnique(map);
+        for (int i = k; i < nums.length; i++) {
+            map.put(nums[i-k], map.get(nums[i-k])-1);
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i])+1);
+            } else {
+                map.put(nums[i], 1);
+            }
+            countUnique(map);
+        }
+        return result;
+    }
+    
+    private void countUnique(Map<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if (entry.getValue() == 1) {
+                result++;
+            }
+        }
+    }
+}
