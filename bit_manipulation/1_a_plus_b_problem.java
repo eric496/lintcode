17337967d2cc1516269f8664851e05f95ba4e5af
eric496 <eric.mlengineer@gmail.com>
2018-01-04 16:40:51 -0500

    add thought

--- bit_manipulation/1_a_plus_b_problem.java
+++ bit_manipulation/1_a_plus_b_problem.java
@@ -16,11 +16,21 @@ Example
     Given a=1 and b=2 return 3
 */
 
+/*
+Thought process:
+    The bitwise XOR operator is equivalent to arithmetic addition without carry. For example, a^b is just a+b without carry.
+    Carry occurs when both bits are 1s, so a&b is where carry occurs and (a&b) << 1 is the result of carry only.
+    The final result is just addition without carry and the carry itself only, which is just a + b = a ^ b + a & b << 1.
+    Since + operator is not allowed, let b = carry << 1, which is the result with carry for each place, 
+    and a = a ^ b which is the result of addition with carry at one place each time. 
+    Repeat this using a while loop until b does not carry any more.     
+*/
+
 class Solution {
     public int aplusb(int a, int b) {
-        while(b != 0) {
+        while (b != 0) {
             int carry = a & b;
-            a = a ^ b;
+            a ^= b;
             b = carry << 1;
         }
         return a;
