f31d271ee2684f321c579727942fe669ea1ad400
eric496 <eric.mlengineer@gmail.com>
2018-01-09 17:32:31 -0500

    add thought

--- strings/720_rearrange_a_string_with_integers.java
+++ strings/720_rearrange_a_string_with_integers.java
@@ -6,6 +6,14 @@ Example
     Alphabets in the lexicographic order, followed by the sum of integers(2 and 3).
 */
 
+/*
+Thought process:
+    Since there is only uppercase alphabets (26 in total) and integers, we can use an array of length 26 to count the frequency of the characters.
+    If it is not a alphabet, then it must be an integer, add it to sum. 
+    After a whole pass of the string, we get the alphabets in order and their frequencies.
+    Append the ordered alphabets to result and append the sum at the end.
+*/
+
 public class Solution {
     public String rearrange(String str) {
         int[] count = new int[26];
@@ -17,16 +25,16 @@ public class Solution {
                 sum += str.charAt(i) - '0';
             } 
         }
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < 26; i++) {
             char ch = (char)('A'+i);
             while (count[i]-- != 0) {
-                result += ch;
+                result.append(ch);
             }
         }
         if (sum > 0) {
-            result += sum;
+            result.append(sum);
         }
-        return result;
+        return result.toString();
     }
 }
