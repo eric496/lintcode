6372a5c825ff915be5f5120f71089f6be01cabf6
eric496 <eric.mlengineer@gmail.com>
2018-01-09 17:39:24 -0500

    modify solution

--- arrays/100_remove_duplicates_from_sorted_array.java
+++ arrays/100_remove_duplicates_from_sorted_array.java
@@ -10,14 +10,14 @@ Example
 /*
 Thought process:
     Two pointers: Create a unique value counter, which is a position pointer as well.
-                  Iterate the array, once the current element is different from the elemtent at the position pointer, move it to the position right to the position pointer. 
-                  Meanwhile, increment the position pointer.
+                  Iterate the array, once the current element is different from the elemtent at the position pointer, 
+                  move it to the position right to the position pointer. Meanwhile, increment the position pointer.
 */
 
 // O(n) time and O(1) space
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0 || nums == null) return 0;
+        if (nums.length <= 1 || nums == null) return 0;
         // set count to 1 because there is at least one element
         int count = 1;
         for (int i = 1; i < nums.length; i++) {
