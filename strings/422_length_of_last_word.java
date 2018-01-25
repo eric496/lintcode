3498d3c82a57c9827352fbe59bfd4ab7f2bba3a0
eric496 <eric.mlengineer@gmail.com>
2018-01-24 22:06:05 -0500

    add solution

--- strings/422_length_of_last_word.java
+++ strings/422_length_of_last_word.java
@@ -88,3 +88,19 @@ public class Solution {
         return count;
     }
 }
+
+// simplify the above solution
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.trim().length() == 0) return 0;
+        int count = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else if (s.charAt(i) == ' ' && count != 0) {
+                break;
+            }
+        }
+        return count;
+    }
+}
