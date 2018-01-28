99a229e660aafa323bff18245b1b163b18cc2c25
eric496 <eric.mlengineer@gmail.com>
2018-01-27 23:34:49 -0500

    modify solution

--- linked_lists/102_linked_list_cycle.java
+++ linked_lists/102_linked_list_cycle.java
@@ -30,11 +30,11 @@ Thought process:
 public class Solution {
     public boolean hasCycle(ListNode head) {  
         if (head == null || head.next == null) return false;
-        ListNode slow = head, fast = head.next;
+        ListNode slow = head, fast = head;
         while (fast.next != null && fast.next.next != null) {
-            if (slow == fast) return true;
             slow = slow.next;
             fast = fast.next.next;
+            if (slow == fast) return true;
         }
         return false;
     }
