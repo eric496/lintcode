3a78869ca8550c7f0ab47e03dae88bff88156c15
eric496 <eric.mlengineer@gmail.com>
2018-01-31 00:03:49 -0500

    modify solution

--- strings/638_string_homomorphism.java
+++ strings/638_string_homomorphism.java
@@ -55,8 +55,7 @@ public class Solution {
         int[] m2 = new int[256];
         for (int i = 0; i < s.length(); i++) {
             if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
-            m1[s.charAt(i)] = i;
-            m2[t.charAt(i)] = i;
+	    m1[s.charAt(i)] = m2[t.charAt(i)] = i + 1;
         }
         return true;
     }
